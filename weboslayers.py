664458c536c3f1540187624a86040f619c1e6751
Martin Jansa <martin.jansa@lge.com>
2020-01-10 01:30:02 -0800

    weboslayers.py: use known to build tag of meta-webosose instead of latest in thud branch
    * I'll start tagging the branches in meta-webosose every time I rebase them on newer
      OSE build or when adding more changes for compatibility with newer upstream layers
      that way the build-webos should always work and you would be able to go back to
      older revision as well (without needing to find some compatible meta-webosose
      revision on your own)
    
    Signed-off-by: Martin Jansa <martin.jansa@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.0',  31, 'git://github.com/shr-project/meta-webosose.git'          '', ''),
 ('meta-webos-backports-3.1',  32, 'git://github.com/shr-project/meta-webosose.git'          '', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git'          'branch=thud', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git'          'branch=thud,commit=thud-2020-01-26', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=4e5be97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git'          '', ''),
