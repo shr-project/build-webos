356a41c5a14f0ff072246029cad317fd06b5d545
Martin Jansa <martin.jansa@lge.com>
2020-04-07 09:24:32 -0700

    webos-image: Update layer pins for 2020-04-07 for thud
    meta-updater as of 2020-04-02 17:26:27 +0200
    
    f4d9d3c Merge pull request #709 from advancedtelematic/feat/thud/2020.5
    ba0db1c aktualizr: Bump to 2020.5 and get garage-sign from the new
            AWS bucket.

--- weboslayers.py
+++ weboslayers.py
@@ -68,7 +68,7 @@ webos_layers = [
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
-('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=thud,commit=2d73938', ''),
+('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=thud,commit=f4d9d3c', ''),
 ('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=thud,commit=7685c7d', ''),
 
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=20331bd', ''),
