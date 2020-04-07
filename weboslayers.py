c44236776784978f9e09573e54dd0191ad7e528d
Martin Jansa <martin.jansa@lge.com>
2020-04-07 09:22:06 -0700

    webos-image: Update layer pins for 2020-04-07 for zeus
    meta-updater as of 2020-04-02 17:26:08 +0200
    
    416a934 Merge pull request #707 from advancedtelematic/feat/zeus/2020.5
    073a7d9 aktualizr: Bump to 2020.5 and get garage-sign from the new
            AWS bucket.
    
    meta-virtualization as of 2020-04-05 22:16:15 -0400
    
    69f08a9 hyperstart: refresh
            0001-container.c-Fix-compiler-errors-that-gcc-8.1.0-repor.patch
            to apply cleanly

--- weboslayers.py
+++ weboslayers.py
@@ -68,8 +68,8 @@ webos_layers = [
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
-('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=zeus,commit=8997893', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=zeus,commit=561b597', ''),
+('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=zeus,commit=416a934', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=zeus,commit=69f08a9', ''),
 
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=20331bd', ''),
 
