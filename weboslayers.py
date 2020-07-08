630db225275e09f169cf095cf6ac7330b5570676
Martin Jansa <martin.jansa@lge.com>
2020-07-08 03:54:28 -0700

    webos-image: Update to Qt 5.12.9 (build 19xx part 1/2)
    meta-webosose
    81526d08 qtdeclarative: restore 0009-QQuickWindow-Add-invalidateCursorItem.patch temporarily
    0ab3c296 qtdeclarative=webos54 (backport a fix to use python3 explicitly)
    11f1550b qtdeclarative=webos53: update for 5.12.9
    
    meta-qt5 as of 2020-07-07 12:06:31 +0200
    
    90f99d8 qt5: upgrade to 5.12.9
    
    Change-Id: I9be40ef0a1f9fbd9899e704f3ee32e856bb70611

--- weboslayers.py
+++ weboslayers.py
@@ -71,7 +71,7 @@ webos_layers = [
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=thud,commit=e1b2028', ''),
 ('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=thud,commit=7685c7d', ''),
 
-('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=7d0b17a', ''),
+('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=90f99d8', ''),
 
 ('meta-webos-backports-2.7',  30, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-backports-3.0',  31, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
