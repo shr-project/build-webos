f771d54c7b8d3bf9f8eabd07df391b7abbd203ca
Martin Jansa <martin.jansa@lge.com>
2020-10-10 09:12:54 -0700

    Use dunfell-2020-10-10 tag for meta-webosose
    More host-user-contaminated fixes
    
    meta-webosose as of 2020-10-10 09:10:59 -0700
    
    318a5c7b qml-webos-framework=r31 (fix host-user-contaminated QA issues)
    7300a25a qt-features-webos=r7
    d2b20f5f qtwayland=webos21 (backport fix for do_install)
    fde761b6 qtbase=webos81 qtbase-native=webos3

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=9248dc7', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2020-10-09', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2020-10-10', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=085fb07', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
