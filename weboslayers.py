26434ef132314bc8ada1660bb1ca74cdb84c0019
Martin Jansa <martin.jansa@lge.com>
2020-10-31 03:58:27 -0700

    webos-image: Update layer pins for 2020-10-31 from dunfell
    meta-webosose as of 2020-10-31 17:40:47 -0700
    
    a67c7e27 gstreamer1.0-plugins-bad=webosrpi4 (update patches to apply on 1.16.3)
    
    oe-core as of 2020-10-26 15:06:51 -1000
    
    8a367cbbf0 glib-2.0: fix parsing of slim encoded tzdata
    d0dd153b0a gst-validate: Update 1.16.2 -> Update 1.16.3
    5e93456e8e gstreamer1.0-python: Update 1.16.2 -> Update 1.16.3
    559751de08 gstreamer1.0-omx: Update 1.16.2 -> Update 1.16.3
    9cc8c65823 gstreamer1.0-rtsp-server: Update 1.16.2 -> Update 1.16.3
    3895453690 gstreamer1.0-vaapi: Update 1.16.2 -> Update 1.16.3
    cc27832fb7 gstreamer1.0-libav: Update 1.16.2 -> Update 1.16.3
    cb759c9a07 gstreamer1.0-plugins-ugly: Update 1.16.2 -> Update 1.16.3
    8ef5803a85 gstreamer1.0-plugins-bad: Update 1.16.2 -> Update 1.16.3
    ae0f8a8895 gstreamer1.0-plugins-good: Update 1.16.2 -> Update 1.16.3
    595625b709 gstreamer1.0-plugins-base: Update 1.16.2 -> Update 1.16.3
    d7e74eb1e3 gstreamer1.0: Update 1.16.2 -> Update 1.16.3
    62ecf9737c gstreamer1.0: Fix reproducibility issue around libcap
    326541919b ffmpeg: fix CVE-2020-12284
    406a1636e1 gcc: mitigate the Straight-line Speculation attack
    ed912771ea oeqa: Add sync call to command execution
    89e6fc44a3 qemu: change TLBs number to 64 in 34Kf mips cpu model
    83787094df packagegroup-core-tools-debug: Disable for rv32/glibc
            as well
    4f77fe468f selftest/virgl: drop the custom 30 sec timeout
    0a22c2a37f grub2: fix CVE-2020-10713
    2f607a61a8 python3: fix CVE-2020-26116
    f2e07dcf2c libproxy: fix CVE-2020-25219
    197b405dd0 siteinfo: Recognize bigendian sh3be and sh4be
    8c602978de siteinfo: Recognize 32bit PPC LE
    1bdaf40c02 toolchain-shar-extract.sh: don't print useless info
    8d2702b7fe tune-riscv.inc: use nf suffix also for TUNE_PKGARCH
    6243df4e8d tune-ep9312.inc: add t suffix for thumb to
            PACKAGE_EXTRA_ARCHS_tune-ep9312
    5a5abae29f tune-mips64r6.inc: fix typo in mipsisa64r6-nf
    c1bc580dc4 arch-mips.inc: remove duplicated mips64el-o32 from
            PACKAGE_EXTRA_ARCHS_tune-mips64el-o32
    96efcd0d12 arch-armv7a.inc: fix typo
    
    bitbake as of 2020-10-26 22:18:43 +0000
    
    89fc9450 tests/fetch: Update upstream master->main branchname
            transition
    
    meta-updater as of 2020-10-30 17:03:28 +0200
    
    1479cef Merge pull request #781 from
            advancedtelematic/feat/dunfell/2020.10
    e5325ff ostree-kernel-initramfs: add back initramfs fit workaround
    75518b1 image_types_ostree: fix chmod
    4d054ea aktualizr: Upgrade to 2020.10 release

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=d8daad57', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=89fc9450', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=3ee9590f96', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=8a367cbbf0', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=2a5c534d2b', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -68,14 +68,14 @@ webos_layers = [
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
-('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=69a19e8', ''),
+('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=1479cef', ''),
 ('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=ff997b6', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=07dca1e', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=9248dc7', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2020-10-23', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2020-10-31', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=085fb07', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
