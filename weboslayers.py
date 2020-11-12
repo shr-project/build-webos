dbbe2f844b4db0e4c1ba6611657e6fedf109b5e7
Martin Jansa <martin.jansa@lge.com>
2020-11-12 07:19:26 -0800

    webos-image: Update layer pins for 2020-11-12 from dunfell
    oe-core as of 2020-11-08 16:51:24 -1000
    
    4f395ad49e grub: clean up CVE patches
    af52a1f1f3 grub: fix several CVEs in grub 2.04
    80077479fd ruby: fix CVE-2020-25613
    2c4607edf1 netbase: bump PE to purge bogus hash equivalence from
            autobuilder
    ba9a77477a netbase: update SRC_URI to reflect new file name
    6d53fe9a67 linux-firmware: upgrade 20200817 -> 20201022
    0671d04978 linux-firmware: package nvidia firmware
    37a815399a linux-firmware: package marvel sdio 8997 firmware
    9d30f3ff95 linux-firmware: add Amlogic VDEC firmware package
    10413cf013 update_udev_hwdb: clean hwdb.bin
    04c13fc467 qemuboot.bbclass: Fix a typo
    3ac10faff7 meta: fix some unresponsive homepages and bugtracker links
    8d54034bb8 sqlite3: fix CVE-2020-13632
    582f253d67 sqlite3: fix CVE-2020-13631
    5780879dec sqlite3: fix CVE-2020-13630
    4780662eba sqlite3: fix CVE-2020-13435
    0338c2eb09 sqlite3: fix CVE-2020-13434
    698c0cbf77 bluez5: update to 5.55 to fix CVE-2020-27153
    555cfcf53a jquery: Upgrade 3.4.1 -> 3.5.0 to fix CVE-2020-11022 and
            CVE-2020-11023
    e0201460f7 libsdl2: Fix directfb SDL_RenderFillRect
    f8d552be2e libsdl2: Fix directfb syntax error
    0898bd34fa Map license names PSF and PSFv2 to PSF-2.0
    9f452e54e9 Add license text for PSF-2.0
    72f2c45880 insane: add GitLab /archive/ tests
    61959632cb recipes-core/busybox: fixup licensing information
    d395630a02 common-licenses: add bzip2-1.0.4
    e40068578e timezone: upgrade to 2020d
    cda769ed7c sstatesig: Log timestamps for hashequiv in reprodubile
            builds for do_package
    419d45ad6a Revert "lib/oe/patch: fix handling of patches with no
            header"
    bbd467c393 lib/oe/patch: GitApplyTree: save 1 echo in commit-msg hook
    f4349c3957 lib/oe/patch: prevent applying patches without any subject
    0f688c1419 xcb-proto: backport fix for python gcd function

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=89fc9450', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=5f644082fc', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=4f395ad49e', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=2a5c534d2b', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=6a9de2d', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2020-11-11', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2020-11-12', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=9879932', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
