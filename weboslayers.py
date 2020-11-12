64c31bc8095b6d1a38a06aa51b61c9537d3c3005
Martin Jansa <martin.jansa@lge.com>
2020-11-12 07:23:01 -0800

    webos-image: Update layer pins for 2020-11-12 from master
    oe-core as of 2020-11-11 10:08:07 +0000
    
    4dce4e01cf binutils: reproducibility: reuse debug-prefix-map for stabs
    2bcc643195 testimage: print results for interrupted runs
    1c5e8baf57 oeqa/core/context: initialize _run_end_time
    a97ae47525 oeqa/core/context: expose results as variable
    309132e50d openssh: whitelist CVE-2014-9278
    55e09fff01 webkitgtk: fix build with ICU 68.1
    b5950dbe48 libical: backport a patch to fix build with ICU 68.1
    1b12900a22 icu: upgrade 67.1 -> 68.1
    70da1f956b openssl: Add c_rehash to misc package and add perl runtime
            dependency
    1bdcfa4b0d add new extrausers command passwd-expire
    e434627b29 iproute2: upgrade 5.8.0 -> 5.9.0
    5933d7bae8 python3: Update manifest
    bb8fa9950e wayland-utils: introduce a recipe
    09ef3dacc5 weston-init: fall back to fbdev under x32
    72dd74c5d3 weston-init: correctly start under systemd
    f89138e12c conf-notes.txt: mention more important images than just sato
    09ffd51416 musl: Update to latest master
    984da023a5 gcc: Handle duplicate names for variables
    f59a2db89a lto.inc: Disable LTO for perf
    4698e7868b puzzles: Check for excessive constant arguments
    50cd8edc98 gcc: Do no parameterize LTO configuration flags
    129ec897a2 lto.inc: Disable LTO for xserver-xorg
    c5252fc889 weston: Fix linking with LTO
    2b59fe1076 libaio: Disable LTO
    0a5778f40f lto: Introduce LTOEXTRA variable
    770c69c51f lto.inc: Add -ffat-lto-objects and -fuse-linker-plugin
    2ccaa99871 python3: Enable lto if its in DISTRO_FEATURES
    228b5a9151 lto: Add global LTO distro policy file
    
    bitbake as of 2020-11-09 17:21:05 +0000
    
    36ee8584 cookerdata: tweak to avoid mutable default argument
    991f92b4 bb.ui: delete __init__.py to make bb.ui a namespace package
    95990596 main: extract creation of argument parser into function so
            it can be utilized externally, e.g. by unit tests
    
    meta-oe as of 2020-11-11 10:14:00 -0800
    
    f3a2338978 suitesparse: Exclude from world
    4cdd651960 strongswan: remove ldap from default PACKAGECONFIG
    b570072902 strongswan: do not use deprecated stroke and starter
            by default
    65751e16ac packagegroup-meta-networking.bb: add ntopng related recipes
    127e50b456 ntop: remove the recipe
    f5b9e4ecd2 ntopng: add new recipe
    3b6b36e32d libmaxminddb: add new recipe
    1b37387c39 ndpi: add new recipe
    b7274c62b3 Add suitesparse
    27afa0a168 xmlsec1: upgrade 1.2.30 -> 1.2.31
    3c24232169 minifi-cpp: depend on nettle and lz4
    5b949a4c13 tmux: upgrade 3.1b -> 3.1c
    f36802756e poppler: upgrade 20.10.0 -> 20.11.0
    f20a4ba04f openvpn: upgrade 2.4.9 -> 2.5.0
    3866391da4 opensaf: upgrade 5.20.05 -> 5.20.08
    034d7be384 mraa: upgrade 2.1.0 -> 2.2.0
    ceba11b778 fbgrab: upgrade 1.3.3 -> 1.4
    83a340c9ad redis-plus-plus: Upgrade to 1.2.1
    7a0117ec4e dovecot: Disable LTO
    1b825a6e58 cmocka: Fix buils with LTO
    294e945309 traceroute: Fix build with LTO
    608a92b4e6 fluentbit: Disable LTO
    8c9ff09482 opengl-es-cts: Disable LTO
    7b31db8928 rdma-core: Upgrade to 31.1
    ac2b49017d fuse3: Upgrade to 3.10.0
    e1745fb68f lksctp-tools: Update to latest master
    71d8c0589b fmt: Upgrade to 7.1.2
    1752c98525 lvm2.inc: switch branch master to main

--- weboslayers.py
+++ weboslayers.py
@@ -58,11 +58,11 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=5b4a5bb0', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=36ee8584', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=bcb8b6719b', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=4dce4e01cf', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=b6e1480592', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=f3a2338978', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
