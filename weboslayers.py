a0b2685b2c1c59a39bd909bfa0383d1d50e681a6
Martin Jansa <martin.jansa@lge.com>
2020-11-13 07:07:35 -0800

    webos-image: Update layer pins for 2020-11-13 from master
    oe-core as of 2020-11-13 14:31:56 +0000
    
    9c3e3f83b5 libbsd: Remove BSD-4-Clause from main package
    fd55d41c9d mesa: Add xcb-fixes to loader when using x11 and dri3
    440dc8bacf systemtap: split runtime material in its own package
    75bca65ccd systemtap: fix install when python3-probes is disabled
            in PACKAGECONFIG
    70e3c46a4d systemtap: clarify the relation between exporter and
            python3-probes feature
    01372c6673 systemtap: remove extra dependencies
    ddc82d153f systemtap: split examples and python scripts out of
            main package
    2e753a12cf layer.conf: fix syntax error in PATH setting
    b7e2f53745 gdb: add PACKAGECONFIG for xz (lzma) compression support
    de7e36a785 systemd: Fix systemd when used with busybox less
    fb24d6504c lto.inc: Disable LTO for webkit
    51009ed1b8 libsdl2: Disable video-rpi
    5bc83ca06d gawk: Avoid using host ar during cross compile
    74ed1d1043 lrzsz: Use Cross AR during compile
    89ca044a9a Revert "rt-tests: Enable only for x86/ppc64 architectures"
    bf5b9c531e rt-tests: backport patch that enable build for all archs
    
    meta-oe as of 2020-11-12 16:13:30 -0800
    
    7a6a1ac1d3 python3-cvxopt: Exclude from world builds
    167c48962c Add python3-cvxopt
    24b704c925 meta-python: Drop RDEPEND'ing on python3-typing
    1a3788c926 python3-icu: Update to 2.6
    adf407f3f8 Add python3-cerberus
    8c3fc85553 Add python3-dill
    d1c2512eee Add python3-transitions

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=36ee8584', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=4dce4e01cf', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=9c3e3f83b5', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=f3a2338978', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=7a6a1ac1d3', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
