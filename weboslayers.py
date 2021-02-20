02328f41aa5baaee37c6bc89eefd4ebc6a228144
Martin Jansa <martin.jansa@lge.com>
2021-02-20 14:52:29 -0800

    webos-image: Update layer pins for 2021-02-20 from dunfell
    meta-webosose as of 2021-02-20 14:51:18 -0800
    
    01a30780 webruntime=r44.* (use correct flags and tools for host builds when target has the same cpu as host)
    9de49742 ose: webos_npm_env.bbclass: add new bbclass to unify the stanza for setting NPM
    
    oe-core as of 2021-02-18 23:43:04 +0000
    
    a8debddd6c build-appliance-image: Update to dunfell head revision
    48157a1ca6 python3targetconfig.bbclass: Make py3 dep and tasks only
            for target recipes
    bf0e67e6b9 gpgme: use python3targetconfig
    961b6ce18c meta: drop _PYTHON_SYSCONFIGDATA_NAME hacks
    458b8f495d distutils3-base.bbclass: use python3targetconfig
    ba32302980 python3-pycairo: use python3targetconfig
    0bdc6a25c1 python3: split python target configuration into own class
    7cb7c5a97d gcc-9.3.inc: Fix potential runtime crash
    641c9f6a20 p11-kit: upgrade 0.23.21 -> 0.23.22
    f28b3af0c7 p11-kit: upgrade 0.23.20 -> 0.23.21
    b33b4f318e libcroco: Added CVE
    2ebd235bc8 libgcrypt: Whitelisted CVEs
    dfa4b0e6ca oe-pkgdata-util: Check if environment script is initialized
    0807c033f1 opkg: Fix patch glitches
    61c639fcfd opkg: Fix build reproducibility issue
    07a1771c9b image_types.bbclass: tar: use posix format instead of gnu
    6a474f7742 image_types: Ensure tar archives are reproducible
    f67e9a8ede uninative: Upgrade to 2.10
    
    bitbake as of 2021-02-18 16:44:25 +0000
    
    fa94374b lib/bb/fetch2/__init__.py: drop _PYTHON_SYSCONFIGDATA_NAME
            unsetting
    
    meta-virtualization as of 2021-02-19 10:48:52 -0500
    
    502b81a libvirt-python: inherit python3targetconfig
    
    meta-python2 as of 2021-02-16 19:23:17 +0100
    
    c96cfe3 python: Avoid installing test data into recipe-sysroot

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=2351b496', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=fa94374b', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=9efabaff73', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=a8debddd6c', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=de37512b25', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -69,13 +69,13 @@ webos_layers = [
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=4617a60', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=92cd346', ''),
-('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=07dca1e', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=502b81a', ''),
+('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=c96cfe3', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=c7ec556', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2021-02-08', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2021-02-20', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
