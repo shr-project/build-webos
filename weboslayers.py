6a4c584e6e161fc8df77f25cd7098ff7199b152d
Martin Jansa <martin.jansa@lge.com>
2021-03-04 13:53:11 -0800

    webos-image: Update layer pins for 2021-03-04 from dunfell
    oe-core as of 2021-03-04 04:19:50 -1000
    
    dd893279f3 selftest/reproducible: Don't call sync between each
            file compare
    fdb3ff363c qemu: Backport patch to avoid assertion fails on icache
            line size
    48acaa7d53 oeqa/commands: Fix compatibility with python 3.9
    7677ba2599 oe/recipeutils: Fix copying patches when BBLAYERS entries
            are not normalised
    00af6882a2 icu: backport fix for rare random genrb segmentation fault
    341e18ff05 wpebackend-fdo: Fix missing .so symlink when using dev
            package
    6842791f97 package_rpm: Enable use_source_date_epoch_as_buildtime in
            package_rpm class
    69c8987da1 df.py: Add feature check for read-only-rootfs
    cffe25dcbd weston-init: Fix weston-keyboard path in weston.ini
    24afa7308c mtd-utils: Remove duplicate assignments to alternative
            link names
    42f742452d libomxil: Fix up commercial license flag
    3720c39524 npm.bbclass: avoid building target nodejs for native
            npm recipes
    8382f9c1c9 local.conf.sample.extended: prefer INIT_MANAGER
    5b129afb21 local.conf.sample.extended: fix double 'of' typo
    40047178fa groff: Fix determinism issue
    987562f2eb xmlto: Fix reproducibility
    2a50e0e6c2 xorg-minimal-fonts: Really fix determinism
    60c515d84d xorg-fonts-minimal: Fix reproducibility
    d99e2a0c4e watchdog: Avoid reproducibility failures after fixing build
    a272b6e4c0 watchdog: Fix determinism issue from sendmail host path
    670efddd8d vim: Fix a race over creation of the desktop files
    a7e6887d73 vim: Improve determinism
    52658a8df1 cwautomacros: Ensure version is set deterministically
    56d4c28677 oeqa/runlevel : add test for runlevels
    60a9e09529 oeqa: reproducible: Add more logging
    f81fa4cf7f oeqa: reproducible: Fix SSTATE_MIRRORS variable
    fb8063147c buildtools-extended-tarball: Add glibc-gconvs needed
            for build
    a90e10d66b quilt: Be determnistic about column presence
    91363bc050 linux-yocto/5.4: update to v5.4.98
    19698ff8e9 linux-yocto/5.4: update to v5.4.96
    ff2622311e go: update to 1.14.15
    896e7d02e3 sudo: 1.8.31 -> 1.8.32
    9c6131bc46 u-boot: fix CVE-2020-8432 and CVE-2020-10648
    4281c88dd3 cve-check.bbclass: add layer to cve log
    cdbed91b1e cve-check: add CVE_CHECK_REPORT_PATCHED variable to suppress
            reporting of patched CVEs
    7432d1048f cve-check: introduce CVE_CHECK_RECIPE_FILE variable to
            allow changing of per-recipe check file
    b09b9a28f8 pseudo: Update to include fixes for glibc 2.33
    8a21081add pseudo: Update for rename and faccessat fixes
    3bf3de8159 pseudo: Update to work with glibc 2.33
    
    meta-python2 as of 2021-03-03 16:55:18 +0100
    
    ed4876c python: Add fix for CVE-2019-20907
    c6dec34 python: Add fix for CVE-2019-9674

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=fa94374b', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=a8debddd6c', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=dd893279f3', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=346681e7bf', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -70,7 +70,7 @@ webos_layers = [
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=1a4a721', ''),
 ('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=502b81a', ''),
-('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=c96cfe3', ''),
+('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=ed4876c', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=c7ec556', ''),
