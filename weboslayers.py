a37c1067ddfff70363abedd5622d6c9663a86ce7
Martin Jansa <martin.jansa@lge.com>
2021-03-17 16:57:33 -0700

    webos-image: Update layer pins for 2021-03-17 from hardknott
    meta-oe as of 2021-03-17 12:05:44 -0700
    
    a07f3d4591 python3-pastedeploy: Add recipe
    4f06104d01 python3-socketio: Upgrade 5.0.4 -> 5.1.0
    052bec9fc4 python3-twine: Upgrade 3.3.0 -> 3.4.0
    5b17c09df8 python3-defusedxml: Upgrade 0.6.0 -> 0.7.1
    51f0847efd python3-configparser: Upgrade 5.0.1 -> 5.0.2
    e0e9b959fa python3-pulsectl: Upgrade 21.2.0 -> 21.3.4
    5ad7c7fda5 python3-autobahn: Upgrade 21.2.2 -> 21.3.1
    b72263af4f python3-pyflakes: Upgrade 2.2.0 -> 2.3.0
    9a96cb3c46 python3-alembic: Upgrade 1.5.5 -> 1.5.7
    c767715496 python3-bitarray: Upgrade 1.7.0 -> 1.7.1
    a745940c82 python3-pycodestyle: Upgrade 2.6.0 -> 2.7.0
    bc35ce127b python3-luma-core: Upgrade 2.2.0 -> 2.3.1
    2c9000739d python3-configargparse: Upgrade 1.3 -> 1.4
    09fa34b9c7 python3-jsonpointer: Upgrade 2.0 -> 2.1
    593ca57bbc python3-jsonpatch: Upgrade 1.31 -> 1.32
    656e70873b python3-pymisp: Upgrade 2.4.138 -> 2.4.140
    
    meta-virtualization as of 2021-03-17 09:27:54 -0400
    
    46dc036 k8s: update to v1.20.4
    9efb8a0 runc-docker: update to latest -rc93
    
    meta-python2 as of 2021-03-17 11:48:26 +0100
    
    5f4db64 layer.conf: update LAYERSERIES_COMPAT for hardknott
    10c65fc recipes: add conditional PNBLACKLIST in all recipes except
            python-native

--- weboslayers.py
+++ weboslayers.py
@@ -62,20 +62,20 @@ webos_layers = [
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=ecb038f14c', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=36bb7d02a0', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=a07f3d4591', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=master,commit=070d291', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=a57bd00', ''),
-('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=master,commit=0440152', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=46dc036', ''),
+('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=hardknott,commit=5f4db64', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=c7ec556', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=master,commit=master-2021-03-15', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=master,commit=hardknott-2021-03-17', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=fdf02ac', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
