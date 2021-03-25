9aca2e8e78f48a936be232eef575d5e6b4b4bc6c
Martin Jansa <martin.jansa@lge.com>
2021-03-25 01:09:53 -0700

    webos-image: Update layer pins for 2021-03-25 from gatesgarth
    bitbake as of 2021-03-24 22:47:49 +0000
    
    5d02c984 Force parser shutdown after catching an exception
    947c2ad6 runqueue: Add setscene task overlap sanity check
    0a61f86a runqueue: Fix task execution corruption issue

--- weboslayers.py
+++ weboslayers.py
@@ -58,7 +58,7 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.48,commit=7a651724', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.48,commit=5d02c984', ''),
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=gatesgarth,commit=6311cb4930', 'oe-core'),
 
