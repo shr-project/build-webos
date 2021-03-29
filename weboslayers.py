8395849f4b63f72f51b7a8af3ad0d77e4fc73611
Martin Jansa <martin.jansa@lge.com>
2021-03-29 02:40:07 -0700

    webos-image: Update layer pins for 2021-03-29 from hardknott
    oe-core as of 2021-03-28 22:28:09 +0100
    
    c032297695 grub-efi: Re-introduce lost cast to long
    a8a8b7f3db grub2: Disable for RISCV32
    6f2c9602bc packagegroups: delete useless "PROVIDES" lines
    6a13e35723 image-uefi: Set efi_file for rv32/rv64
    ce8eba2636 populate_sdk_ext: Avoid copying and producing .pyc files
    51c3ca662c linux-yocto-rt/5.10: update to -rt34
    59ab12f804 linux-yocto/5.4: update to v5.4.107
    6fa4bbe774 linux-yocto/5.10: update to v5.10.25
    627fb3181e rootfs.py: uninstall the run-postinsts package if not needed
    557d956743 openssl: update to 1.1.1k to fix CVE-2021-3450 and
            CVE-2021-3449
    633393830a bitbake.conf: ensure BUILD_* tools match target tools
    65efd76198 util-linux-libuuid: Simplify recipe and rename from
            util-linux-uuid
    d8451cbef5 libtool: make sure autoheader run before autoconf
    fb7692da7f bitbake.conf: correct description of HOSTTOOLS_DIR
    bbca4d6645 tune-cortexa32: Add hard FPU
    d8f1f3a6b0 classes/image: use oe.utils.directory_size() instead of du
    6ca53ad7b2 lib/oe/utils: add directory size function
    ae9273f7e3 ffmpeg: disable GPL features by default
    29e1e2ad0b initramfs-framework:rootfs: fix some conditional check
    410a45639d documentation-audit.sh: Fix typo in specifying
            LICENSE_FLAGS_WHITELIST
    
    meta-oe as of 2021-03-26 10:34:21 -0700
    
    6e99122ed8 cryptsetup: DEPEND on renamed util-linux-libuuid
    301a0a2cd9 tclap: add pkg-config file
    a81380029e aom: update to 3.0.0
    bfe2d874c6 guider: Upgrade 3.9.7 -> 3.9.8
    4ec462ebc4 python3-docutils: new package
    d0cccb374c python3-djangorestframework: Upgrade 3.12.2 -> 3.12.3
    0715e83336 python3-django: Upgrade 3.1.1 -> 3.1.7
    c0f4406563 python3-flask-sqlalchemy: Upgrade 2.4.4 -> 2.5.1
    b433558aa0 python3-pint: Upgrade 0.16.1 -> 0.17
    a028581d60 python3-pyflakes: Upgrade 2.3.0 -> 2.3.1
    9c3e50528d python3-rfc3339-validator: Upgrade 0.1.2 -> 0.1.3
    74218482b6 python3-alembic: Upgrade 1.5.7 -> 1.5.8
    b5fbc3ebd9 python3-elementpath: Upgrade 2.2.0 -> 2.2.1
    c59a073c39 python3-pytest-helpers-namespace: Upgrade 2019.1.8 ->
            2021.3.24
    a35c8d8c94 python3-backports-functools-lru-cache: Upgrade 1.6.1
            -> 1.6.3
    8b93217eec python3-configshell-fb: Upgrade 1.1.28 -> 1.1.29
    ccb08248e4 python3-jmespath: new package
    da97e1d0af python3-prompt-toolkit: Upgrade 3.0.17 -> 3.0.18
    749f6c2432 python3-lazy-object-proxy: Upgrade 1.5.2 -> 1.6.0
    1d0cdcb288 python3-pyperf: Upgrade 2.1.0 -> 2.2.0
    8de7638b23 python3-croniter: Upgrade 1.0.8 -> 1.0.9
    5c07f4ca2d python3-colorlog: Upgrade 4.7.2 -> 4.8.0
    aa8402007c python3-scrypt: Upgrade 0.8.6 -> 0.8.17
    
    meta-security as of 2021-03-25 18:29:41 +0000
    
    7758709 swtpm: update to 0.5.2
    5e1ae67 tpm2-tss-engine: update 1.1.0
    
    meta-updater as of 2021-03-29 10:00:17 +0200
    
    a8aaf6f Merge pull request #804 from shr-project/jansa/hardknott
    79b0cad layer.conf: update LAYERSERIES_COMPAT for hardknott
    
    meta-virtualization as of 2021-03-26 11:12:29 -0400
    
    438ed8b runx: update to v1.0
    6907613 go-build: export GO111MODULE=off

--- weboslayers.py
+++ weboslayers.py
@@ -60,16 +60,16 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.50,commit=ed8e1fd4', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=7ae12e4278', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=c032297695', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=ba2576ed79', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=6e99122ed8', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
-('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=master,commit=070d291', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=46dc036', ''),
+('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=master,commit=a8aaf6f', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=438ed8b', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=hardknott,commit=5f4db64', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
@@ -83,5 +83,5 @@ webos_layers = [
 ('meta-webos-virtualization', 53, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 
 ('meta-webos-smack',          75, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
-('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=master,commit=f9fdf97', ''),
+('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=master,commit=7758709', ''),
 ]
