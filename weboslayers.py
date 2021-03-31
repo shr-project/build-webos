f5ec9795b4717e7a0a32b44146de9e09e27f0e83
Martin Jansa <martin.jansa@lge.com>
2021-03-31 01:56:11 -0700

    webos-image: Update layer pins to use master branch
    meta-oe as of 2021-04-07 07:45:13 -0700
    
    a23fa069d0 networkmanager-openvpn: Fix packageing
    e4ad6f3507 doxygen: Upgrade 1.8.20 -> 1.9.1
    412cb3a15c pidgin-sipe: fix g_memdup2 changes to be backwards
            compatible with glib-1.67
    7e60ff5353 android-tools: use PN instead of BPN in RDEPENDS
    f825ad7d7e libyui(-ncurses): upgrade to 4.1.1, libyui repo was
            rewritten completely
    902919167a libyui: switch to libyui-old repo which still has this
            SRCREV
    08fad9f3a6 libsmi: use /bin/sh instead of ${base_bindir}/sh to silence
            QA error with usrmerge
    555b90779f fluidsynth: update SRC_URI to remove non-existing 2.1.x
            branch
    c6f3bec2e9 poppler: Backport patches to fix build with glib-2.0 2.68+
            and GCC11
    6f2099cf78 gjs: Fix build with gcc11
    ad8c6ba311 pidgin-sipe: Fix build with glib-2.0 >= 2.68
    285e612ac9 libcppkafka: Add initial recipe for cppkafka wrapper
    3e5209b691 librdkafka: Add initial recipe v1.6.1
    cf0b587ddd libmanette: Add recipe
    87932984c3 iwd: Upgade to 1.13
    ccba1eb6a8 uml-utilities: fix installed-vs-shipped with usrmerge
    e721f6d944 telepathy-glib: respect GI_DATA_ENABLED when enabling
            vala-bindings
    5c83f2da63 packagegroup-meta-oe: use 4 spaces for identation
    976c8d3f69 phonet-utils: remove
    3bffa82504 packagegroup-meta-oe: include glfw, icewm, geis only with
            x11 in DISTRO_FEATURES
    ece4e7d4f0 freeradius: Upgrade to 3.0.21
    bd8e72c139 mariadb: Fix build on newer 32bit architectures
    7e64cce442 mariadb: upgrade 10.5.8 -> 10.5.9
    ad0df8ff6f python3-regex: Upgrade 2021.3.17 -> 2021.4.4
    befe8552bd python3-bitarray: Upgrade 1.8.2 -> 1.9.0
    a2964529ec python3-docutils: Upgrade 0.16 -> 0.17
    2a73a50db2 python3-pysonos: Upgrade 0.0.40 -> 0.0.41
    6b98728840 python3-networkx: Upgrade 2.5 -> 2.5.1
    6cfe535f40 python3-aiohttp: Upgrade 3.7.4 -> 3.7.4.post0
    0fc9235bbb python3-pillow: Upgrade 8.1.2 -> 8.2.0
    a112f5ed6c python3-decorator: Upgrade 4.4.2 -> 5.0.1
    8507a0dda6 python3-typeguard: Upgrade 2.11.1 -> 2.12.0
    37ce67f575 python3-contextlib2: Upgrade 0.6.0 -> 0.6.0.post1
    d35780c181 python3-tqdm: Upgrade 4.58.0 -> 4.59.0
    b3b8d4f6a6 python3-pymisp: Upgrade 2.4.140 -> 2.4.141
    4161ee91c1 python3-xlsxwriter: Upgrade 1.3.7 -> 1.3.8
    212c6a5f14 python3-google-api-python-client: Upgrade 2.0.2 -> 2.1.0
    454644c578 libxaw: upgrade 1.0.13 -> 1.0.14
    649fb54245 nodejs: ppc64le machine support
    5f3159f634 zabbix: upgrade 5.2.5 -> 5.2.6
    7d2604942c ostree: upgrade 2020.8 -> 2021.1
    1358f588e3 fbgrab: upgrade 1.4 -> 1.5
    aaef364ac8 emacs: upgrade 27.1 -> 27.2
    66dcc24cd7 snort: upgrade 2.9.17 -> 2.9.17.1
    30a80664e3 networkmanager-openvpn: upgrade 1.8.12 -> 1.8.14
    55669e9214 lldpd: upgrade 1.0.4 -> 1.0.8
    d399c6043c fetchmail: upgrade 6.4.17 -> 6.4.18
    a281dd572c gnome-autoar: upgrade 0.2.4 -> 0.3.1
    56058fe5b9 libnet-ssleay-perl: add rdep on perl-module-autoloader
    9dda6b9608 rsyslog: fix some of the ptests
    285e4c9bfb gpsd: backport d-bus message time patch from upstream
    75ef564e59 python3-cheetah: Upgrade 3.2.6 -> 3.2.6.post1
    f95812d0bc python3-h5py: Upgrade 3.1.0 -> 3.2.1
    8f3ee91cfc python3-pyroute2: Upgrade 0.5.15 -> 0.5.16
    ba370b99af python3-matplotlib: Upgrade 3.3.4 -> 3.4.1
    3e1cedb62a python3-parso: Upgrade 0.8.1 -> 0.8.2
    ce5a70222c python3-httplib2: Upgrade 0.19.0 -> 0.19.1
    db7fca08f3 python3-bitarray: Upgrade 1.8.1 -> 1.8.2
    b1d3eb11b3 python3-sqlalchemy: Upgrade 1.4.3 -> 1.4.4
    
    Signed-off-by: Martin Jansa <martin.jansa@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -58,24 +58,24 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.50,commit=b32e6c8d', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=e70b925b', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=08b2c9a23c', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=14241ed09f', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=hardknott,commit=94fe80ce86', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=5d8dfa1ad4', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=master,commit=6c31579', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=hardknott,commit=38b5761', ''),
-('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=hardknott,commit=810d6d8', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=3124f23', ''),
+('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=master,commit=810d6d8', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=c7ec556', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=master,commit=hardknott-2021-04-19', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=master,commit=master-2021-04-19', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=bb974dc', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
