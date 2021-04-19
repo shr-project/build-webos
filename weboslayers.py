5861edd3976216c8266006ab68e6d5a51e9fe4ef
Martin Jansa <martin.jansa@lge.com>
2021-04-19 01:49:52 -0700

    webos-image: Update layer pins for 2021-04-19 from gatesgarth
    meta-oe as of 2021-04-10 07:37:44 -0700
    
    c9815c86bf telepathy-glib: respect GI_DATA_ENABLED when enabling
            vala-bindings
    1cc2e43550 uml-utilities: fix installed-vs-shipped with usrmerge
    845bd5a5f1 hostapd: fix CVE-2021-0326 and CVE-2021-27803
    04ba527e94 hostapd: fix CVE-2019-5061
    d59f6e6e1b libyui: switch to libyui-old repo which still has this
            SRCREV
    66adbbce87 libsmi: use /bin/sh instead of ${base_bindir}/sh to silence
            QA error with usrmerge
    a5ab08adbc libsmi: set awk as target path
    124319339b fluidsynth: update SRC_URI to remove non-existing 2.1.x
            branch

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=gatesgarth,commit=fdae970656', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=gatesgarth,commit=317c2dd540', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=gatesgarth,commit=c9815c86bf', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
