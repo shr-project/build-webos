7b47b576db507668b659b7db03e34064c41cff15
Martin Jansa <martin.jansa@lge.com>
2021-04-19 01:52:42 -0700

    webos-image: Update layer pins for 2021-04-19 from hardknott
    oe-core as of 2021-04-16 13:35:54 +0100
    
    08b2c9a23c oeqa/selftest: Hardcode test assumptions about heartbeat
            event timings
    aeae9467af oe-time-dd-test.sh: provide more information from "top"
    d58d5ce00a oe-time-dd-test.sh: make executable
    
    bitbake as of 2021-04-15 15:01:47 +0100
    
    b32e6c8d bitbake-user-manual: add REQUIRED_VERSION and adjust
            PREFERRED_VERSION entry
    082683da bitbake-user-manual: document no support for using passwords
            in git URLs
    
    meta-raspberrypi as of 2021-04-12 17:11:03 +0100
    
    bb974dc userland: Correct PV after SRCREV update
    
    meta-security as of 2021-04-14 13:49:47 +0000
    
    ca9264b lkrg-module: Add Linux Kernel Runtime Guard
    879330a clamav: remove rest of mirror.dat ref
    5f07a3d Clearly define clang toolchain in Parsec recipes
    1b796b3 gitlab-ci: fine tune order
    9286904 kas-security-base.yml: tweek build vars
    6f763e6 .gitlab-ci.yml: reorder to speed up builds
    076e75d initramfs-framework-ima: introduce IMA_FORCE
    269cd6a Define secure images with parsec-service and parsec-tool
            included and add the images into gitlab CI
    409a8d4 Add meta-parsec layer into meta-security.
    6ad6bb0 README: cleanup
    aebcf9a layer.conf: add dynamic-layer for rust pkg
    aa6d847 suricata: update to 6.0.2
    f935958 Use libest "main" branch instead of "master".
    a00b285 python3-suricata-update: update to 1.2.1
    0f79f5a swtpm: fix check for tscd deamon on host
    
    meta-virtualization as of 2021-04-18 22:31:14 -0400
    
    38b5761 xen-tools: fix 32-bit x86 build by setting pvshim configure
            flags
    dec3c21 xtf-image: add a new image for the Xen Test Framework
    6e05877 xtf: introduce recipe for the Xen Test Framework
    ac27f1b xen, xen-tools: Document the version update process in README
    cb962b9 xen, xen-tools: uprev git recipes after 4.15 release
    e151dcd xen, xen-tools: add recipes for new stable Xen 4.15 release
    c090201 lxc: add upstream-status to patch
    12fc14e lxc-recipe: Fix compilation without seccomp when libseccomp
            is installed
    1f9a36f python3-boto3: Upgrade 1.14.7 -> 1.17.48
    3939517 python3-dotenv: Upgrade 0.13.0 -> 0.17.0
    0099422 python3-newrelic: Upgrade 6.0.1.155 -> 6.2.0.156
    91dc9ce python3-webob: Upgrade 1.8.5 -> 1.8.7
    d4025d3 python3-paramiko: Upgrade 2.7.0 -> 2.7.2
    cbec124 crun: bump to latest
    ff18ca2 ipset: drop recipe

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.50,commit=e70b925b', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.50,commit=b32e6c8d', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=14241ed09f', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=08b2c9a23c', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=hardknott,commit=94fe80ce86', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -69,7 +69,7 @@ webos_layers = [
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=master,commit=6c31579', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=3124f23', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=hardknott,commit=38b5761', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=hardknott,commit=810d6d8', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
@@ -77,11 +77,11 @@ webos_layers = [
 
 ('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=master,commit=hardknott-2021-04-19', ''),
 
-('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=f9a13f3', ''),
+('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=bb974dc', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-updater',        52, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-virtualization', 53, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 
 ('meta-webos-smack',          75, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
-('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=master,commit=e70a97b', ''),
+('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=master,commit=ca9264b', ''),
 ]
