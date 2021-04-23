0c105a611cc2790ae61a42346c04c0b7ae34eb09
Martin Jansa <martin.jansa@lge.com>
2021-04-23 12:22:05 -0700

    webos-image: Update layer pins for 2021-04-23 from master
    meta-oe as of 2021-04-23 08:56:25 -0700
    
    44ca91f0fe gimp: Disable vector icons on musl/x86
    03115dcd5f tinyproxy: upgrade 1.10.0 -> 1.11.0
    4b04112fdd redis: upgrade 6.2.1 -> 6.2.2
    428db530e6 ostree: upgrade 2021.1 -> 2021.2
    fd27d08d47 libwebsockets: upgrade 4.1.6 -> 4.2.0
    1dc5835403 libdvdread: upgrade 6.1.1 -> 6.1.2
    56782012e3 networkmanager: upgrade 1.30.2 -> 1.30.4
    29f4f5a166 bearssl: Update to tip of master

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=ca72375a3b', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=239a3a3a3a', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=44ca91f0fe', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
