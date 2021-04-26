d19dbb470010d06201cdb22fb7d8f6b48a6eebfd
Martin Jansa <martin.jansa@lge.com>
2021-04-26 00:10:33 -0700

    webos-image: Update layer pins for 2021-04-26 from master
    oe-core as of 2021-04-24 17:53:01 +0100
    
    bf1b467dac yocto-check-layer: Avoid bug when iterating and autoadding
            dependencies
    2bb146e731 libssh2: fix build failure with option no-ecdsa
    23be2a27e1 pyyaml: Add missing HOMEPAGE
    0a8600f9ce python3-pyyaml: Add recipe
    c983359eae python3-jinja2: Enable ptests
    48c83fc114 python3-markupsafe: Enable ptests
    757d87f676 python3-docutils: Upgrade to 0.17.1
    a6690c2295 gcr: fix one parallel build failure
    1330ac1902 unzip: use optimization from bitbake
    547ac986a7 qemu: fix CVE-2021-20257
    e2b5bc11d1 qemu: fix CVE-2021-3416
    e2fb8c15a6 qemu: fix CVE-2021-3409
    59a44f8c70 qemu: fix CVE-2021-20221
    481e012de8 qemu: fix CVE-2020-29443
    c2f79065ef qemu: fix CVE-2021-20181
    cf0e3397d3 kernel-yocto.bbclass: chdir to ${WORKDIR} for
            do_kernel_checkout
    0fb6280432 cmake.bbclass: remove ${B} before cmake_do_configure
    abaaf8c6bc lz4: use CFLAGS from bitbake
    f2d1523b19 perl: fix startperl configuration option for perl-native
    
    meta-oe as of 2021-04-23 21:41:26 -0700
    
    440f11c497 nodejs: Enable snapshot
    cb1cf05378 zram: add support for mem_limit
    a7d0d87854 nss: upgrade 3.63 -> 3.64
    7bc8f7d2d1 gd: Replace deprecated types from tiff
    6710d0bba5 gvfs: rdepend on gsettings-desktop-schemas

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=784a904f', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=ca72375a3b', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=bf1b467dac', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=44ca91f0fe', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=440f11c497', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
