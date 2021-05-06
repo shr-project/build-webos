0158ebe89ccc6005acd44f4ba2b78adec1400a27
Martin Jansa <martin.jansa@lge.com>
2021-05-06 11:05:30 -0700

    webos-image: Update layer pins for 2021-05-06 from gatesgarth
    meta-oe as of 2021-04-29 21:41:27 -0700
    
    70ee729301 nodejs: 12.20.2 -> 12.21.0
    4565a84ab6 nodejs: 12.20.1 -> 12.20.2
    cc88a8ff4e packagegroup-meta-webserver: remove nostromo from pkg grp
    2d1f84f978 nostromo: Blacklist and exclude from world builds

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=gatesgarth,commit=d47b7cdc35', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=gatesgarth,commit=29ca52c782', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=gatesgarth,commit=70ee729301', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
