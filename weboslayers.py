ab613ffe0e4bfaade4e0fbf43271bddacd05a6b6
Martin Jansa <martin.jansa@lge.com>
2021-05-06 01:58:41 -0700

    webos-image: Update layer pins for 2021-05-06 from master
    oe-core as of 2021-05-06 08:41:21 +0100
    
    4b04153a7d boost: correct upstream version check
    62c94bb925 sanity.bbclass: mention CONNECTIVITY_CHECK_URIS in network
            failure message
    15c65f90a3 classes/image: Use xargs to set file timestamps
    0e9850486b Revert "oeqa: Set LD_LIBRARY_PATH when executing native
            commands"
    87884d9938 diffoscope: add native libraries to LD_LIBRARY_PATH
    395812c345 binutils: Fix CVE-2021-20197
    3301536f82 bitbake.conf: sort MIRROR list, add missing SAMBA_MIRROR
    a0257aee7d qemu: fix CVE-2021-3392
    2e7494cd38 Revert "qemu: fix CVE-2021-3392"
    853eb9a9c4 strace: Upgrade to 5.12
    ace7a71b09 xwayland: remove protocol.txt - it clashes with xserver-xorg
    a48178f6d0 oe-buildenv-internal: add BitBake's library to PYTHONPATH
    b0aedd74f1 ccache: add packageconfig docs option
    231c99728d Revert "perl: fix startperl configuration option for
            perl-native"
    fdeee94fa7 busybox: Fix reproducibility
    44befb922e gstreamer1.0: update ptest patch
    793b6f72e5 gstreamer1.0: rename patches
    1cb679e6a4 ptest-runner: libgcc must be installed for pthread_cancel
            to work
    7ad71de89d qemu: fix CVE-2021-20263
    df92b33597 qemu: fix CVE-2020-27821
    667352cc46 linux-yocto/5.4: update to v5.4.116
    2cfc4489c1 linux-yocto/5.10: update to v5.10.34
    a24b865136 linux-yocto/5.4: update to v5.4.114
    7eccb9c0c2 perf: fix python-audit RDEPENDS
    91fcd09461 linux-yocto/5.10: update to v5.10.32
    894f5328d3 linux-yocto/5.10: qemuriscv32.cfg: RV32 only supports 1G
            physical memory
    c290adec4e linux-yocto/5.10: aufs fixes
    ed270bd146 libubootenv: upgrade 0.3.1 -> 0.3.2
    1d9d38eb6b yocto-uninative: Update to 3.1 which includes a patchelf fix
    e64bf999a9 gcc: Upgrade to GCC 11
    7883b4c138 libgcc-initial: Do not build fp128 to decimal ppc functions
    1e5b73f068 webkitgtk: Update patch status
    8f08ca440b webkitgtk: Fix reproducibility in minibrowser
    6a6c64426f busybox: Enable long options for enabled applets
    3d949f286c cml1.bbclass: Return sorted list of cfg files
    
    meta-oe as of 2021-05-03 22:57:42 -0700
    
    4fe1065655 emacs: Do not use SIGSTKSZ
    422f18343b libowfat: Replace __pure__ with pure and remove using
            __deprecated__
    564f721954 packagegroup-meta-oe: conditional remove tbb for powerpc
    7178a18131 dibbler: Add libpthread to linker flags
    8105f98ad6 ctags: Switch to universal ctags
    fec057d6d0 libupnp: Upgrade to 1.14.6
    0a0894b78e ufw: bump version to 0.36 and add services
    17f43204f2 mutter/wayland: replace xserver-xorg-xwayland by xwayland
            in rdep
    df10c7bcc0 zsh: reduce priority slightly to avoid conflict with bash
    b8bbcd4dea python3-m2crypto: Upgrade to 0.37.1

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=2db57132', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=df540a630f', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=4b04153a7d', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=57baca4bc1', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=4fe1065655', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
