f5ddf9796eed0146a8aa8b868b673e3de26e93cf
Martin Jansa <martin.jansa@lge.com>
2021-05-06 11:05:56 -0700

    webos-image: Update layer pins for 2021-05-06 from hardknott
    meta-oe as of 2021-04-29 18:57:25 -0700
    
    c3a36263f9 packagegroup-meta-webserver: remove nostromo from pkg grp
    c4a4070a46 nostromo: Blacklist and exclude from world builds
    ef6adcba6f zram: add support for mem_limit
    981a6b3cb1 nss: upgrade 3.63 -> 3.64
    88db66c3e1 gvfs: rdepend on gsettings-desktop-schemas
    8dcdded940 gimp: Disable vector icons on musl/x86
    085332dcd2 redis: upgrade 6.2.1 -> 6.2.2
    25532d82a7 libdvdread: upgrade 6.1.1 -> 6.1.2
    67eaaf8548 networkmanager: upgrade 1.30.2 -> 1.30.4
    3fe42a6805 python3-hyperframe: Upgrade 6.0.0 -> 6.0.1
    ba22785a42 python3-asttokens: Upgrade 2.0.4 -> 2.0.5
    2eb29a3805 python3-pysonos: Upgrade 0.0.42 -> 0.0.43
    ab71ab067d tbb: Re-introduce PE
    601c080499 freeradius: check existence of openssl's commands in
            bootstrap

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=940111cefa', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=hardknott,commit=71b546ed85', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=hardknott,commit=c3a36263f9', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
