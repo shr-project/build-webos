cfb5bb490eb58756cfefdbc3769580959ea05f99
Martin Jansa <martin.jansa@lge.com>
2021-05-28 02:09:32 -0700

    webos-image: Update layer pins for 2021-05-28 from master
    oe-core as of 2021-05-28 06:40:30 +0100
    
    7beaae812f flex: correct license information
    706bdcaec5 expat: set CVE_PRODUCT
    ad19d41cbd bind: upgrade 9.16.15 -> 9.16.16
    2ad815dbaf oeqa/runtime/rpm: Drop log message counting test component
    f6093c2fb6 libucontext: upgrade 0.10 -> 1.1
    a9524b680f libtest-needs-perl: upgrade 0.002006 -> 0.002009
    3228a011c9 libedit: upgrade 20210419-3.1 -> 20210522-3.1
    1de8a2a93b stress-ng: upgrade 0.12.08 -> 0.12.09
    dabd47e2f7 logrotate: upgrade 3.18.0 -> 3.18.1
    4ff5f3234c python3-pytest: upgrade 6.2.3 -> 6.2.4
    2165dc8b7f python3-more-itertools: upgrade 8.7.0 -> 8.8.0
    2fab69a424 python3-markupsafe: upgrade 2.0.0 -> 2.0.1
    cebb8e0e9d python3-libarchive-c: upgrade 2.9 -> 3.0
    592a2f1e95 distro/maintainers: Fix up the ptest image entries
    2db500793d images: add ptest images based on core-image-minimal
    0d5bd979a9 images: remove sato/weston ptest images
    65e9606bae apt: Add missing <array> header
    42016f34ae icecc-create-env: Silence warning: invalid ICECC_ENV_EXEC
    720254152c icecc: Demote "could not get ICECC_CC" warning to note
    a0e8695149 Revert "icecc: Don't use icecc when INHIBIT_DEFAULT_DEPS
            is set"
    
    meta-raspberrypi as of 2021-05-26 10:00:04 +0100
    
    faf3f33 layer.conf: Define LAYERDEPENDS

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=74042b5b', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=2cda6242f2', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=7beaae812f', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=aace2a3b91', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=master,commit=master-2021-05-27', ''),
 
-('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=11209a4', ''),
+('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=faf3f33', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-updater',        52, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-virtualization', 53, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
