7a9c8cf83888534e6fd2f22dc98e302f9fdb571a
Martin Jansa <martin.jansa@lge.com>
2021-06-07 00:50:32 -0700

    webos-image: Update layer pins for 2021-06-07 from hardknott
    oe-core as of 2021-06-02 14:26:58 +0100
    
    6ca1047e98 oeqa/runtime/rpm: Drop log message counting test component
    959e1faa91 package_rpm: pass XZ_THREADS to rpm
    07f223048a pkgconfig: update SRC_URI
    f5c5763ae5 flex: correct license information
    30357a56df expat: set CVE_PRODUCT
    7c39b71b78 curl: fix CVE-2021-22876
    b11dc35cce curl: fix CVE-2021-22890
    e3ded54f9f qemu: Exclude CVE-2020-3550[4/5/6] from cve-check
    5e1a46f082 bind: upgrade 9.16.15 -> 9.16.16
    bceca3c36e bind: upgrade 9.16.13 -> 9.16.15
    342cdbc067 bind: upgrade 9.16.12 -> 9.16.13
    1447726356 xinetd: Exclude CVE-2013-4342 from cve-check
    4a70af7b89 cve-extra-exclusions.inc: add exclusion list for intractable
            CVE's
    a1130182a0 grub: Exclude CVE-2019-14865 from cve-check
    07451418e8 grub2: Add CVE whitelist entries for issues fixed in 2.06
    4acd52e211 lib/oe/gpg_sign.py: Fix gpg verification
    01a9358abe sstate: Ignore sstate signing key
    146f04f9d3 boost: fix do_fetch failure
    179a912bf1 baremetal-image: Fix post process command
            rootfs_update_timestamp
    622b1a409a ltp: Disable problematic tests causing autobuilder hangs
    e1e04de65e libxml2: Fix CVE-2021-3541
    6c59d33ee1 libxml2: Fix CVE-2021-3518
    
    meta-oe as of 2021-05-23 08:29:00 -0700
    
    9ee0e08ba2 libgtop: fix do_compile error
    280a7a1692 libgtop: tidy up recipe
    0567340210 opencv: Disable tbb on riscv/musl
    5aa0e96d7a postgresql: upgrade 13.2 -> 13.3
    15938ed77b trace-cmd: Conflict resolution
    bf945de831 sysdig: disable building for ppc
    c7c0a292cf opencv: remove tbb packageconfig for powerpc
    26c88bf3eb hostapd: fix building with CONFIG_TLS=internal
    4210e9cb2a libsdl: Fix CVE-2019-13616
    93a33c9611 exiv2: Fix CVE-2021-29473
    ce025415dd exiv2: Fix CVE-2021-29470
    11ef1c6cb5 exiv2: Fix CVE-2021-29464
    d3f0f8957f exiv2: Fix CVE-2021-3482
    0e8fcf0e77 exiv2: Fix CVE-2021-29463
    8355be5c64 exiv2: Fix CVE-2021-29458
    52455c3047 exiv2: Fix CVE-2021-29457
    a1ae03d096 uftrace: Fix a plthook crash on aarch64 with binutils2.35.1
            and later versions on aarch64
    9e5c4181ec python3-pymongo: Upgrade 3.11.3 -> 3.11.4
    cc118b56ad python3-rfc3339-validator: Upgrade 0.1.3 -> 0.1.4
    b318670297 python3-django: upgrade 3.2 -> 3.2.2
    f3758cb444 python3-django: upgrade 2.2.20 -> 2.2.22
    d555451d10 python3-robotframework: Upgrade 4.0.1 -> 4.0.2
    3863aba2ac python3-cerberus: Upgrade 1.3.3 -> 1.3.4

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.50,commit=0c83e98b', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=6d69f7453f', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=hardknott,commit=6ca1047e98', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=hardknott,commit=cf5bd6a830', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=hardknott,commit=9ee0e08ba2', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
