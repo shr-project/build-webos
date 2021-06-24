45faa181b99cae023e2511feadfc0e990184b9b0
Martin Jansa <martin.jansa@lge.com>
2021-06-24 03:00:00 -0700

    webos-image: Update layer pins for 2021-06-24 from master
    oe-core as of 2021-06-22 15:17:10 +0100
    
    8bc339b83a populate_sdk_ext: copy BBMULTICONFIG files
    db2b1da511 uboot-sign.bbclass: fix some install commands
    8628a276a0 u-boot-tools: fix a mkimage signature issue
    d5d0f173be coreutils: remove NOSTAT_LEAF_OPTIMIZATION
    40f09e184a qemurunner: add second qmp port
    6196a785ea sdk: Enable do_populate_sdk with multilibs
    fbdc442236 gcc: Apply multilib fix to ARC as well
    e6e3570d9d mesa: upgrade 21.1.2 -> 21.1.3
    
    meta-oe as of 2021-06-23 10:45:31 -0700
    
    a976968c10 packagegroup-meta-python: Add newly added recipes
    b32b434724 meta-python: Add python3-fastjsonschema and its dependencies
    fa2d3338fb python3-django: upgrade 2.2.23 -> 2.2.24
    0a14d9cea6 iwd: update to 1.15
    9b6333a554 python3-mypy: Upgrade 0.812 -> 0.902
    39f4be7829 python3-prompt-toolkit: Upgrade 3.0.18 -> 3.0.19
    ea3d3f9b2c python3-elementpath: Upgrade 2.2.2 -> 2.2.3
    2ac52ca61c python3-fasteners: Upgrade 0.16.1 -> 0.16.3
    1d3302a325 python3-cmd2: Upgrade 1.5.0 -> 2.1.1

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=master,commit=398a1686', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=5fdc061856', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=master,commit=8bc339b83a', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=ab28d08793', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=a976968c10', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
