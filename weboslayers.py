7e8616b4c19f6bb3b7f3a7d0f19968a68bd2ccb5
Martin Jansa <martin.jansa@lge.com>
2021-07-29 06:12:41 -0700

    webos-image: Update layer pins for 2021-07-29 from dunfell as in WRN-3779 from build 3xx
    oe-core as of 2021-07-20 06:36:58 -1000
    
    9ae339ace9 kernel-devsrc: fix 32bit ARM devsrc builds
    6bee4dbf37 linux-yocto/5.4: update to v5.4.132
    8c3a582b00 linux-yocto/5.4: update to v5.4.131
    
    meta-oe as of 2021-07-26 16:05:08 -0700
    
    85f8047c71 php: Upgrade to 7.4.16
    11c1cd6bf5 php: allow php as empty
    9982d7f277 php: split out phpdbg into a separate package
    a420980c4f wireshark: update to 3.2.15
    695c9cce6c ostree: Do not check for meta-python
    d34d040fda python3-{pyyaml,cython,pyparsing}: move from meta-python
            to meta-oe
    8bdc784831 python3-jinja2: remove recipe
    b6123048db python3-markupsafe: remove recipe
    9bd8d14ef2 libdevmapper,lvm2: Do not inherit license
    da09c4c743 ufw: backport patches, update RRECOMMENDS, python3 support,
            tests
    5eff5262e3 hiawatha: fix url.
    3434c737b6 mariadb: update to 10.4.20
    09825964eb vboxguestdrivers: add a fix for build failure with
            kernel 5.13
    9b7a52e06b vboxguestdrivers: upgrade 6.1.20 -> 6.1.22
    17828d03d6 vboxguestdrivers: upgrade 6.1.18 -> 6.1.20
    12bc39d8c5 vboxguestdrivers: Add __divmoddi4 builtin support
    4435dfaa9e vboxguestdrivers: Add patch proposed upstream to fix a
            build failure on i386
    c67ddfd590 vboxguestdrivers: upgrade 6.1.16 -> 6.1.18
    9c33c42196 vboxguestdrivers: fix build against kernel v5.10+
    0066ffb6eb vboxguestdrivers: upgrade 6.1.14 -> 6.1.16
    5d3ac060df vboxguestdrivers: upgrade 6.1.12 -> 6.1.14 Drop kernel
            5.8 compatibility patch, now part of upstream codebase
    155c453355 vboxguestdrivers: Fix build with kernel 5.8
    8d62c9d4c9 vboxguestdrivers: fix failed to compile with kernel 5.8.0
    2fe2ea3f15 vboxguestdrivers: upgrade 6.1.6 -> 6.1.12
    10082fce3b postgresql: update to 12.7
    13ceac25a8 sysprof: Enable sysprofd/libsysprof only when polkit in
            DISTRO_FEATURES
    ff470b3e85 tracker-miners: Check for commercial license to enable
            ffmpeg
    65c7872a3f nss: add CVE-2006-5201 to allowlist
    5c1356a1ec ntp: fix ntpdate to wait for subprocesses
    aeae0a34cf apache2: fix CVE-2020-13950 CVE-2020-35452 CVE-2021-26690
            CVE-2021-26691 CVE-2021-30641
    d9c8c33db8 nginx: fix CVE-2021-23017
    
    meta-virtualization as of 2021-07-23 12:56:13 -0400
    
    7588686 uxen-guest-tools: inherit dos2unix to fix do_patch failure

--- weboslayers.py
+++ weboslayers.py
@@ -60,23 +60,23 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=0e0af15b', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=9b83aefa9c', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=9ae339ace9', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=7bd47ef6c9', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=85f8047c71', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=5d49b28', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=4d6216a', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=7588686', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=b901080', ''),
 
 ('meta-qt6',                  20, 'git://code.qt.io/yocto/meta-qt6.git',                    'branch=6.2,commit=9a4453a', ''),
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 
-('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2021-07-13', ''),
+('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=dunfell,commit=dunfell-2021-07-26', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=59c2d6f', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
