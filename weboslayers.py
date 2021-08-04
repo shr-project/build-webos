d9a4c3bd5aaeca97db64e8aa3bdb01812045ee9d
Martin Jansa <martin.jansa@lge.com>
2021-08-04 04:05:53 -0700

    weboslayers.py: Use master-next branch of meta-virtualization with overrides syntax changes
    meta-virtualization as of 2021-08-02 23:36:31 -0400
    
    96712be image-spec: explictly reference main branch
    9d848a2 conf: set compatibility to honister
    f72770a docker-distribution: manually finish override syntax conversion
    1b6e39e global: overrides conversion pass 2
    d876cfc global: overrides syntax conversion

--- weboslayers.py
+++ weboslayers.py
@@ -69,7 +69,7 @@ webos_layers = [
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/shr-project/meta-updater.git',          'branch=jansa/syntax,commit=cfff200', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=5fdf66c', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master-next,commit=96712be', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=master,commit=f522fa7', ''),
 
 ('meta-qt6',                  20, 'git://github.com/shr-project/meta-qt6.git',              'branch=jansa/6.2-9a4453a-overrides-syntax,commit=c9e58d2', ''),
