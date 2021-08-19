6ce6deb9253a506dcc35145e9c2c080c90c00560
Martin Jansa <martin.jansa@lge.com>
2021-08-19 05:24:44 -0700

    prerequisites.sh: Add file, liblz4-tool and zstd (v2.2.6)
    :Release Notes:
    liblz4-tool and zstd are required by:
    https://git.openembedded.org/openembedded-core/commit/?id=58f5389e3c6b0e5b29df8850c2056b5fc41b618f
    
    file is not new, but with Ubuntu 21.10 it's not installed
    by default in small ubuntu:impish Docker image.
    
    :Issues Addressed:
    [WRN-5295] CCC: Upgrade to latest revisions from Yocto 3.1 Dunfell
    
    Change-Id: If749b5cf4d410599cf4fa0d1fbdf008fcda7a46f

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -18,7 +18,7 @@
 
 check_sanity=true
 usage="$0 [--help|-h] [--version|-V] [--force|-f]"
-version="2.2.5"
+version="2.2.6"
 statusfile="/etc/webos.prerequisites"
 
 for i ; do
@@ -124,16 +124,18 @@ essential="\
     chrpath \
     cpio \
     diffstat \
+    file \
     gawk \
     git \
     iputils-ping \
+    liblz4-tool \
     locales \
-    lsb-release \
     python \
     python2.7 \
     python3 \
     texinfo \
     wget \
+    zstd \
 "
 
 # add python3-distutils only for 18.04 and 20.04 (and later should be added for newer)
