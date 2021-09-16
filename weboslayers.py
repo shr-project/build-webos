3f316225a9f17e2ce5bd279e5a73534f13667008
Martin Jansa <martin.jansa@lge.com>
2021-09-16 03:08:08 -0700

    webos-image: Update layer pins for 2021-09-16 as in [WRN-5295]
    oe-core as of 2021-09-15 18:34:19 +0100
    
    c7d2281eb6 build-appliance-image: Update to dunfell head revision
    49ca1f62cc build-appliance-image: Update to dunfell head revision
    1eaac89b03 go: Several Security fixes
    ee4a4f9053 xserver-xorg: Security fix for CVE-2020-14360/-25712
    9dfc6abbb8 go: Exclude CVE-2021-29923 from report list
    b939b005b0 flex: Add CVE-2019-6293 to exclusions for checks
    4cebabf100 dbus: upgrade 1.12.18 -> 1.12.20
    6c90e4dd59 dbus,dbus-test: Move common parts to dbus.inc
    d747c666ed dbus-test: Remove EXTRA_OECONF_X configs
    296746f58d dbus-test: upgrade 1.12.16 -> 1.12.18
    7337d7e4fa dbus: upgrade 1.12.16 -> 1.12.18
    
    meta-oe as of 2021-09-10 15:16:48 -0700
    
    2e7e98cd0c dnsmasq: Security fix CVE-2021-3448
    06d80777f4 krb5: fix CVE-2021-36222
    892b724cd1 stunnel: upgrade 5.56 -> 5.57

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=c2a3bda3', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=ed52bea54f', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=c7d2281eb6', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=5c347d8ce4', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=2e7e98cd0c', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
