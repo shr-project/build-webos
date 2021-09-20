4aa58560fd5735e29ab36b754fc493c008a12aeb
Martin Jansa <martin.jansa@lge.com>
2021-09-20 07:33:00 -0700

    webos-image: Update layer pins for 2021-09-20 from Honister
    oe-core as of 2021-09-19 11:33:03 +0100
    
    46575e0d5b rust-common.bbclass: make sure ccache exist
    1753a8d8fd cpan-base.bbclass: use raw string for regexp
    d1d260dd2d wic: keep rootfs_size as integer
    01152c9410 meta: correct collections vs collections.abc deprecation
    e9715d4234 rng-tools: add systemd-udev-settle wants to service
    9611586531 machine/qemuarm*: use virtio graphics
    6450138afe core-image-sato: Fix runqemu error for qemuarmv5
    83169c33f7 Update mailing list address
    b98c739aa1 tcf-agent: Move to the latest master version
    9b5b797b0e expat: pull from github releases
    b1821691c2 ffmpeg: fix LICENSE
    
    bitbake as of 2021-09-18 18:47:09 +0100
    
    f9cddaee cookerdata: Show a readable error for invalid multiconfig name
    bc43fbb8 bitbake: enable python warnings at the first opportunity
    0f35dc4d fetch2/s3: allow to switch profile from environment variable
    aff52fe2 bitbake: correct deprecation warning in process.py
    8d3c6cbb bitbake: adjust parser error check for python 3.10
            compatibility
    76c9030d bitbake: do not import imp in layerindexlib
    660e6ad4 bitbake: fix regexp deprecation warnings
    ae219e1f bitbake: correct the collections vs collections.abc
            deprecation
    62d96ea2 bitbake: drop old rules for python warnings
    3250bc95 fetch2/git: Avoid races over mirror tarball creation
    ed7e2da8 runqueue/knotty: Improve UI handling of setscene task counting
    
    meta-oe as of 2021-09-17 08:39:00 -0700
    
    e7251cf6ba gdm: Add polkit to required distro features
    b23770aa9f pahole: call python via env in the shebang
    0f21174c99 python3-pulsectl: upgrade 21.5.18 -> 21.9.1
    a6b48db6fe python3-pandas: upgrade 1.3.2 -> 1.3.3
    c28aa6bab6 python3-openpyxl: upgrade 3.0.7 -> 3.0.8
    01b96e4b8b python3-grpcio: upgrade 1.39.0 -> 1.40.0
    750f28d538 python3-grpcio-tools: upgrade 1.39.0 -> 1.40.0
    8f87c3d3c2 python3-decorator: upgrade 5.0.9 -> 5.1.0
    c06d4366e6 python3-bitarray: upgrade 2.3.3 -> 2.3.4
    50b9128ed0 python3-beautifulsoup4: upgrade 4.9.3 -> 4.10.0
    
    meta-raspberrypi as of 2021-09-17 11:36:53 +0100
    
    5f72ec0 raspberrypi-firmware: Update to latest
    937ca69 linux-raspberrypi: Update to 5.10.63
    
    meta-security as of 2021-09-16 21:45:01 -0700
    
    de6712a tpm-quote-tools: Update SRC_URI
    
    meta-virtualization as of 2021-09-17 08:27:13 -0400
    
    1fae692 Revert "ceph/libibverbs: replace libibverbs with rdma-core"
    807506c lxc: Fix -c command for lxc-attach
    0644e80 libvirt: fix CVE-2021-3631
    e250a89 ovs: add upstream-status to CVE patch
    15388c4 openvswitch: fix CVE-2021-36980
    47f9bda lxc: update to 4.0.10 and switch to git
    ddd00d3 python3-dtc: update BSD License to more specific BSD-2-Clause
    513f059 ceph/libibverbs: replace libibverbs with rdma-core
    36cf756 python3-sphinx: add specific Licenses
    6fed58d python3-sphinx: update to 4.2.0
    fc7fb6f grpc-go: update license to Apache-2.0
    852ae69 grpc-go: update to 1.4.0
    4e36360 singularity: update generic "BSD" license specification
    fe37e0a spf13-pflag: update license to specific BSD clause
    98c8cbc spf13-pflag: update to 1.0.4
    cafbf24 go-errors: update license to precise BSD variant
    0be39c0 go-errors: update to 0.8.1

--- weboslayers.py
+++ weboslayers.py
@@ -58,29 +58,29 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.52,commit=7e0af70f', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.52,commit=f9cddaee', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=honister,commit=7b7dfbfaed', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=honister,commit=46575e0d5b', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=5eb46e36a6', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=e7251cf6ba', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=master,commit=f2e740c', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=88a8ccb', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=master,commit=1fae692', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=honister,commit=f522fa7', ''),
 
 ('meta-qt6',                  20, 'git://github.com/shr-project/meta-qt6.git',              'branch=jansa/6.2-3b3572a-overrides-syntax,commit=fc3471b', ''),
 
 ('meta-webos',                40, 'git://github.com/shr-project/meta-webosose.git',         'branch=honister,commit=honister-2021-09-21', ''),
 
-('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=a6fa6b3', ''),
+('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=master,commit=5f72ec0', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-updater',        52, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 ('meta-webos-virtualization', 53, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
 
 ('meta-webos-smack',          75, 'git://github.com/shr-project/meta-webosose.git',         '', ''),
-('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=master,commit=b233768', ''),
+('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=master,commit=de6712a', ''),
 ]
