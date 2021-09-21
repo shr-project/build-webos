a39f035a313c987f2b62c69861d17ae2a6cd274e
Martin Jansa <martin.jansa@lge.com>
2021-09-21 09:23:29 -0700

    webos-image: Update layer pins for 2021-09-21 from master
    meta-oe as of 2021-09-20 08:43:37 -0700
    
    aee2f10638 python3-flask-bootstrap: Update LICENSES
    0f65032120 python3-send2trash: set precise BSD license
    7778fd743d python3-jdcal: set precise BSD license
    1b3828ccb2 python3-kiwisolver: set precise BSD license
    e8225d5668 python3-gnupg: set precise BSD license
    f458d300d8 python3-aenum: set precise BSD license
    a1e8a2d680 python3-geojson: set precise BSD license
    c9a59786bd python3-pyperclip:set precise BSD license
    a8f0d90dc9 python3-webencodings: set precise BSD license
    af46e0d4bf python3-sqlparse: set precise BSD license
    a998097ba0 python3-sympy: set precise BSD license
    bc614d67f0 python3-mpmath: set precise BSD license
    60490f912b python3-ipython:set precise BSD license
    352707a9b4 python3-parallax: set precise BSD license
    52cc5505eb python3-traitlets: set precise BSD license
    5eb413fa00 python3-ipython-genutils: set precise BSD license
    b824795302 python3-dill: set precise BSD license
    34fe7c761a python3-cycler: set precise BSD license

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=honister,commit=46575e0d5b', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=e7251cf6ba', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=aee2f10638', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
