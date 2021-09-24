8710feb41b786ce35b998cf65ba7f535d19ec4d9
Martin Jansa <martin.jansa@lge.com>
2021-09-24 05:28:34 -0700

    webos-image: Update layer pins for 2021-09-24 from master
    oe-core as of 2021-09-23 22:13:51 +0100
    
    907ebb5bb3 lttng-ust: fix do_compile error when PACKAGECONFIG examples
            is enabled
    414020a9bd oeqa/selftest: Add tests for bitbake shell/python task
            output
    29d285bc07 ssate: Cleanup directtasks handling
    601cee016d sstate: Avoid deploy_source_date_epoch sstate when unneeded
    8047c184f8 sstate: Ensure deploy tasks don't pull in toolchains
    1fdbe55032 staging: Mark deploy an sstate task
    6e2cbfc561 meta: Drop useless class
    030d56e2e8 buildtools-tarball/uninative-tarball/meta-ide-support:
            Drop useless meta class
    41d7f1aa2c populate_sdk_base/images: Drop use of 'meta' class and
            hence do_build dependencies
    938e4dfb68 reproducible_build/package_XXX: Ensure SDE task is in
            dependency chain
    220d29a5ab package_ipk/deb/rpm: Drop recursive do_build task
            dependencies
    29daffc241 siteinfo/autotools: Ensure task checksums reflect site files
    5562342020 image/qemu: Add explict depends for qemu-helper
            addto_recipe_sysroot task
    a5c5dd9229 site: Drop ORBit2 relared cached variables
    587d0b51b0 site: Drop caching libIDL_cv_long_long_format
    eadf7bb172 connman: add CVE_PRODUCT
    d938a74097 scripts/oe-publish-sdk: Disable git gc to avoid build errors
    add36cf853 oeqa/target/ssh: don't assume target_dumper is set
    d7e36d01e8 pybootchart: Avoid divide by zero
    250a3f9a80 libsoup-2.4: remove obsolete intltool dependency
    b67ae98ef8 shared-mime-info: use a more concise description
    72f0e03a69 cross: Drop unused do_install
    0438c8e734 avahi: remove obsolete intltool-native dependency
    1b436e4894 testimage: remove aarch64 xorg exclusion
    7b2dfb4910 qemu-native: add direct dependency on ninja-native and
            meson-native
    ba2587beb2 lttng-tools: Add missing DEPENDS on bison-native
    777d974457 recipes: Add missing pkgconfig inherit
    6f5770b49f sqlite3: Exclude CVE-2021-36690 from cve checks
    24664297ab libgcrypt: Upgrade 1.9.3 -> 1.9.4
    54d3d023ce vim: Backport fix for CVE-2021-3770
    1965b344ab testimage: symlink the task log and qemu console log
            to tmp/log/oeqa
    
    bitbake as of 2021-09-23 12:27:01 +0100
    
    032190aa build: Fix log flushing race
    df89e37c build: Make exception printing clearer
    8966b437 build: Ensure python stdout/stderr is logged correctly
    36f6dce1 tests/fetch2: Use our own git server for dtc test repo
    ea26682b fetch2/svn: Allow peg-revision functionality to be disabled
    ee26e258 data_smart: Don't add None to ExpansionError varlist
    9d84fd55 tests/fetch2: Fix quoting warning
    b3cccaa6 fetch2/git: Use os.rename instead of mv

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.52,commit=f9cddaee', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.52,commit=032190aa', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=honister,commit=46575e0d5b', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=honister,commit=907ebb5bb3', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=master,commit=aee2f10638', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
