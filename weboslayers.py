ee5e8ba0cd1ebd21b808ee53973dbd9b93f19e72
Martin Jansa <martin.jansa@lge.com>
2021-11-03 08:29:09 -0700

    webos-image: Update layer pins for 2021-11-03 from gatesgarth
    bitbake as of 2021-11-03 11:30:53 +0000
    
    44320ce7 fetch/git: Handle github dropping git:// support

--- weboslayers.py
+++ weboslayers.py
@@ -58,7 +58,7 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.48,commit=b81b326d', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.48,commit=44320ce7', ''),
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=gatesgarth,commit=9cce475ffc', 'oe-core'),
 
