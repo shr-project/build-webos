8800ff40e095bd00048aace0867eca09002920de
Martin Jansa <martin.jansa@lge.com>
2021-11-09 02:34:17 -0800

    webos-image: Update layer pins for 2021-11-09 from dunfell
    bitbake as of 2021-11-08 14:05:56 +0000
    
    f1a3e9d2 tests/fetch: Update github urls
    bd8883d7 tests/fetch2: Fix quoting warning
    
    meta-oe as of 2021-11-02 08:34:34 -0700
    
    7889158dcd python3-fasteners: update 0.15 -> 0.16.3
    ca35402be5 keyutils: fix install path
    4b8f554f4d drdb-utils: Define SRCREV_FORMAT
    46eb172b64 grpc: Define SRCREV_FORMAT
    5dd554753d spirv-tools: Define SRCREV_FORMAT
    9298852a91 android-tools: Define SRCREV_FORMAT
    f8a2a7e41a gst-shark: Define SRCREV_FORMAT
    d10d52aef9 pm-qa: fix paths for shell scripts
    6be10fe608 redis: update to 5.0.14
    98ccbca491 Apache: Several CVE fixes
    
    meta-raspberrypi as of 2021-11-09 00:03:42 +0000
    
    2e704f5 99-com.rules: fix error invalid substitution type
    c4d0917 recipes: Update SRC_URI protocols for github
    
    meta-updater as of 2021-11-08 09:23:29 +0100
    
    5546c92 Merge pull request #20 from uptane/github-https-dunfell
    d6344aa aktualizr: use https protocol to fetch it
    
    meta-qt6 as of 2021-11-08 10:24:42 +0000
    
    fb8e8e8 qtbase-native: don't try to find tools
    
    Signed-off-by: Martin Jansa <martin.jansa@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -58,28 +58,28 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=c222eddc', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=f1a3e9d2', ''),
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=6cd21ddc6f', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=814eec96c2', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=7889158dcd', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=dunfell,commit=a3b8398', ''),
+('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=dunfell,commit=5546c92', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=dunfell,commit=180241e', ''),
 ('meta-python2',              17, 'https://git.openembedded.org/meta-python2',              'branch=dunfell,commit=b901080', ''),
 
-('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=181cbf3', ''),
+('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=fb8e8e8', ''),
 
 ('meta-webos-backports-3.2',  33, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=dunfell,commit=dunfell-2021-11-07', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=dunfell,commit=dunfell-2021-11-09', ''),
 
-('meta-raspberrypi',          50, 'https://git.yoctoproject.org/git/meta-raspberrypi',      'branch=dunfell,commit=59c2d6f', ''),
+('meta-raspberrypi',          50, 'https://git.yoctoproject.org/git/meta-raspberrypi',      'branch=dunfell,commit=2e704f5', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-updater',        52, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
