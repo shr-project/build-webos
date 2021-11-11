77cc69a2c69faf01d3989bc6ab439677e05366f4
Martin Jansa <martin.jansa@lge.com>
2021-11-11 08:34:39 -0800

    webos-image: Update layer pins for 2021-11-11 from master
    oe-core as of 2021-11-10 19:26:15 +0000
    
    365dc1471a kernel: provide virtual/kernel for all kernels
    7a7d1eed8e linux-yocto/5.10: update to v5.10.78
    6a5ab16bbb linux-yocto/5.15: update to v5.15.1
    a9ac5a388d linux-yocto/5.14: update to v5.14.17
    d57bc72810 linux-yocto/5.10: update to v5.10.77
    ae32a59571 linux-yocto/5.14: update to v5.14.16
    a784cd98bb linux-yocto-rt/5.15: update to -rt17
    1ca455a98d meta: use ln -rs instead of lnr
    222be29051 vim: add patch number to CVE-2021-3778 patch
    df9dca9fe4 wic: Use os.rename instead of bb.utils.rename
    b493eb4f9a vim: fix CVE-2021-3796, CVE-2021-3872, and CVE-2021-3875
    52b44cb877 msmtp: upgrade 1.8.18 -> 1.8.19
    928afb0d6f libpcre2: upgrade 10.37 -> 10.39
    bdc61a7c56 iw: upgrade 5.9 -> 5.16
    bc2b7decc9 iso-codes: upgrade 4.7.0 -> 4.8.0
    609fd71c5d iproute2: upgrade 5.14.0 -> 5.15.0
    e1bcaabdf7 harfbuzz: upgrade 3.0.0 -> 3.1.1
    70709ff074 squashfs-tools: follow-up fix for CVE-2021-41072
    2968b9f18e ell: upgrade 0.44 -> 0.45
    b9c59b5d2a diffoscope: upgrade 189 -> 190
    77d08d327b btrfs-tools: upgrade 5.14.2 -> 5.15
    09a7d9d8d8 gdb-cross: add dependency on gmp-native
    1833cb0c58 mirrors: Add uninative mirror on kernel.org
    35040bba25 qemu: Fix build on aarch64/musl
    
    bitbake as of 2021-11-10 19:25:21 +0000
    
    8e7f2b65 cooker: Handle parsing results queue race
    edc3b0c3 fetch/wget: Add timeout for checkstatus calls (30s)
    8c31e755 parse/ast: Show warnings for append/prepend/remove operators
            combined with +=/.=
    19291665 cooker: Remove debug code, oops :(
    
    meta-oe as of 2021-11-11 06:36:53 -0800
    
    c1968ea88c linpack: Build both sp and dp binaries
    ef4f5c1f33 nginx: use ln -rs
    00fbc4a643 python3-configparser: Upgrade 5.0.2 -> 5.1.0
    3a1794cfb4 python3-httplib2: upgrade 0.20.1 -> 0.20.2
    85eba2f654 python3-graphviz: upgrade 0.17 -> 0.18
    c24ba0d0ef python3-google-api-python-client: upgrade 2.28.0 -> 2.30.0
    f0d9f52829 python3-fastnumbers: upgrade 3.1.0 -> 3.2.1
    c5051f8f0a python3-async-timeout: upgrade 3.0.1 -> 4.0.0
    e6292ce3c9 python3-aiohttp: upgrade 3.7.4 -> 3.8.0
    2f9391543b python3-aenum: upgrade 3.1.2 -> 3.1.3
    bf1e71cea9 protobuf-c: Export native protoc
    32083888af jsoncpp: upgrade 1.9.4 -> 1.9.5
    03647ab540 debootstrap: upgrade 1.0.124 -> 1.0.126
    b57d824fdf syslog-ng: adjust control socket location
    18c419fcdb libmxml: upgrade 3.2 -> 3.3
    c971a10498 iwd: upgrade 1.18 -> 1.19
    eb1424e83d fetchmail: upgrade 6.4.21 -> 6.4.23
    9abae0ea02 packagegroup-meta-oe: Add fwupd and fwupd-efi
    786a317182 fwupd: add new recipe
    2a46c9947c fwupd-efi: add new recipe
    
    meta-virtualization as of 2021-11-11 08:59:11 -0500
    
    e13d364 python3-dtc: port to Python 3.10
    c5d89fe kubernetes: update to v1.22.4

--- weboslayers.py
+++ weboslayers.py
@@ -58,23 +58,23 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=87c9e120', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=8e7f2b65', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=afad2f09eb', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=365dc1471a', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=19de3586be', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=c1968ea88c', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=851c5b7', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=ecd241f', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=e13d364', ''),
 ('meta-python2',              17, 'https://git.openembedded.org/meta-python2',              'branch=master,commit=bb9e1ac', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=fb8e8e8', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2021-11-09', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2021-11-11', ''),
 
 ('meta-raspberrypi',          50, 'https://git.yoctoproject.org/git/meta-raspberrypi',      'branch=master,commit=e31fba4', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
