94c0e578f01431d14fdb30c7a737d8d896011eda
Martin Jansa <martin.jansa@lge.com>
2021-11-27 09:38:11 -0800

    webos-image: Update layer pins for 2021-11-27 from master
    oe-core as of 2021-11-26 16:58:55 +0000
    
    3770d539d8 oeqa/utils/dump: Fix typo
    8ddc6f46d4 gcc: Drop further unneeded precompiled header patch
    16b8939bf0 base.bbclass: Add sysroot deps for repo fetcher
    db9d86bdf2 repo: Add recipe for 2.17.3
    59922c95fc native/cross: Add ar wrapper for determinism
    e0010af733 gcc: Drop mips default ABI patch
    e1f40670c4 nativesdk: Handle chown/chgrp calls in nativesdk do_install
            tasks
    8406e83ade perf: sort-pmuevents: allow for additional type qualifiers
            and storage class
    e1382583cd perf: sort-pmuevents: don't drop elements
    c12a91827e linux-yocto-dev: introduce dt-validation PACKAGECONFIG
    a8525391f8 kernel: introduce python3-dtschema-wrapper
    ea71e50be2 linux-yocto: export pkgconfig variables to devshell
    333af4b995 python3: Add missing HOMEPAGE entries
    dff7ce9b72 maintainers: update for kernel dtschema checking
            dependencies
    5126515104 python: import vcversioner from meta-python
    1f00aad201 python: import strict-rfc3339 from meta-python
    9a3cb1a02a python: import rfc3987 from meta-pyton
    e25d4417bc python: import pyrsistent from meta-python
    748f7d377b python: import ruamel-yaml from meta-python
    cbba71e821 python: import webcolors from meta-python
    ffe5336563 python: import rfc3986-validator from meta-python
    8f9ade9448 python: import rfc3339-validator from meta-python
    14fdb80c3c python: import idna from meta-python
    c28fd66c67 python: import jsonschema from meta-python
    7e56176027 ptest-packagelists: Add missing python3-jsonpointer entry
    687823be0e python3-jsonpointer: Update 2.1 to 2.2
    5e057c058d python: import jsonpointer from meta-python
    d56fcb5806 python: introduce python3-dtschema
    9172d61c57 kernel: export native PKGCONFIG variables
    37fb1592eb linux-yocto-dev: bump to v5.16+
    
    bitbake as of 2021-11-26 17:01:34 +0000
    
    7e1ca7ab test/fetch: Add striplevel unpack parameter test
    f0442a30 bitbake-user-manual: Add striplevel unpack parameter
    aa4926e5 fetch2: Add striplevel support to unpack
    a08e57c9 fetch2: Unify tar command in unpack
    ebd00330 data_smart.py: Skip old override syntax checking for
            anonymous functions

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=0d0e552d', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=7e1ca7ab', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=6fca075a8d', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=3770d539d8', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=2ba87bfbb0', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
