1db9295d1c40ff493d4e04ad063f69335acf2964
Martin Jansa <martin.jansa@lge.com>
2021-12-06 03:57:32 -0800

    webos-image: Update layer pins for 2021-12-06 from bitbake/1.46
    bitbake as of 2021-12-05 12:36:37 +0000
    
    f18b65d0 bblayers/action: When adding layers, catch BBHandledException
    a6ad6eaf cooker: Ensure reparsing is handled correctly

--- weboslayers.py
+++ weboslayers.py
@@ -58,7 +58,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=7fdd43c5', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=f18b65d0', ''),
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=f61fcb6e17', 'oe-core'),
 
