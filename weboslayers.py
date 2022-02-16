0a58f604960aed7d9857fb9e63b012db1914a58a
Martin Jansa <martin.jansa@lge.com>
2022-02-16 11:42:32 +0000

    webos-image: Update layer pins for 2022-02-16 from honister
    oe-core as of 2022-02-09 23:15:24 +0000
    
    418a9c4c31 build-appliance-image: Update to honister head revision
    3e40efa0a0 libxml2: Backport python3-lxml workaround patch
    593a10c37f core-image-sato-sdk: allocate more memory when in qemu
    61dcaeda02 vim: upgrade to patch 4269
    4857274b27 expat: upgrade to 2.4.4
    0fccab0724 libusb1: correct SRC_URI
    6ff05fe05a yocto-check-layer: add debug output for the layers that
            were found
    289a849f8f linux-firmware: Add CLM blob to linux-firmware-bcm4373
            package
    773b15f456 linux-yocto/5.10: update to v5.10.93
    dff0f30789 linux-yocto/5.10: update to v5.10.92
    2948fdb7df tiff: backport fix for CVE-2022-22844
    18a43fe970 icu: fix make_icudata dependencies
    62de1f71d5 glibc : Fix CVE-2021-3999
    184125f721 glibc : Fix CVE-2021-3998
    25a7e8dae1 sstate: Improve failure to obtain archive message/handling
    3f183a7f5b insane.bbclass: Correct package_qa_check_empty_dirs()
    b2a5d9bc61 sstate: A third fix for for touching files inside pseudo
    cc9d4a69e6 kernel: introduce python3-dtschema-wrapper
    
    meta-oe as of 2022-01-29 06:44:29 -0800
    
    c05ae80ba6 tiptop: update download URL and HOMEPAGE
    ce24c81d84 cmocka: use https protocol for fetching
    a750888a40 snappy: use main branch to fix fetch failure
    033fa7a408 dbus-daemon-proxy: add missing `return` statement
    feab111e79 python3-prctl: Use https protocol for git fetcher
    cf939ad690 plymouth: switch to KillMode=mixed

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.52,commit=c039182c', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=honister,commit=2d3c5b078f', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=honister,commit=418a9c4c31', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=honister,commit=4647e3ea37', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=honister,commit=c05ae80ba6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
