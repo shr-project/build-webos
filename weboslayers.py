491e1f16fd1b908bc565580e892a72b850192ee9
Martin Jansa <martin.jansa@lge.com>
2022-02-21 11:38:03 +0000

    webos-image: Update layer pins for 2022-02-21 from master
    oe-core as of 2022-02-21 07:51:46 +0000
    
    6ae14b4ff7 tiff: Add backports for two CVEs from upstream
    a8d0a40263 vim: Upgrade 8.2.4314 -> 8.2.4424
    3859f49db2 libxml-parser-perl: Add missing RDEPENDS
    a5b0f6cacb perl: Improve and update module RPDEPENDS
    91aa5e3f94 expat: Upgrade 2.4.5 -> 2.4.6
    b71344dacb expat: Upgrade 2.4.4 -> 2.4.5
    3ca368aa12 libhandy: Use upstream regex to check version of upgrade.
    a545a0848c waffle: add wayland-protocols when building with wayland
    d425964576 grub: Fix build with bintutils 2.38 on riscv
    ee862994d2 libgit2: Upgrade to 1.4.1
    cabc3cc2ea sstate: Setup fetcher environment in advance
    3d50ee782e oeqa/selftest/bbtests: Update after license changes
    ceda3238cd meta/meta-selftest/meta-skeleton: Update LICENSE variable
            to use SPDX license identifiers
    512cd4ca91 scripts: Add a conversion script to use SPDX license names
    f87b32833a core-image-testmaster: Rename to core-image-testcontroller
    7ca1bce20d gstreamer1.0: update licenses of all modules
    ee8ec22c75 gdb: update 11.1 -> 11.2
    
    bitbake as of 2022-02-20 16:45:06 +0000
    
    c73bb602 fetch2: Abstract fetcher environment to a function
    52630eef wget: Fix grammar "can happen"
    8f761178 utils: Fix typo "dependency" and "spawning"
    2e0407f6 tinfoil: Fix typo "receive" and "something"
    bb43be9b progress: Fix typo "wherever"
    2874e9cb process: Fix typo: "process"
    f9bea2b2 persist_data: Fix typo "committed"
    02e29e90 fetch2/git: Remove duplicate "the"
    e1c13a82 fetch2: Fix typo "conform" and "processed"
    bb60472f event: Fix typo "asynchronous" and "occasionally"
    c93bc384 daemonize: Fix typo "separate"
    d0b63325 cookerdata: Fix typo "normally"
    ac688b6e cooker: Fix typo "isn't" and "tuples"
    346a1a6c fetch2/wget: move loop-invariant load of BB_ORIGENV
    ae4ce4c2 utils: Fix environment decorator logic error
    
    meta-oe as of 2022-02-20 17:14:10 -0800
    
    3d84826958 geany-plugins: Fix build with libgit2 1.4+
    19c0838f17 libsigc++-3: Upgrade to 3.2.0
    6c258bd830 nodejs: add option to use openssl legacy providers
    6ec4451fc9 dnf-plugin-tui: Fix a bug of multilib
    5617b1269d boost-sml: add new recipe
    f3474b141d networking: add new netsniff-ng recipe version 0.6.8
    99e2f59d7c rtc-tools: Update to 2022.02
    6c6b8d97b4 wireplumber: Improve configuration
    5c379cf625 netdata: version bump 1.33.0 -> 1.33.1
    f967602a43 rsyslog: update to 8.2202
    0bf3af0aed plymouth: Add the retain-splash option
    69db1c7a82 libjs-jquery: Upgrade to 3.3.1.
    
    meta-raspberrypi as of 2022-02-21 00:16:17 +0000
    
    157f72b rpi-default-versions: Use 5.15 as default kernel
    50d8dc9 linux-raspberrypi: Add recipe for 5.15 LTS kernel
    076aa51 gstreamer1.0-plugins-bad: remove libmms PACKAGECONFIG and
            add gpl
    e0fc680 sdcard_image-rpi: fix DeprecationWarning
    7a45b41 python3-adafruit-blinka: Disable on musl
    596b92e rpi-base.inc: enable i2c-gpio overlay
    9bd5f90 rpi-base.inc: Add vc4-fkms-v3d-pi4 overlay
    
    meta-security as of 2022-02-20 19:39:36 -0800
    
    a6fc259 tpm2-pkcs11: fix RDEPENDS variable
    caec0c6 scap-security-guide: Fix openembedded platform tests
    9cd85e0 meta-hardening: Fix override syntax
    7898fc8 parsec-service: fix compile issue.
    32c0e16 tpm2-tss: update to 3.1.0
    
    meta-updater as of 2022-02-21 11:18:07 +0100
    
    cee9fbf Merge pull request #24 from uptane/latest-versions-master
    1f23c1c aktualizr: Mark aktualizr-hwid as RRECOMMENDS, not RDEPENDS.
    93968ee aktualizr: Bump to latest, plus garage-sign.

--- weboslayers.py
+++ weboslayers.py
@@ -58,28 +58,28 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=c885e554', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=c73bb602', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=3389fa68ee', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=6ae14b4ff7', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=ea223bd3da', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=3d84826958', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=851c5b7', ''),
+('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=cee9fbf', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=44e95d9', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2.2,commit=1b9f464', ''),
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=kirkstone-2022-02-21', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=b7b34ec', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=157f72b', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-updater',        52, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
 ('meta-webos-smack',          75, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
-('meta-security',             77, 'https://git.yoctoproject.org/git/meta-security',         'branch=master,commit=c626643', ''),
+('meta-security',             77, 'https://git.yoctoproject.org/git/meta-security',         'branch=master,commit=a6fc259', ''),
 ]
