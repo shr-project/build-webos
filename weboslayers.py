00be9d981aee6fd9d52e586259e6b1c0350b466c
Martin Jansa <martin.jansa@lge.com>
2022-02-22 20:40:11 +0000

    webos-image: Update layer pins for 2022-02-22 from master
    oe-core as of 2022-02-22 15:24:48 +0000
    
    46135c8734 scripts: fix file writing in convert-spdx-licenses
    31be8f4750 icecc.bbclass: fix syntax error
    
    meta-oe as of 2022-02-22 08:30:51 -0800
    
    edd2a422cf xfce4-panel: disable vala
    a50aba5570 libxfce4ui: disable vala
    343ec1feec xfconf: inherit vala
    26d9d0b627 libxfce4util: inherit vala
    606eaedc8c libcamera: add pkg-config files
    4a80836f83 libio-socket-ssl-perl: upgrade 2.073 -> 2.074
    696aab3d80 graphviz: added PACKAGECONFIG for librsvg
    da18587531 libjcat: upgrade 0.1.9 -> 0.1.10
    5bb47bf2ee monit: upgrade 5.30.0 -> 5.31.0
    e20fb95fb5 libgusb: upgrade 0.3.9 -> 0.3.10
    9fd3375530 libxmlb: upgrade 0.3.6 -> 0.3.7
    2d41a2195a gnuplot: upgrade 5.4.2 -> 5.4.3
    b73b7e42d8 inotify-tools: upgrade 3.21.9.6 -> 3.22.1.0
    4d278b9459 iotop: upgrade 1.20 -> 1.21
    358db10e03 iscsi-initiator-utils: upgrade 2.1.5 -> 2.1.6
    e5613af40e libwnck3: upgrade 40.0 -> 40.1
    aca61b8dc7 gnome-autoar: upgrade 0.4.2 -> 0.4.3
    8918d9485c gedit: upgrade 40.1 -> 41.0
    491ea7f7fc libwacom: upgrade 1.12 -> 2.1.0
    4d8a991551 python3-croniter: Upgrade 1.2.0 -> 1.3.4
    e5350050e1 python3-itsdangerous: Upgrade 2.0.1 -> 2.1.0
    c0be70f84c python3-dt-schema: remove recipe
    
    meta-raspberrypi as of 2022-02-22 16:21:17 +0000
    
    2bfbdd6 meta: update license names
    3a83596 meta: update variable names
    0bc1eac layers: Bump to use kirkstone

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=3b88562d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=187ac1ea0a', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=46135c8734', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=9d5081e524', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=edd2a422cf', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -75,7 +75,7 @@ webos_layers = [
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=kirkstone-2022-02-23', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=157f72b', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=2bfbdd6', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-updater',        52, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
