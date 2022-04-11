85464038ea61e5881abb80ed165d976e10f1ecb1
Martin Jansa <martin.jansa@lge.com>
2022-04-11 15:08:10 +0000

    webos-image: Update layer pins for 2022-04-11 master
    meta-oe as of 2022-04-11 09:22:30 -0400
    
    22fa522261 python3-zeroconf: update to 0.38.4
    079f04dbb2 python3-xmlschema: update to 1.10.0
    c7b81b9038 python3-twitter: update to 4.8.0
    22ce644f15 python3-tqdm: update to 4.64.1
    4158bd57db python3-sympy: update to 1.10.1
    917127aec5 python3-regex: update to 2022.3.15
    6842d6ae21 python3-pywbem: update to 1.4.1
    82f1f3d165 python3-pytest-helpers-namespace: update to 2021.12.29
    5d72100f74 python3-pyscaffold: update to 4.2.1
    f42937f461 python3-pymogo: update to 4.1.0
    2d24d2964c python3-pycurl: update to 7.45.1
    30a9117412 python3-pulsectl: update to 22.3.2
    696a6996be python3-portalocker: update to 2.4.0
    c2d4089100 python3-pint: update to 0.19.1
    2b62902692 python3-mypy: update to 0.942
    09c9b9b004 python3-jmespath: update to 1.0.0
    bd3643a5c3 python3-ipython: update to 8.2.0
    9827b0faf8 python3-grpcio-tools: update to 1.45.0
    cf279b1fe6 python3-google-auth: update to 2.6.3
    48ac68e512 python3-google-api-core: update to 2.7.1
    e43b914f4f python3-gcovr: update to 5.1
    db1c8cea6a python3-flask: update to 2.1.1
    50378fee90 python3-flask-login: update to 0.6.0
    a8589dd218 python3-elementpath: update to 2.5.0
    8bfdee2f78 python3-can: update t 4.0.0
    2f55a5ee2f python3-bidict: update to 0.22.0
    7177c2ec0f python3-beautifulsoup: update to 4.11.1
    46612115db python3-wxgtk4: Require DISTRO_FEATURES as gtk3
    b924bc52ca python3-wxgtk4: pass '--baselib' to WX_CONFIG

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=b3114d5d43', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=35ea4b37ba', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=22fa522261', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
