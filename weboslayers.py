240923b6f07d9e66e855ab9aac0ce67b73389843
Martin Jansa <martin.jansa@lge.com>
2022-04-11 13:10:04 +0000

    webos-image: Update layer pins for 2022-04-11 from kirkstone
    oe-core as of 2022-04-09 22:54:56 +0100
    
    b3114d5d43 license_image.bbclass: close package.manifest file
    df6d96cc5e spirv-tools: fix to use sdk-1.3.204 branch
    4705b8a724 python3-cryptography: backport fix for leaky test
    3ba177a1b8 package_ipk/deb: Fix specific version handling
    e3da4da7e5 uninative: Upgrade to 3.6 with gcc 12 support
    ada1a900b0 gzip: upgrade 1.11 -> 1.12
    97bf86ccde xz: fix CVE-2022-1271
    9e2cb139fa connman-conf: ignore eth0 in qemu in a way that is not
            sysvinit-specific
    
    meta-oe as of 2022-04-10 10:05:35 -0700
    
    35ea4b37ba libcereal: Disable on ppc/ppc64
    8d3ee7b2d8 gparted: update to v1.4.0
    150581b637 gnuplot: Disable libcerf and fix typo
    668ed5c5c9 libcereal: Link in libatomic on rv32 for ptests
    a3dab74a89 grpc: remove unused patches
    085bfd3601 libotr: Include missing sys/socket.h header
    ae60e9869b drbd-utils: update 9.13.1 -> 9.20.2
    6305aacfde Revert "python3-cppy: upgrade 1.2.0 -> 1.2.1"
    5e5d973d78 wxwidgets: Fix building without x11
    b04696ddf8 grpc: remove useless link with libatomic append
    1a91a9cb27 grpc: add cmake support for target
    8291505ccd ndctl: fix build failure with EXEWRAPPER_ENABLED False
    d8187782ca libyang: do not inherit binconfig-disabled
    8e55b9aef8 wxwidgets: fix wx-config multilib issue
    bbbd86d87e MAINTAINERS: add entry for OpenThread
    b75d3b2461 wpantund: add new recipe
    47640c2235 ot-daemon: add recipe for OpenThread daemon
    bb6a70a463 ot-br-posix: add recipe for an OpenThread Border Router
    
    meta-virtualization as of 2022-04-11 08:13:16 -0400
    
    654f8d0 cni: fix protocol argument typo
    589391d cni: add flannel plugin from separate repository
    
    meta-qt6 as of 2022-04-08 17:43:11 +0000
    
    4ffc6cb Stage all tools to sysroot
    c1a84ec python3-antlr4-runtime: use correct recipe name
    c47fff0 coin: use shared Hash Equivalence server
    9576147 qtbase: update license checksum
    9b986ef Update submodule refs on '6.2' in yocto/meta-qt6

--- weboslayers.py
+++ weboslayers.py
@@ -60,18 +60,18 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=41eeb4f3', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=62851965fc', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=b3114d5d43', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=b9e440ead8', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=35ea4b37ba', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=826542a', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=9857587', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=654f8d0', ''),
 
-('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=72fa838', ''),
+('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=4ffc6cb', ''),
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=kirkstone-2022-04-01', ''),
 
