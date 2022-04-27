880069a1947f9958f6c0eeb56b6744d4f3cdf5a7
Martin Jansa <martin.jansa@lge.com>
2022-04-27 11:32:50 +0000

    webos-image: Update layer pins for 2022-04-27 as in WRO-3779
    oe-core as of 2022-04-25 15:58:54 +0100
    
    8e81d38048 build-appliance-image: Update to dunfell head revision
    84f30aecf2 linux-yocto/5.4: update to v5.4.190
    6a445ac241 linux-yocto/5.4: update to v5.4.188
    bcabfe3640 linux-yocto/5.4: update to v5.4.186
    d0399ed433 linux-yocto/5.4: update to v5.4.183
    7efa786ed2 linux-yocto/5.4: update to v5.4.182
    8606d99041 git update from 2.24.3 to 2.24.4
    4da46e0423 u-boot: Correct the SRC_URI
    635d1ab7ed wireless-regdb: upgrade 2022.02.18 -> 2022.04.08
    de880e23bf linux-firmware: upgrade 20220310 -> 20220411

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=0784db7d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=8fd5133fc7', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=8e81d38048', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=fdd1dfe6b4', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
