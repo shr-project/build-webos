977ac3ae764e3ceacf2a39c5639f40ec77d3d7e4
Martin Jansa <martin.jansa@lge.com>
2022-04-27 11:37:35 +0000

    webos-image: Update layer pins for 2022-04-27 from kirkstone
    meta-oe as of 2022-04-23 17:14:31 -0700
    
    bb2b5b31a8 meta-openemnedded: Add myself as kirkstone maintainer
    64156a6f8f gpsd: Correct the creation of the gps-utils-python package
    ec6d073518 sdbus-c++-libsystemd: bugfix dev package is not installed
    b932a20f26 drbd-utils: fix for usrmerge
    35f0d19d4f python3-imageio: upgrade 2.16.1 -> 2.17.0
    bc92c3e718 python3-django: upgrade 2.2.27 -> 2.2.28
    0f00037a20 python3-cmd2: upgrade 2.4.0 -> 2.4.1
    c6022c3ffb python3-aws-iot-device-sdk-python: upgrade 1.5.1 -> 1.5.2
    a2683269de python3-aenum: upgrade 3.1.8 -> 3.1.11
    a1b80175b3 python3-marshmallow: Upgrade 3.14.1 -> 3.15.0
    9881151e80 python3-bitstruct: Upgrade 8.13.0 -> 8.14.0
    674306a429 python3-blivetgui: use symbolic list-add and edit- icons
    b3be90be2f python3-cppy: upgrade 1.2.0 -> 1.2.1
    
    meta-virtualization as of 2022-04-26 23:03:42 -0400
    
    21a5c29 xen: Add missing space in append to IMAGE_INSTALL
    c21e22e libvirt: Fix error on undefining domain and segfault on
            libvirt_driver_qemu.so
    63b026b containerd-opencontainers: add CVE_VERSION setting
    bff2ad3 nerdctl: move SRC_URI appends to include file
    c6464d8 nerdctl: update to v0.18
    48ef790 xen-tools: don't declare xencommons as an initscript for
            systemd
    6ed391f k3s: Add missing IP Virtual Server (ip_vs) feature to the
            kernel config
    e02f0d3 openvswitch: uprev from v2.17.0-30-g2404d4536 to v2.17.1
    7df9b0f xen: Remove 4.14 recipes and related patches
    265518e xen: Add recipes for stable Xen 4.16 release and update master
    aa4ebb5 xen: Bump SRCREV of 4.15 recipes to 4.15.2
    51e404a xen-tools: remove unused systemd/sysvinit files

--- weboslayers.py
+++ weboslayers.py
@@ -62,14 +62,14 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=741064d6e9', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=12058113c4', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=bb2b5b31a8', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=826542a', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=1f9c37e', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=21a5c29', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=6215bb4', ''),
 
