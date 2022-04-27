56b77bf991457597e4355d067006484d9ccb2f16
Martin Jansa <martin.jansa@lge.com>
2022-04-27 11:35:16 +0000

    webos-image: Update layer pins for 2022-04-27 from honister
    oe-core as of 2022-04-19 22:22:49 +0800
    
    4c556b4fd0 license_image.bbclass: close package.manifest file
    7c3f8ffdbe libxshmfence: Correct LICENSE to HPND
    a41229effb externalsrc/devtool: Fix to work with fixed export funcition
            flags handling
    c3d6829f6c tiff: Add marker for CVE-2022-1056 being fixed
    5f23b1fde0 uninative: Upgrade to 3.6 with gcc 12 support
    dd6239a0f6 xz: fix CVE-2022-1271
    ab69b7566f pseudo: Fix handling of absolute links
    8b0079d029 linux-yocto/5.10: update to v5.10.109
    4fdc8884c4 vim: Upgrade 8.2.4524 -> 8.2.4681
    57b608a3c3 unzip: fix CVE-2021-4217
    ad7a8bf827 scripts/runqemu: Fix memory limits for qemux86-64

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.52,commit=70ea322a', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=honister,commit=75e47b96d8', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=honister,commit=4c556b4fd0', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=honister,commit=9a0caf5b09', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
