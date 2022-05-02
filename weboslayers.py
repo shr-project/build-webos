c731870cb0b0d5d3181dce5c68d22128d50f2891
Martin Jansa <martin2.jansa@lgepartner.com>
2022-05-02 15:14:45 +0000

    webos-image: Update layer pins for 2022-05-02 from meta-oe/hardknott
    meta-oe as of 2022-04-29 18:43:20 -0700
    
    786d3aa313 fix renamed patch
    0b477424ed postgresql: Fix build on riscv
    44ba6b5727 abseil-cpp: Fix build with glibc 2.34

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=hardknott,commit=fbbb689c8d', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=hardknott,commit=a970ee45c2', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=hardknott,commit=786d3aa313', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
