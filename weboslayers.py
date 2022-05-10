7e022999b4c9ebf258e47f0ec26797697f1b7053
Martin Jansa <martin2.jansa@lgepartner.com>
2022-05-10 11:06:24 +0000

    webos-image: Update layer pins for 2022-05-10 from master
    oe-core as of 2022-05-10 09:45:31 +0100
    
    98bf3f4277 linux-firmware: replace mkdir by install
    f0bd9e5d10 Revert "image.bbclass: allow overriding dependency on
            virtual/kernel:do_deploy"
    c713692fe0 pciutils: Add make-native dependency
    6dd928534d freetype: Upgrade 2.12.0 -> 2.12.1
    b12e6cfe3b liberror-perl: Update sstate/equiv versions to clean cache
    6936e44d10 libgcrypt: Fix reproducibility issues in ptest
    dd660e5c3f package: Ensure we track whether PRSERV was active or not
    77d745bd49 vim: Upgrade 8.2.4681 -> 8.2.4912
    aab854a94e e2fsprogs: update upstream status
    09e18ee246 wic/plugins/rootfs: Fix permissions when splitting rootfs
            folders across partitions
    be61b9dac7 openssh: Add openssh-sftp-server to openssh RDEPENDS
    61f6b0ad09 pypi.bbclass: Set CVE_PRODUCT to PYPI_PACKAGE
    e6c18c9d9d eudev: Add PACKAGECONFIG for manpages & selinux
    31005f24d3 eudev: Upgrade 3.2.10 -> 3.2.11
    3b66727303 abi_version/sstate: Bump hashequiv and sstate versions
            due to git changes
    e28dd48ffb Revert "bitbake.conf: mark all directories as safe for
            git to read"
    27de610ac3 scripts/git: Ensure we don't have circular references
    af27c81eaf scripts: Make git intercept global
    52c37e133f base: Avoid circular references to our own scripts
    4682226804 oeqa/selftest: add test for git working correctly inside
            pseudo
    c22f81a375 coreutils: update 9.0 -> 9.1
    a1f51bae8e systemd: Fix build regression with latest update
    c09daf7fdc libgcrypt: Drop GPLv3 license after upstream changes
    f763b906ea cairo: Add missing GPLv3 license checksum entry
    5ab122a79e stress-ng: disable apparmor from the correct spot
    3ea0f248ac MAINTAINERS.md: no more need for a prelink-cross maintainer
    c4ba21f401 libxml2: Upgrade 2.9.13 -> 2.9.14
    
    bitbake as of 2022-05-10 09:47:29 +0100
    
    ea6a676c bitbake-diffsigs: break on first dependent task difference
    d3d406e8 fetch/git : Use cat as pager
    a70a7376 cache: correctly handle file names containing colons
    
    meta-oe as of 2022-05-07 18:47:16 -0700
    
    71bbe89bac layer.conf: Post release codename changes
    f26d0085d7 minidlna: fix obsolete license warning
    fd330c4514 kernel-selftest: install kselftest runner
    aca2dbebf0 meta-networking: Add meta-python to BBFILES_DYNAMIC
    1556da8902 libcoap: Add recipe
    80a5a789fb ulogd2: Add recipe
    48515bae74 zfs: Make systemd and sysvinit into packageconfigs
    a7c3aacb09 zfs: Disable on mips
    f53c26a3ee zfs: Disable on riscv32
    e1ccf12c15 zfs: Fix build on musl systems
    da8a50684a Add zfs recipe
    0716b34c88 vboxguestdrivers: upgrade 6.1.32 -> 6.1.34
    1c3465b4d7 devmem2: update SRC_URI according to redirect
    3af1d119de devmem2: add support for different page sizes
    e192bd13ff devmem2: reinstate previous patches, removed by mistake
    f334a00947 s-nail: Set VAL_MTA
    
    meta-raspberrypi as of 2022-05-07 12:37:11 +0100
    
    a5c2bef linux-firmware-rpidistro: Create
            brcmfmac43455-sdio.raspberrypi,4-model-b.bin symlink
    
    meta-virtualization as of 2022-05-09 13:08:06 -0400
    
    d32042e xen-tools: Fix build with GCC 12
    6a43be4 seabios: Upgrade to 1.16 release
    27e5ade Revert "qemuboot, xen-image-minimal: enable runqemu for
            qemuarm Xen images"
    bd66956 xen/4.16: fix gcc 12 build
    d7a2d1b xen images, qemuboot: configure xenbr0 bridge for runqemu,
            testimage
    1c746a9 qemuboot, xen-image-minimal: enable runqemu for qemuarm
            Xen images
    da1e54c xen, xen-tools: add recommendation for Qemu for non-hvm x86
    b9e18c1 xen-tools: correct xencommons initscript for systemd

--- weboslayers.py
+++ weboslayers.py
@@ -58,24 +58,24 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=dd2475e3', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=ea6a676c', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=ab6bd34a07', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=98bf3f4277', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=6c2bd3c0fa', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=71bbe89bac', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=826542a', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=973c8d0', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=d32042e', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.2,commit=314b0b2', ''),
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2022-05-10', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=90448d7', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=a5c2bef', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-updater',        52, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
