dbd94fd1afe1ad31d0da0af2861a303ccbfffcc2
Martin Jansa <martin2.jansa@lgepartner.com>
2022-05-10 20:11:10 +0000

    webos-image: Update layer pins for 2022-05-10 from master
    oe-core as of 2022-05-10 13:11:20 +0100
    
    b8878cf0d3 gcc: upgrade 11.3 -> 12.1

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=ea6a676c', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=98bf3f4277', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=b8878cf0d3', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=71bbe89bac', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
