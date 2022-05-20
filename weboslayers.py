ab06c46a064e9929652f8cbef2d4b3c5a5e3108d
Martin Jansa <martin2.jansa@lgepartner.com>
2022-05-20 12:26:55 +0000

    webos-image: Update layer pins for 2022-05-20 as in WRO-4984
    oe-core as of 2022-06-01 06:22:09 -1000
    
    d009233f36 cve-check: Allow warnings to be disabled
    2bacd7cc67 cve-check: Only include installed packages for rootfs
            manifest
    8a178a728f cve-check: Add helper for symlink handling
    df09cd71b4 cve-check.bbclass: Added do_populate_sdk[recrdeptask].
    cd259a0050 vim: Upgrade 8.2.4912 -> 8.2.5034 to fix 9 CVEs
    096ca5fa8c libxml2: Fix CVE-2022-29824 for libxml2
    2287d591cf ncurses: Fix CVE-2022-29458
    2a97ba89f2 ffmpeg: Fix for CVE-2022-1475
    1cc84e4c51 libsdl2: Add fix for CVE-2021-33657
    df6242b72b ruby: Whitelist CVE-2021-28966 as this affects Windows
            OS only
    4514b1b8ca ruby: Upgrade ruby to 2.7.6 for security fix
    add860e1a6 oeqa/selftest/cve_check: add tests for recipe and image
            reports
    9d5b4fdc7c mobile-broadband-provider-info: upgrade 20220315 -> 20220511
    7f4daf88b7 pcre2: CVE-2022-1586 Out-of-bounds read
    665f981fcc cve-check: Fix report generation
    48ce924dc8 selftest: skip virgl test on alma 8.6
    1ec7c6f0f0 linux-firmware: upgrade 20220411 -> 20220509
    c89bc0fc7f linux-firmware: replace mkdir by install
    de0cafc018 openssl: Minor security upgrade 1.1.1n to 1.1.1o
    970743af34 git: Use CVE_CHECK_WHITELIST instead of CVE_CHECK_IGNORE
    51a92860bd freetype: Fix CVEs for freetype
    7c71434832 tiff: Add patches to fix multiple CVEs
    bbbd258a1c curl: Fix CVEs for curl
    c7d43000ce vim: Upgrade 8.2.4681 -> 8.2.4912
    
    meta-oe as of 2022-05-25 19:34:39 -0700
    
    deee226017 tcpdump: Add fix for CVE-2018-16301
    9f361cff9c opencl-headers: switch to main branch
    a1c7bb2098 fuse: set CVE_PRODUCT to "fuse_project:fuse"
    c9e034fbaa opencl-icd-loader: switch to main branch
    a38c92d8e9 openjpeg: Whitelist CVE-2020-27844 and CVE-2015-1239
    de4b76934c ostree: prevent ostree-native depending on target
            virtual/kernel to provide kernel-module-overlay
    b99a386cd1 python3-cryptography: backport 3 changes to fix
            CVE-2020-36242
    abd7cf838d lua: fix CVE-2022-28805
    a8d82c80a1 atftp: Add fix for CVE-2021-41054 and CVE-2021-46671

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=0784db7d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=0f6ae13d76', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=d009233f36', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=8ff12bfffc', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=deee226017', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
