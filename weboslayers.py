e7bf68e7f6d377d056c1d6062e1345d93ab98526
Martin Jansa <martin2.jansa@lgepartner.com>
2022-05-28 11:07:04 +0000

    webos-image: Update layer pins for 2022-05-28 from kirkstone
    oe-core as of 2022-05-28 10:38:07 +0100
    
    4eb0b74683 tiff: mark CVE-2022-1622 and CVE-2022-1623 as invalid
    72aa42425e license_image.bbclass: Make QA errors fail the build
    c762a5c97b sqlite3: upgrade 3.38.3 -> 3.38.5
    bbfce5f1c2 mobile-broadband-provider-info: upgrade 20220315 -> 20220511
    4bd58b5131 mesa: upgrade 22.0.2 -> 22.0.3
    6588c45d6c libcgroup: upgrade 2.0.1 -> 2.0.2
    5b2f2ff633 gstreamer1.0-vaapi: upgrade 1.20.1 -> 1.20.2
    e779fb1773 gstreamer1.0: upgrade 1.20.1 -> 1.20.2
    cdafe69411 gstreamer1.0-rtsp-server: upgrade 1.20.1 -> 1.20.2
    5ce15470ae gstreamer1.0-python: upgrade 1.20.1 -> 1.20.2
    038cbfa1ae gstreamer1.0-plugins-ugly: upgrade 1.20.1 -> 1.20.2
    9472f0fe64 gstreamer1.0-plugins-good: upgrade 1.20.1 -> 1.20.2
    19e9c0b2ac gstreamer1.0-plugins-base: upgrade 1.20.1 -> 1.20.2
    8dca7bae0b gstreamer1.0-plugins-bad: upgrade 1.20.1 -> 1.20.2
    2f8bf9e800 gstreamer1.0-omx: upgrade 1.20.1 -> 1.20.2
    b796e337ac gstreamer1.0-libav: upgrade 1.20.1 -> 1.20.2
    a16fd1683d gst-devtools: upgrade 1.20.1 -> 1.20.2
    bcbfff47e2 go: upgrade 1.17.8 -> 1.17.10
    61e8e3c7f0 oeqa/selftest/cve_check: add tests for recipe and image
            reports
    fb6c980105 pcre2: CVE-2022-1586 Out-of-bounds read
    d3beac2335 selftest/imagefeatures/overlayfs: Always append to
            DISTRO_FEATURES
    927837e29a staging: Fix rare sysroot corruption issue
    34b6d6b5f0 classes: rootfs-postcommands: add skip option to
            overlayfs_qa_check
    cefc456255 binutils: Bump to latest 2.38 release branch
    cd16ecd441 librepo: upgrade 1.14.2 -> 1.14.3
    a0f25374ea mmc-utils: upgrade to latest revision
    00b62965bd python3: fix reproducibility issue with python3-core
    2fcc696e27 cve-check: Fix report generation
    b83c7ae43c curl: Backport CVE fixes
    
    bitbake as of 2022-05-25 18:00:34 +0100
    
    494fcfcb build: Add clean_stamp API function to allow removal of
            task stamps
    943701ee data: Do not depend on vardepvalueexclude flag
    8ac6e094 fetch2/osc: Small fixes for osc fetcher
    
    meta-updater as of 2022-05-25 15:30:39 +0200
    
    c2b18de Merge pull request #39 from shr-project/jansa/gcc-12
    c103d77 aktualizr: bump SRCREV
    
    meta-virtualization as of 2022-05-25 12:19:00 -0400
    
    590065c xen, raspberry pi: replace linux-yocto-dev with linux-yocto
            5.15

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=59c16ae6', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=494fcfcb', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=ec9e949773', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=4eb0b74683', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=0b78362654', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -68,8 +68,8 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=945d0dc', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=2fae71c', ''),
+('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=c2b18de', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=590065c', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.3.0,commit=f7c9337', ''),
 
