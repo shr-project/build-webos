6bdc51f53422681376dfe2e3f073bb0789c558fa
Martin Jansa <martin2.jansa@lgepartner.com>
2022-06-07 11:03:53 +0000

    webos-image: Update layer pins for 2022-06-07 from kirkstone
    oe-core as of 2022-06-02 06:48:32 -1000
    
    e63013cc38 gcc: Upgrade to 11.3 release
    1887a69a17 rust-common: Fix native signature dependency issues
    cf7c1cde3a rust-common: Drop LLVM_TARGET and simplify
    a4886f02df rust-common: Fix for target definitions returning 'NoneType'
            for arm
    84e891d812 rust-common: Ensure sstate signatures have correct
            dependencues for do_rust_gen_targets
    424b81ae68 rust-common: Fix sstate signatures between arm hf and non-hf
    d28d84c5b4 ovmf: Fix native build with gcc-12
    bbb7dcb461 pciutils: avoid lspci conflict with busybox
    d96022a389 insane.bbclass: make sure to close .patch files
    b34aaa9acb libseccomp: Add missing files for ptests
    bd72b15e9a staging.bbclass: process direct dependencies in
            deterministic order
    8d5f5823b3 mesa.inc: package 00-radv-defaults.conf
    862c1b109c ncurses: update to patchlevel 20220423
    9cf49b4f07 libpcre2: upgrade 10.39 -> 10.40
    02982668ac linux-yocto/5.10: update to v5.10.114
    7d734c95ba linux-yocto/5.15: update to v5.15.38
    96ed22ed1e linux-yocto/5.10: update to v5.10.113
    90628b9bbc linux-yocto/5.15: update to v5.15.37
    8fd6a9f521 cve-check: Allow warnings to be disabled
    726ce5bf1e cve-extra-exclusions: Add kernel CVEs
    65498411d7 cve-check: Only include installed packages for rootfs
            manifest
    af227e0c62 cve-check: Add helper for symlink handling
    637d6ff44b cve-check.bbclass: Added do_populate_sdk[recrdeptask].
    82f4186ec0 libxslt: Mark CVE-2022-29824 as not applying
    95f1c5d0bb tiff: Add jbig PACKAGECONFIG and clarify CVE-2022-1210
    c703b970f6 vim: Upgrade 8.2.4912 -> 8.2.5034 to fix 9 CVEs

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=494fcfcb', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=4eb0b74683', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=e63013cc38', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=0b78362654', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
