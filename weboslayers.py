6e160d14abfe6e13d44b978eb1688cf3290b75bc
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-06-09 08:23:42 +0900

    weboslayers.py: Add aiframework (build 467 part 1/2)
    meta-webosose as of 2022-06-09 08:21:03 +0900
    
    b103c4031 Add aiframework common recipes to meta-webos
    
    :Release Notes:
    Add aiframework libraryies such as tensorflow-lite, armnn
    For tensorflow-lite gpu acceleration, it requires the latest opencl recipe.
    Along with this, opencv's latest recipe is also requested.
    add meta-webos-backports-4.0 layer
    
    :Detailed Notes:
    
    :Testing Performed:
    Only build tested.
    
    :QA Notes:
    No change to image.
    
    :Issues Addressed:
    [WRO-5487] CCC: tensorflow-lite=r0,v2.6.2 edgeai-vision=7-r0,v1.0.0
    [WRO-2816]
    [WRO-4350]

--- weboslayers.py
+++ weboslayers.py
@@ -75,8 +75,9 @@ webos_layers = [
 
 ('meta-webos-backports-3.2',  33, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
+('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=b727302f9', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=b103c4031', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
