25c7f6d56aa15a9e2e8a3b8add3a8ce0aa15bb91
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-06-09 08:25:29 +0900

    webos-image: Update layer pins for 2022-06-09 (build 467 part 2/2)
    meta-webosose as of 2022-06-09 08:21:52 +0900
    
    2d59ee37c linux-raspberrypi=webosrpi31 containerd-opencontainers=webosvirt2 docker-moby=webosvirt4
    42684703e webos-fluentbit-plugins=32

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=b103c4031', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=2d59ee37c', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
