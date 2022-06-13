6dd277daf8e1a0f22daf1d600f13ccddb65b56fa
Martin Jansa <martin2.jansa@lgepartner.com>
2022-06-13 07:01:57 +0000

    webos-image: Update layer pins for 2022-06-13 from dunfell
    oe-core as of 2022-06-07 08:56:30 -1000
    
    7e056e79a5 linux-yocto/5.4: update to v5.4.196
    487a53522a cve-update-db-native: make it possible to disable database
            updates
    c63d06becc cve-check: add coverage statistics on recipes with/without
            CVEs
    4c10ee956f cve-check: write empty fragment files in the text mode
    c8a0e7ecee cve-check: move update_symlinks to a library
    7b2a1d908d curl: Fix CVE_CHECK_WHITELIST typo
    f8cdafc0ef curl: Backport CVE fixes
    9c736c9dcf libxslt: Mark CVE-2022-29824 as not applying
    3e01aa47b8 libxslt: Fix CVE-2021-30560
    46323b9e0f pcre2: CVE-2022-1587 Out-of-bounds read
    b4f9ba859e e2fsprogs: CVE-2022-1304 out-of-bounds read/write via
            crafted filesystem
    3af161acc1 openssl: update the epoch time for ct_test ptest
    40858a0598 openssl: backport fix for ptest certificate expiration
    68c97aa9e0 Revert "openssl: Backport fix for ptest cert expiry"

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=0784db7d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=4051d1a3aa', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=7e056e79a5', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=deee226017', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
