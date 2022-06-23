e10ada3dae21f55c823b0e3671b99a1a216b83e5
Martin Jansa <martin2.jansa@lgepartner.com>
2022-06-23 11:18:56 +0000

    webos-image: Update layer pins for 2022-06-23 from kirkstone
    oe-core as of 2022-06-17 05:02:15 -1000
    
    b2d10487f8 liberror-perl: Update sstate/equiv versions to clean cache
    7cdaac4a5a systemd: update 0008-add-missing-FTW_-macros-for-musl.patch
    6fa49ffcf2 archiver: don't use machine variables in shared recipes
    7211a6fb51 gcc-source: Fix incorrect task dependencies from ${B}
    025d22e9c9 git: fix override syntax in RDEPENDS
    6168dbc9b2 popt: fix override syntax in RDEPENDS
    8876f33f08 e2fsprogs: add alternatives handling of lsattr as well
    8c74bca4d6 oescripts: change compare logic in OEListPackageconfigTests
    d8e454b370 xxhash: fix build with gcc 12
    d8cb65b2ae archiver: use bb.note instead of echo
    3beb0f1d44 meson.bbclass: add cython binary to cross/native toolchain
            config
    5e6f4d0d3d devtool: Fix _copy_file() TypeError
    e150443701 selftest/multiconfig: Test that multiconfigs in separate
            layers works
    ea280917fc uboot-sign: Fix potential index error issues
    b85e29de4b vim: Upgrade 8.2.5034 -> 8.2.5083
    0f7a8359ba rootfs.py: close kernel_abi_ver_file
    9478e53995 buildhistory.bbclass: fix shell syntax when using dash
    741030ce32 systemd: Correct
            0001-pass-correct-parameters-to-getdents64.patch
    92a5fb9138 lttng-modules: fix shell syntax
    48aad396d9 lttng-modules: Fix build failure for 5.10.119+ and 5.15.44+
            kernel
    6c689e1b6f lttng-modules: fix build against 5.18-rc7+
    a4b49bdba4 linux-yocto/5.10: update to v5.10.119
    87f0b01ea3 linux-yocto/5.15: update to v5.15.44
    6b82997af3 linux-yocto/5.15: cfg/xen: Move x86 configs to separate file
    10bb8f0309 linux-yocto/5.15: Enable MDIO bus config
    88ce6da03c linux-yocto/5.10: update to v5.10.118
    3fb0fd6630 linux-yocto/5.15: update to v5.15.43
    8f04684552 linux-yocto/5.15: bpf: explicitly disable unpriv eBPF
            by default
    64ac42262d lttng-ust: upgrade 2.13.2 -> 2.13.3
    c4114d9bfb iso-codes: upgrade 4.9.0 -> 4.10.0
    fb4672a8b4 baremetal-image: fix broken symlink in do_rootfs
    e30ef926e4 bitbake.conf: Make TCLIBC and TCMODE lazy assigned
    e36a8a22bd glibc: Drop make-native dependency
    6b1bc1366b perl: Add dependency on make-native to avoid race issues
    4b694ded86 sanity: Switch to make 4.0 as a minimum version
    
    meta-virtualization as of 2022-06-20 12:09:51 -0400
    
    0d35c19 netns: adjust GOROOT, CGO_CFLAGS and CGO_LDFALGS
    9bfdd56 oci-image-tools: adjust GOROOT, CGO_CFLAGS and CGO_LDFALGS
    0d2ea6a riddler: adjust GOROOT, CGO_CFLAGS and CGO_LDFALGS
    ca033ba oci-runtime-tools: adjust GOROOT, CGO_CFLAGS and CGO_LDFALGS
    eea5c95 umoci: adjust CGO_CFLAGS and CGO_LDFLAGS settings
    4e19bc7 skopeo: clean up depends and fix CGO settings
    fe1bd21 docker-distribution: upgrade to 2.8.1
    cddd676 docker-distribution: fix build error on new hosts
    0642784 docker-distribution: fix the inehrit and systemd settings
    a2dd7d8 xen: Disable highmem on qemuarm
    15ed3e5 Revert "xen: Disable PCI on qemuarm with Xen"
    42c308c k3s: update recipe formatting and map googlesource to github

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=494fcfcb', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=6ade2bfdd9', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=b2d10487f8', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=fcc7d7eae8', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -69,7 +69,7 @@ webos_layers = [
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=6f3016d', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=b70d6bb', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=0d35c19', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.3.0,commit=f7c9337', ''),
 
