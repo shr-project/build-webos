6e6fb99a7be3f1bd81b65c7f8bed110b39ddda19
Martin Jansa <martin2.jansa@lgepartner.com>
2022-06-23 11:18:19 +0000

    webos-image: Update layer pins for 2022-06-23 as in WRO-7292
    oe-core as of 2022-06-15 06:40:10 -1000
    
    135adeb82c archiver: don't use machine variables in shared recipes
    14aa189c2e gcc-source: Fix incorrect task dependencies from ${B}
    28090d32c8 e2fsprogs: add alternatives handling of lsattr as well
    9eecfbfc95 oescripts: change compare logic in OEListPackageconfigTests
    fd1ed873ec archiver: use bb.note instead of echo
    b761150790 rootfs.py: close kernel_abi_ver_file
    27cd074d74 license.bbclass: Bound beginline and endline in
            copy_license_files()
    a1b73bc6ba alsa-plugins: fix libavtp vs. avtp packageconfig
    7777cc853d kernel-yocto.bbclass: Reset to exiting on non-zero return
            code at end of task
    2bba60d687 vim: Upgrade 8.2.5034 -> 8.2.5083
    179b862e97 openssh: Whitelist CVE-2021-36368
    cc657868d3 cups: fix CVE-2022-26691

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=0784db7d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=7e056e79a5', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=135adeb82c', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=deee226017', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.3',  34, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=dunfell,commit=dunfell-2022-06-15', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=dunfell,commit=dunfell-2022-06-23', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
