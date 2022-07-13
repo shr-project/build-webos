9b1c0184539b9be23937403d38c6397c8c7e44ae
Martin Jansa <martin2.jansa@lgepartner.com>
2022-07-13 07:49:57 +0000

    webos-image: Update layer pins for 2022-07-13 from kirkstone
    meta-oe as of 2022-07-04 09:26:10 -0400
    
    fb7b26b0fb fix(syslog-ng): warning about conf version
    1cd38eed74 strongswan: upgrade 5.9.5 -> 5.9.6
    e4c5f5b359 postgresql: upgrade 14.3 -> 14.4
    6ff027e8d3 exo: upgrade 4.16.3 -> 4.16.4
    7040cffbb0 networkmanager: fix build with enabled ppp
    d7aaee2c37 chrony: create /var/lib/chrony by systemd-tmpfiles
    3d47772a01 dnsmasq: Security fix CVE-2022-0934
    18767f177d libgpiod: move test dependencies to ptest package
    16621ac526 python3-pybluez: fix a runtime issue with python 3.10
    9c3976adde iperf: Set CVE_PRODUCT to "iperf_project:iperf"
    fad4c407c9 ntfs-3g-ntfsprogs: Set CVE_PRODUCT to "tuxera:ntfs-3g"
    3a019f2b2d tesseract-lang: switch from master branch to main
    14023da4de netserver: don't change permissions on /dev/null
    caab54d593 leveldb: switch from master branch to main

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=4667abcc92', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=fcc7d7eae8', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=fb7b26b0fb', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
