d7fced5259af7416dab4e2b4cb384814860cacbc
Martin Jansa <martin2.jansa@lgepartner.com>
2022-07-18 17:11:03 +0000

    webos-image: Update layer pins for 2022-07-18 from kirkstone
    oe-core as of 2022-07-13 11:54:16 -1000
    
    a4bfb5ceb5 libsoup: upgrade 3.0.6 -> 3.0.7
    a2871b43e3 glib-networking: upgrade 2.72.0 -> 2.72.1
    0d9a2d4d65 glib-2.0: upgrade 2.72.2 -> 2.72.3
    3903a248e1 weston: update 10.0.0 -> 10.0.1
    e008697900 gstreamer1.0-vaapi: upgrade 1.20.2 -> 1.20.3
    4f00cdf815 gstreamer1.0-rtsp-server: upgrade 1.20.2 -> 1.20.3
    05f62a8fbb gstreamer1.0-python: upgrade 1.20.2 -> 1.20.3
    e1dd7d56f5 gstreamer1.0-plugins-ugly: upgrade 1.20.2 -> 1.20.3
    4aee173335 gstreamer1.0-plugins-good: upgrade 1.20.2 -> 1.20.3
    c827ede3d6 gstreamer1.0-plugins-base: upgrade 1.20.2 -> 1.20.3
    0ead6c03c2 gstreamer1.0-plugins-bad: upgrade 1.20.2 -> 1.20.3
    e190d67fed gstreamer1.0-omx: upgrade 1.20.2 -> 1.20.3
    9b8a62dd39 gstreamer1.0-libav: upgrade 1.20.2 -> 1.20.3
    84b2d193fa gst-devtools: upgrade 1.20.2 -> 1.20.3
    2c177d00e1 gstreamer1.0: upgrade 1.20.2 -> 1.20.3
    2023e334ef binutils : stable 2.38 branch updates
    96f6badee7 glibc-tests: Avoid reproducibility issues
    05760b2957 glibc: stable 2.35 branch updates
    7616275703 openssl: update 3.0.4 -> 3.0.5
    640ccea311 vim: upgrade to 9.0.0021
    21b66e6ffe u-boot: fix CVE-2022-34835
    dc627cea88 tiff: backport the fix for CVE-2022-2056, CVE-2022-2057,
            and CVE-2022-2058
    537e7d323f harfbuzz: fix CVE-2022-33068
    83ab9f51f4 qemu: Avoid accidental libvdeplug linkage
    84390c353d qemu: Avoid accidental librdmacm linkage
    171fa2d709 qemu: add PACKAGECONFIG for capstone
    6db1eb67e7 ruby: add PACKAGECONFIG for capstone
    
    meta-oe as of 2022-07-14 09:06:29 -0700
    
    b9bbc38bfb protobuf-c: update to 1.4.1 fix CVE-2022-33070
    6d1dbf79a0 modemmanager: update to 1.18.8
    2dd643aa8c imagemagick: upgrade 7.0.10-25 -> 7.0.10-62
    4eaa3091e6 ntfs-3g-ntfsprogs: upgrade to 2022.5.17
    1a09e4ffd2 php: upgrade 8.1.6 -> 8.1.7
    e5b177aea4 cyrus-sasl: CVE-2022-24407 failure to properly escape SQL
            input allows an attacker to execute arbitrary SQL commands
    bf2822d599 dlt-daemon: upgrade to commit 6a3bd901d8 to fix
            CVE-2022-31291
    97375c7712 emlog: ignore unrelated CVEs
    c455cbab36 freeradius: mutlilib fixes
    
    meta-virtualization as of 2022-07-18 08:44:32 -0400
    
    33fb354 image-oci-umoci: add parameter for stop signal
    6c6b928 image-oci-umoci: properly handle tags other than latest
    90f42b3 fuse-overlayfs: Fix buffer overflow bug on workdir path
    39ef27d podman: Add support for rootless mode
    c30b03a podman: Silence docker emulation warnings
    55e9f7a slirp4netns: Runtime recommend the tun kernel module

--- weboslayers.py
+++ weboslayers.py
@@ -60,16 +60,16 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=b8fd6f5d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=4667abcc92', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=a4bfb5ceb5', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=fb7b26b0fb', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=b9bbc38bfb', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=03fc4eb', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=d81de7a', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=33fb354', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.3.0,commit=f7c9337', ''),
 
