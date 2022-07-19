dd21db675e0faeb083842c516afbc627c98de1cd
Martin Jansa <martin2.jansa@lgepartner.com>
2022-07-19 09:30:49 +0000

    webos-image: Update layer pins for 2022-07-19 from master
    meta-oe as of 2022-07-18 10:03:12 -0700
    
    0c2e416567 Revert "python3-antlr4-runtime: Inherit setuptools3 instead
            of python_setuptools_build_meta"
    46a484bb45 Revert "python3-pycups: Inherit setuptools3 instead of
            python_setuptools_build_meta"
    63700a0c57 Revert "catfish: Inherit setuptools3 instead of
            python_setuptools_build_meta"
    334a04aba8 poco: Link with libatomic on mips
    de4097f230 postgresql: ignore unrelated CVE
    1642bfcb07 php: ignore patched CVEs
    ed904e6541 mongodb: ignore unrelated CVEs
    efa12676dd meta-oe: ignore patched CVEs
    261465eb6e libplist: ignore patched CVEs
    76c5388966 python3-jsonrpcserver: added the python3-oslash rdepends
    1b3be50ad2 python3-oslash: added the oslash Python package
    ecbcc3bfc0 python3-jsonrpcclient: Added the jsonrpcclient Python
            package
    4012175df3 python3-aspectlib: updated the summary and added a
            description.
    88541e824d xscreensaver: Upgrade to 6.04

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=5260d81dab', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=cb7d3afba8', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=0c2e416567', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
