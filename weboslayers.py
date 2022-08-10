2f3cd6a70b6c2e45f83aecc001beb0830b363a5e
Martin Jansa <martin2.jansa@lgepartner.com>
2022-08-10 20:06:40 +0000

    webos-image: Update layer pins for 2022-08-10 from kirkstone
    oe-core as of 2022-08-10 14:59:51 +0100
    
    2cafa6ed5f build-appliance-image: Update to kirkstone head revision
    455acda350 lttng-modules: Fix build failure for kernel v5.15.58
    5fb62be8e6 lttng-modules: update 2.13.3 -> 2.13.4
    6216cc2fb8 linux-yocto/5.15: fix reproducibility issues
    f9c284ce5b linux-yocto/5.15: update to v5.15.59
    2bca0a8c74 linux-yocto-rt/5.15: update to -rt48 (and fix -stable merge)
    5a6bc91cf3 linux-yocto/5.15: update to v5.15.58
    373f857775 linux-yocto/5.10: update to v5.10.135
    c6435f0fad glibc: revert one upstream change to work around broken
            DEBUG_BUILD build
    ec5de254e4 glibc : stable 2.35 branch updates

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=b8fd6f5d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=c33eb7fb1d', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2cafa6ed5f', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=8f2dc10234', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
