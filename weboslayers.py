fd7545df61b1648c3d91775520af314e3bbc16ad
Martin Jansa <martin2.jansa@lgepartner.com>
2022-09-04 12:26:26 +0000

    webos-image: Update layer pins for 2022-09-04 from kirkstone
    oe-core as of 2022-09-03 13:09:42 +0100
    
    2363d69d68 gcr: Define _GNU_SOURCE
    b59027f32e apr: Use correct strerror_r implementation based on
            libc type
    9c9dd76aee pinentry: enable _XOPEN_SOURCE on musl for wchar usage
            in curses
    9cc6d5760f watchdog: Include needed system header for function decls
    37322e0009 xinetd: Pass missing -D_GNU_SOURCE
    60b55c1dde parselogs: Ignore xf86OpenConsole error
    8acf5a1b5d util-linux: Remove --enable-raw from EXTRA_OECONF
    e5ce332288 shadow: Avoid nss warning/error with musl
    84b2e9a6c1 rootfspostcommands.py: Cleanup subid backup files generated
            by shadow-utils
    5cdc9c1809 shadow: Enable subid support
    cb51e97f66 lttng-modules: fix build for kernel 5.10.137
    cfa124b8a4 linux-yocto/5.10: update to v5.10.137
    3dbf621c29 linux-yocto/5.10: update to v5.10.136
    26a6fdef2a linux-yocto: Fix COMPATIBLE_MACHINE regex match
    6b41953720 linux-yocto/5.15: update to v5.15.62
    9d8d44dc6c linux-yocto/5.15: update to v5.15.60
    75c4b8361e bind: upgrade 9.18.4 -> 9.18.5
    fdc82b2314 sqlite: fix CVE-2022-35737
    
    bitbake as of 2022-09-02 11:44:47 +0100
    
    ce9fe701 utils: Pass lock argument in fileslocked

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=bc85c044', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=ce9fe701', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=92f122e0c1', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2363d69d68', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=acbe748798', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
