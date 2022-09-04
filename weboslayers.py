45b9d92458bbdd34f40cd429afe0a148ed9ce1b8
Martin Jansa <martin2.jansa@lgepartner.com>
2022-09-04 12:25:50 +0000

    webos-image: Update layer pins for 2022-09-04 from dunfell
    oe-core as of 2022-08-29 05:02:16 -1000
    
    345193f36d relocate_sdk.py: ensure interpreter size error causes
            relocation to fail
    653b03aa6f cryptodev-module: fix build with 5.11+ kernels
    2663435831 linux-yocto/5.4: update to v5.4.210
    8b69eafa5c wireless-regdb: upgrade 2022.06.06 -> 2022.08.12
    b0a0abbcc5 tzdata: upgrade 2022a -> 2022b
    5dd5130f9b mobile-broadband-provider-info: upgrade 20220511 -> 20220725
    169537045e vim: Upgrade 9.0.0115 -> 9.0.0242
    4cc681fd66 cve-check: Don't use f-strings
    b9312041e4 libxml2: Add fix for CVE-2016-3709
    9b3420c9a9 golang: CVE-2022-32189 a denial of service
    2c4fb77f41 golang: fix CVE-2022-30635 and CVE-2022-32148
    9ffaae8877 golang: fix CVE-2022-30632 and CVE-2022-30633
    6813a265c7 golang: fix CVE-2022-30629 and CVE-2022-30631
    462d4a55a4 libtiff: CVE-2022-34526 A stack overflow was discovered
    
    bitbake as of 2022-09-02 11:45:17 +0100
    
    048d682b utils: Pass lock argument in fileslocked

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=3b719e8e', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=048d682b', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=a3cba15142', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=345193f36d', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=f22bf6efaa', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
