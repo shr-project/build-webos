4e5565d3444447677d502dee4c18a3dff335ce1b
Martin Jansa <martin2.jansa@lgepartner.com>
2022-09-23 09:09:13 +0000

    webos-image: Update layer pins for 2022-09-23 from master
    oe-core as of 2022-09-22 21:37:56 +0100
    
    d6b54f37aa vim: Upgrade 9.0.453 -> 9.0.541
    943d28a339 qemu: fix CVE-2022-2962
    09bcf6d2a6 qemu: re-add the fix for CVE-2022-1050
    35bec42c40 binutils: handle symlink files for gprofng
    bac2c95f63 gcompat: Add __getauxval wrapper needed on aarch64
    6e277bc261 virglrenderer: upgrade 0.9.1 -> 0.10.3
    a85d0566c0 mesa: upgrade 22.1.6 -> 22.2.0
    2f1a233e75 oeqa/runtime/virgl: Update test to match new mesa version
    
    bitbake as of 2022-09-22 21:40:21 +0100
    
    3f23140f bitbake-user-manual: mention pydevshell in OE
    
    meta-oe as of 2022-09-22 03:51:38 -0700
    
    2239a3063c emacs: Update to version 28.2
    03a565a7bf cglm: New recipe for cglm-0.8.5
    9c136fcd9e Make graph dependencies optional
    ca7447990d fwupd: Replace python3.7 patch with upstreamed one
    fc8209e8bc ot-br-posix: Add dep to ipset as used by firewall
    a1965d0972 python3-h5py: fix wrong file driver version
    9d03f96bb1 python3-termcolor: Upgrade 1.1.0 -> 2.0.1
    1a2556e97d python3-regex: upgrade 2022.8.17 -> 2022.9.13
    5621e9d7a8 python3-pyzmq: upgrade 23.2.1 -> 24.0.0
    7a60fda35c python3-pyproj: upgrade 3.3.1 -> 3.4.0
    c95f9bc073 python3-pycocotools: upgrade 2.0.4 -> 2.0.5
    63477f724e python3-protobuf: upgrade 4.21.5 -> 4.21.6
    7450ae51a5 python3-jsonrpcserver: upgrade 5.0.8 -> 5.0.9
    c5533c184e python3-grpcio: upgrade 1.48.1 -> 1.49.0
    bb27cf1c1e python3-grpcio-tools: upgrade 1.48.1 -> 1.49.0
    f5420bc37a python3-google-api-python-client: upgrade 2.60.0 -> 2.61.0
    6326b33239 python3-google-api-core: upgrade 2.10.0 -> 2.10.1
    2b28c5dd24 python3-flask-socketio: upgrade 5.3.0 -> 5.3.1
    8022ae6264 python3-fasteners: upgrade 0.17.3 -> 0.18
    163cce5191 python3-eth-typing: upgrade 3.1.0 -> 3.2.0
    e4795b05c3 python3-cantools: upgrade 37.1.2 -> 37.2.0
    13cff82b13 python3-cachecontrol: upgrade 0.12.11 -> 0.12.12
    0965806670 xwud: upgrade 1.0.5 -> 1.0.6
    9df3e793b8 xmag: upgrade 1.0.6 -> 1.0.7
    719fea0f0f xlsfonts: upgrade 1.0.6 -> 1.0.7
    5418003de6 xkbutils: upgrade 1.0.4 -> 1.0.5
    4050fcaa86 xclock: upgrade 1.0.9 -> 1.1.1
    cd9d926b8c smarty: upgrade 4.2.0 -> 4.2.1
    f4c137418c setxkbmap: upgrade 1.3.2 -> 1.3.3
    5b0583d4e9 passwdqc: fix the libpasswdqc FILES setting
    8308d1d6fa neon: upgrade 0.32.2 -> 0.32.4
    afd774f0ed libxmlb: upgrade 0.3.9 -> 0.3.10
    eac0f028bd libpfm4: upgrade 4.11.0 -> 4.12.0
    414fa782be libjcat: upgrade 0.1.11 -> 0.1.12
    cc744accb8 libcec: fix runtime dependencies for ${PN}-examples
    7cd43b79c5 libblockdev: upgrade 2.27 -> 2.28
    8e79d5a737 iwd: upgrade 1.29 -> 1.30
    20e3e937f1 iceauth: upgrade 1.0.8 -> 1.0.9
    f3631ebba3 ctags: upgrade 5.9.20220904.0 -> 5.9.20220911.0
    6f7de16466 bats: upgrade 1.7.0 -> 1.8.0
    31a597956d appstream-glib: fix manpage generation
    2abb349278 libadwaita: upgrade 1.1.5 -> 1.2.0

--- weboslayers.py
+++ weboslayers.py
@@ -58,11 +58,11 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=19f82650', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=3f23140f', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=b9348a0937', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=d6b54f37aa', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=6a06553cef', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=2239a3063c', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
