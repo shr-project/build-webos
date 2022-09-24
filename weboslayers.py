03927a5d23e405aa3f6b75e9d5ce6a8de9d67388
Martin Jansa <martin2.jansa@lgepartner.com>
2022-09-24 09:50:09 +0000

    webos-image: Update layer pins for 2022-09-24 from dunfell
    oe-core as of 2022-09-22 04:40:18 -1000
    
    ef38f7acee linux-yocto/5.4: update to v5.4.213
    ddfc3eebd9 linux-yocto/5.4: update to v5.4.212
    4efa4490be expat: Fix CVE-2022-40674
    b42ea2b7f9 sqlite3: Fix CVE-2021-20223
    7fdd4d2dc0 subversion: fix CVE-2021-28544
    f547c9610f qemu: Add PACKAGECONFIG for brlapi
    9badcf0261 qemu: Define libnfs PACKAGECONFIG
    16a6e8530c qemu: fix and ignore several CVEs
    86334559e3 connman: CVE-2022-32293 man-in-the-middle attack against
            a WISPR HTTP
    2541fd0d0e sqlite3: Fix CVE-2020-35527
    ced472cf1d sqlite3: Fix CVE-2020-35525

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=048d682b', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=46ba253059', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=ef38f7acee', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=6792ebdd96', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
