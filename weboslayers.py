f3983154ac3b065f56be561791385b3fe4e146d0
Martin Jansa <martin2.jansa@lgepartner.com>
2022-10-19 16:13:08 +0000

    webos-image: Update layer pins for 2022-10-19 from langdale
    meta-oe as of 2022-10-18 11:49:35 -0700
    
    8073ec2275 Add recipe for Perfetto
    923635099d grpc: Update to 1.50.x release
    050b5414b3 xfce4-settings: upgrade 4.16.2 -> 4.16.3
    36f92899a6 Add condition for libusbgx-examples
    aca28022cc unbound: upgrade 1.16.3 -> 1.17.0
    a758d95ebb python3-zopeinterface: upgrade 5.4.0 -> 5.5.0
    6a83049859 tio: upgrade 2.0 -> 2.1
    bfa8892763 yelp: upgrade 42.1 -> 42.2
    41db40c805 python3-stevedore: upgrade 4.0.0 -> 4.0.1
    8308650fdd python3-pywbem: upgrade 1.4.1 -> 1.5.0
    36dc55c427 python3-pymodbus: upgrade 2.5.3 -> 3.0.0
    93e9fe9f7a python3-google-auth: upgrade 2.11.1 -> 2.12.0
    fbea86469f python3-google-api-python-client: upgrade 2.62.0 -> 2.64.0
    7126279eb4 python3-google-api-core: upgrade 2.10.1 -> 2.10.2
    369ab518e1 python3-gevent: upgrade 22.8.0 -> 22.10.1
    034fbd64b1 python3-absl: upgrade 1.2.0 -> 1.3.0
    1e5473508a tesseract: upgrade 4.1.3 -> 5.2.0
    5b6ccc07b5 cli11: upgrade 2.2.0 -> 2.3.0
    e5ecbd204b blueman: upgrade 2.3.2 -> 2.3.4
    833c5f30ff xfstests: upgrade 2022.09.25 -> 2022.10.09
    c6fddf41de nbdkit: upgrade 1.33.1 -> 1.33.2
    c2b5393b55 libwacom: upgrade 2.4.0 -> 2.5.0
    d067d2e48e gst-editing-services: upgrade 1.20.3 -> 1.20.4
    f71bd5172a freerdp: upgrade 2.8.0 -> 2.8.1
    c57b93b6a6 debootstrap: upgrade 1.0.127 -> 1.0.128
    c43afad630 ctags: upgrade 5.9.20221002.0 -> 5.9.20221009.0
    7794625627 cppzmq: upgrade 4.8.1 -> 4.9.0
    0afaceb22a broadcom-bt-firmware: upgrade
    3ab22400d1 abseil-cpp: Upgrade to head on 2022-10-14
    3ba38a79f7 uriparser: Upgrade 0.9.6 -> 0.9.7
    4206fbd4aa python3-distro: Upgrade 1.7.0 -> 1.8.0
    e14f87b812 python3-imageio: Upgrade 2.22.1 -> 2.22.2
    528d29b4a9 double-conversion: added double-conversion lib
    0c1f379f42 md4c: added md4c lib
    4a0e41431e conntrack-tools: Use canonical shell spacing
    4d8d56d0e7 conntrack-tools: Add PACKAGECONFIGs for build options
    80b68df709 conntrack-tools: Upgrade 1.4.6 -> 1.4.7
    349c26d877 crucible: Add recipe
    72273d28e2 wireplumber: upgrade 0.4.11 -> 0.4.12
    2c71d7f098 pipewire: upgrade 0.3.57 -> 0.3.59
    feacae5c38 onboard: remove
    442909de28 v4l-utils: Support building without NLS
    5b8ae45ee5 remmina: Update to 1.4.27
    c8d9407eec redis: upgrade 7.0.4 to 7.0.5
    
    Signed-off-by: Martin Jansa <martin2.jansa@lgepartner.com>

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=744a227784', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=6529e5f963', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=8073ec2275', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -73,7 +73,7 @@ webos_layers = [
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4.0,commit=8370a30', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=langdale-2022-09-30', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=langdale-2022-10-19', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=fc5f80a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
