9660ace4a4eee3d2f0ff7fc8b54f3359c251577c
Martin Jansa <martin2.jansa@lgepartner.com>
2022-11-01 07:15:09 +0000

    webos-image: Update layer pins for 2022-11-01 from kirkstone as in WRO-14882
    oe-core as of 2022-10-29 16:32:59 +0100
    
    fbdf93f43f build-appliance-image: Update to kirkstone head revision
    82237927db Revert "lttng-tools: Upgrade 2.13.4 -> 2.13.8"
    6ad25304ab classes: files: Extend overlayfs-etc class
    a1e0ddbb7b files: overlayfs-etc: refactor preinit template
    43badfadb9 git: upgrade 2.35.4 -> 2.35.5
    ab154b2441 glibc: stable 2.35 branch updates.
    3ea33ca1a8 binutils: stable 2.38 branch updates
    de59742490 linux-yocto/5.10: update to v5.10.149
    68f28c7cba linux-yocto/5.10: update to v5.10.147
    
    meta-updater as of 2022-10-31 13:42:33 +0100
    
    c74a067 Merge pull request #59 from quaresmajose/aktualizr-hwid
    ae0fbc5 aktualizr-hwid: cleanup and refactor
    891a6db aktualizr-hwid: make the installed toml complaint

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=c90d5749', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=4781fee6ae', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=fbdf93f43f', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=744a4b6eda', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -68,7 +68,7 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=f42fa04', ''),
+('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=c74a067', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=9a487c1', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.3.1,commit=39eeba2', ''),
