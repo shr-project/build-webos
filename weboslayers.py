fe899c1284fc061453cdd9f086f5a0885fd6f036
Martin Jansa <martin2.jansa@lgepartner.com>
2022-11-05 08:41:12 +0000

    webos-image: Update layer pins for 2022-11-05 from master
    oe-core as of 2022-11-03 07:45:23 +0000
    
    51e3d2ecef valgrind: update to 3.20.0
    eb00d50e5d weston: update 10.0.2 -> 11.0.0
    1243d6afc0 lttng-modules: upgrade 2.13.5 -> 2.13.7
    4fed5eee37 patchelf: upgrade 0.15.0 -> 0.16.1
    7636a2b808 cargo-update-recipe-crates: small improvements
    35d65248c2 python3-sphinx: upgrade 5.2.3 -> 5.3.0
    c28d3f1ccd python3-zipp: upgrade 3.9.0 -> 3.10.0
    b78ce30f0f python3-pytz: upgrade 2022.4 -> 2022.6
    f31ea27844 python3-more-itertools: upgrade 8.14.0 -> 9.0.0
    1071b68375 python3-hypothesis: upgrade 6.56.1 -> 6.56.4
    30f77f222f python3-pytest-subtests: upgrade 0.8.0 -> 0.9.0
    8ae3b896b0 python3-pytest: upgrade 7.1.3 -> 7.2.0
    893fc6a99f python3-psutil: upgrade 5.9.2 -> 5.9.3
    3c0c033127 python3-cryptography{-vectors}: 38.0.1 -> 38.0.3
    509a684b74 python3-bcrypt: upgrade 4.0.0 -> 4.0.1
    e1587fbac4 linux-yocto-dev: add qemuarmv5
    72b3b79ad8 bluez5: Point hciattach bcm43xx firmware search path
            to /lib/firmware
    4bbfd74270 rust: submit a rewritten version of crossbeam_atomic.patch
            upstream
    fd33d169a5 go: update 1.19.2 -> 1.19.3
    6050d1f74c archiver: avoid using machine variable as it breaks
            multiconfig
    0059a5c9c0 oeqa/selftest/archiver: Add multiconfig test for shared
            recipes
    98b058a039 bc: Add ptest.
    a918ea5645 rust-common.bbclass: use built-in rust targets for -native
            builds
    bb6ebb9956 oeqa/runtime/dnf: rewrite test_dnf_installroot_usrmerge
    e244228730 oeqa/target/ssh: add ignore_status argument to run()
    49ad6f0314 python3-mako: upgrade 1.2.2 -> 1.2.3
    a69ea1f7db openssl: Upgrade 3.0.5 -> 3.0.7
    ce6491b900 gptfdisk: remove warning message from target system
    1b79d63a07 go: update 1.19 -> 1.19.2
    a257a67427 expat: upgrade to 2.5.0
    f6d917bd0f vim: upgrade 9.0.0614 -> 9.0.0820
    9555a7dc76 libffi: submit patch upstream
    40d24b0b5c go: submit patch upstream
    0f758719ad ovmf: correct patches status
    6ecd02e4af argp-standalone: replace with a maintained fork
    e8f0e3a012 kea: submit patch upstream
    1552df5351 cmake: drop qt4 patches
    bec62455d9 lttng-tools: submit determinism.patch upstream
    8a929c75d9 tcl: correct upstream version check
    9f37e5b83d tcl: correct patch status
    4269cfcd6c groff: submit patches upstream
    1071e2fdd2 python3-cryptography: convert to cargo-update-recipe-crates
            class
    f1ebc71d9c python3-bcrypt: convert to use cargo-update-recipe-crates
            class.
    9eee363112 cargo-update-recipe-crates.bbclass: add a class to generate
            SRC_URI crate lists from Cargo.lock
    df55dced4b curl: Update 7.85.0 to 7.86.0
    c7a92180b2 cargo_common.bbclass: Fix typos
    
    meta-oe as of 2022-11-01 10:13:47 -0700
    
    6ebff843cc ipmitool: fix typo in .bb file's comments, using = instead
            of =?
    5bff9c4298 grpc: upgrade 1.50.0 -> 1.50.1
    00895a31a8 spice-gtk: add recipe
    b7033c7ec2 jwt-cpp: add recipe
    744942c4f5 thingsboard-gateway: upgrade 3.1 -> 3.2
    0239a702d3 poco: upgrade 1.12.2 -> 1.12.3
    30444c8a59 function2: upgrade 4.2.1 -> 4.2.2
    aa5ea92a4a flatbuffers: upgrade 22.9.29 -> 22.10.26
    e72998c004 ctags: upgrade 5.9.20221016.0 -> 5.9.20221023.0
    e173674952 cglm: upgrade 0.8.5 -> 0.8.7
    a9334a5cba wolfssl: upgrade 5.5.1 -> 5.5.2
    db39f8875f tracker: upgrade 3.4.0 -> 3.4.1
    8b70c195b3 tracker-miners: upgrade 3.2.1 -> 3.4.1
    
    meta-virtualization as of 2022-11-02 15:31:33 -0400
    
    9f41aca python3-colorama: sync with meta-openembedded

--- weboslayers.py
+++ weboslayers.py
@@ -60,16 +60,16 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=d2cb388f', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=9acd3f210f', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=51e3d2ecef', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=3f20244401', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=6ebff843cc', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=1538ed8', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=3b63ed0', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=9f41aca', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4.0,commit=8370a30', ''),
 
