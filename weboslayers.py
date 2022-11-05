6da9e717575af25871d05953ccfa5e380744b2e1
Martin Jansa <martin2.jansa@lgepartner.com>
2022-11-05 08:39:43 +0000

    webos-image: Update layer pins for 2022-11-05 from kirkstone
    oe-core as of 2022-11-04 13:13:27 +0000
    
    555fd2848b vim: upgrade 9.0.0614 -> 9.0.0820
    1104d02f9b vim: Upgrade 9.0.0598 -> 9.0.0614
    09b8ff8d23 wayland: fix CVE-2021-3782
    791fe354e5 expat: backport the fix for CVE-2022-43680
    cd94ed0121 tiff: fix CVE-2022-2953
    88e1917dbf lighttpd: fix CVE-2022-41556
    78220bd59d openssl: Upgrade 3.0.5 -> 3.0.7
    098e89e89e openssl: CVE-2022-3358 Using a Custom Cipher with NID_undef
            may lead to NULL encryption
    850ccc2a30 openssl: Fix SSL_CERT_FILE to match ca-certs location
    f51c9af925 openssl: export necessary env vars in SDK

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=c90d5749', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=fbdf93f43f', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=555fd2848b', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=744a4b6eda', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
