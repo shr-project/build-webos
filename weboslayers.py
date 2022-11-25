34fabaaa5fceb7a196abfe6b292848c6a9e22edc
Martin Jansa <martin2.jansa@lgepartner.com>
2022-11-25 07:05:00 +0000

    webos-image: Update layer pins for 2022-11-25 from langdale
    oe-core as of 2022-11-19 03:59:47 -1000
    
    e0dc0f4a15 wic: make ext2/3/4 images reproducible
    fbc6c1dc6c gcc-source: Ensure deploy_source_date_epoch sstate hash
            doesn't change
    bce5a6269d gcc-source: Drop gengtype manipulation
    a431f45ad4 gcc-source: Fix gengtypes race
    2c14f7d707 gcc-shared-source: Fix source date epoch handling
    5c8e0e641c insane: add codeload.github.com to src-uri-bad check
    69db1b3457 kernel.bbclass: Include randstruct seed assets in
            STAGING_KERNEL_BUILDDIR
    1859eaba86 systemd: Consider PACKAGECONFIG in RRECOMMENDS
    40e8a444b4 libuv: fixup SRC_URI
    5ae95506b7 bitbake.conf: Drop export of SOURCE_DATE_EPOCH_FALLBACK
    ede7452e6d get_module_deps3.py: Check attribute '__file__'
    7f742e0f7a bluez5: Point hciattach bcm43xx firmware search path
            to /lib/firmware
    3a4e447dd9 rust: submit a rewritten version of crossbeam_atomic.patch
            upstream
    a71a534383 archiver: avoid using machine variable as it breaks
            multiconfig
    6f5e10d682 oeqa/runtime/dnf: rewrite test_dnf_installroot_usrmerge
    3ab83b07ba gptfdisk: remove warning message from target system
    7d1ff6602e libffi: submit patch upstream
    91371976a7 ovmf: correct patches status
    bf79cd69a6 kea: submit patch upstream
    8167b87695 lttng-tools: submit determinism.patch upstream
    d480c9842c tcl: correct patch status
    cd49ff1795 groff: submit patches upstream
    48e7851721 cargo_common.bbclass: Fix typos
    2ee8bfb667 sudo: upgrade 1.9.11p3 -> 1.9.12p1
    231c073f57 Revert "sudo: backport fix for CVE-2022-43995"
    9f78a2b3fd libxcrypt: upgrade 4.4.28 -> 4.4.30
    d7570afa41 socat: upgrade 1.7.4.3 -> 1.7.4.4
    b8d39056f5 inetutils: upgrade 2.3 -> 2.4
    14b6bcb46a bind: upgrade 9.18.7 -> 9.18.8
    9d80f9260d lttng-modules: upgrade 2.13.5 -> 2.13.7
    ce8d8fb105 python3-mako: upgrade 1.2.2 -> 1.2.3
    ff2436215e go: update 1.19.2 -> 1.19.3
    a5cfa272ee go: update 1.19 -> 1.19.2
    e917306e0d go: submit patch upstream
    5544b66484 expat: upgrade to 2.5.0
    
    bitbake as of 2022-11-22 09:23:09 -1000
    
    3b2d1331 runqueue: Fix race issues around hash equivalence and
            sstate reuse

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=138dd788', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=3b2d1331', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=9237ffc4fe', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=e0dc0f4a15', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=langdale,commit=c5668905a6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
