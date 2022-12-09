24879b7926a6380437dbc5cc7f3eb1b32c26cfd5
Martin Jansa <martin2.jansa@lgepartner.com>
2022-12-09 06:46:59 +0000

    weboslayers, rebase: Use jansa/langdale branches
    Change-Id: I505bc67665f38ebdf7188f3c63b4a37833b9cb7a

--- weboslayers.py
+++ weboslayers.py
@@ -58,32 +58,31 @@ Machines = ['qemux86', 'qemux86copy', 'qemux86-64', 'qemuarm', 'raspberrypi3', '
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://git.openembedded.org/bitbake-contrib',           'branch=jansa/2.0', 'bitbake'),
+('bitbake',                   -1, 'https://git.openembedded.org/bitbake-contrib',           'branch=jansa/2.2', 'bitbake'),
 
-('meta',                       5, 'https://git.openembedded.org/openembedded-core-contrib', 'branch=jansa/kirkstone', 'oe-core'),
+('meta',                       5, 'https://git.openembedded.org/openembedded-core-contrib', 'branch=jansa/langdale', 'oe-core'),
 
-('meta-oe',                   10, 'https://git.openembedded.org/meta-openembedded-contrib', 'branch=jansa/kirkstone', 'meta-oe'),
+('meta-oe',                   10, 'https://git.openembedded.org/meta-openembedded-contrib', 'branch=jansa/langdale', 'meta-oe'),
 ('meta-multimedia',           11, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 ('meta-networking',           12, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 ('meta-python',               13, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/shr-project/meta-clang.git',          'branch=jansa/kirkstone', ''),
-('meta-virtualization',       16, 'https://github.com/shr-project/meta-virtualization',     'branch=jansa/kirkstone', ''),
+('meta-clang',                15, 'https://github.com/shr-project/meta-clang.git',          'branch=jansa/langdale', ''),
+('meta-virtualization',       16, 'https://github.com/shr-project/meta-virtualization',     'branch=jansa/langdale', ''),
 
-('meta-qt6',                  20, 'https://github.com/shr-project/meta-qt6',                'branch=jansa/kirkstone', ''),
+('meta-qt6',                  20, 'https://github.com/shr-project/meta-qt6',                'branch=jansa/langdale', ''),
 
-('meta-webos-backports-4.1',  30, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-4.2',  31, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-4.3',  32, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-5.0',  33, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-5.1',  34, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=jansa/kirkstone', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=jansa/langdale', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/shr-project/meta-raspberrypi',        'branch=jansa/kirkstone', ''),
+('meta-raspberrypi',          50, 'https://github.com/shr-project/meta-raspberrypi',        'branch=jansa/langdale', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-security',             76, 'https://github.com/shr-project/meta-security',           'branch=jansa/kirkstone', ''),
+('meta-security',             76, 'https://github.com/shr-project/meta-security',           'branch=jansa/langdale', ''),
 ]
