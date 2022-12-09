89cebe1ca1ae6a803a06b5022c8c62d14a4abcf1
Martin Jansa <martin2.jansa@lgepartner.com>
2022-12-09 06:47:10 +0000

    weboslayers, rebase: Use jansa/master branches
    Change-Id: I505bc67665f38ebdf7188f3c63b4a37833b9cb7a

--- weboslayers.py
+++ weboslayers.py
@@ -58,26 +58,26 @@ Machines = ['qemux86', 'qemux86copy', 'qemux86-64', 'qemuarm', 'raspberrypi3', '
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://git.openembedded.org/bitbake-contrib',           'branch=jansa/2.12', 'bitbake'),
+('bitbake',                   -1, 'https://git.openembedded.org/bitbake-contrib',           'branch=jansa/master', 'bitbake'),
 
-('meta',                       5, 'https://git.openembedded.org/openembedded-core-contrib', 'branch=jansa/walnascar', 'oe-core'),
+('meta',                       5, 'https://git.openembedded.org/openembedded-core-contrib', 'branch=jansa/master', 'oe-core'),
 
-('meta-oe',                   10, 'https://git.openembedded.org/meta-openembedded-contrib', 'branch=jansa/walnascar', 'meta-oe'),
+('meta-oe',                   10, 'https://git.openembedded.org/meta-openembedded-contrib', 'branch=jansa/master', 'meta-oe'),
 ('meta-multimedia',           11, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 ('meta-networking',           12, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 ('meta-python',               13, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://git.openembedded.org/meta-openembedded-contrib', '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/shr-project/meta-clang.git',          'branch=jansa/walnascar', ''),
-('meta-virtualization',       16, 'https://github.com/shr-project/meta-virtualization',     'branch=jansa/walnascar', ''),
+('meta-clang',                15, 'https://github.com/shr-project/meta-clang.git',          'branch=jansa/master', ''),
+('meta-virtualization',       16, 'https://github.com/shr-project/meta-virtualization',     'branch=jansa/master', ''),
 
-('meta-qt6',                  20, 'https://github.com/shr-project/meta-qt6',                'branch=jansa/walnascar', ''),
+('meta-qt6',                  20, 'https://github.com/shr-project/meta-qt6',                'branch=jansa/master', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=jansa/walnascar', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=jansa/master', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/shr-project/meta-raspberrypi',        'branch=jansa/walnascar', ''),
+('meta-raspberrypi',          50, 'https://github.com/shr-project/meta-raspberrypi',        'branch=jansa/master', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-security',             76, 'https://github.com/shr-project/meta-security',           'branch=jansa/walnascar', ''),
+('meta-security',             76, 'https://github.com/shr-project/meta-security',           'branch=jansa/master', ''),
 ]
