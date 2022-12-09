414a307f01c0b2a002ffea57e36a26c508316786
Martin Jansa <martin2.jansa@lgepartner.com>
2022-12-09 06:31:58 +0000

    webos-image: Update layer pins for 2022-12-09 from kirkstone as in WRP-3386
    oe-core as of 2022-11-30 05:51:07 -1000
    
    c0f3da88a9 mirrors.bbclass: update CPAN_MIRROR
    26c1338f5a dhcpcd: fix to work with systemd
    66d85b2d84 resolvconf: make it work
    cb4ad96a5d sstatesig: emit more helpful error message when not finding
            sstate manifest
    6c477dffa8 linux-firmware: add new fw file to ${PN}-qcom-adreno-a530
    ce3381ea0d linux-firmware: upgrade 20221012 -> 20221109
    640ac18b2d kernel.bbclass: make KERNEL_DEBUG_TIMESTAMPS work at rebuild
    a592314318 kern-tools: integrate ZFS speedup patch
    8d06b6b130 linux-yocto/5.15: fix CONFIG_CRYPTO_CCM mismatch warnings
    522ddd3600 linux-yocto/5.15: update to v5.15.78
    3660f67312 linux-yocto/5.15: update to v5.15.76
    6a53f59d89 linux-yocto/5.15: update to v5.15.74
    300216ca35 dbus: upgrade 1.14.0 -> 1.14.4
    399d4986a7 libpam: fix CVE-2022-28321
    212dd2ce83 dropbear: fix CVE-2021-36369
    4754f33d7e curl: Fix CVE-2022-42915
    e6796b4265 curl: Fix CVE-2022-42916
    518bea85c9 curl: Fix CVE-2022-32221
    97ad715419 tiff: add CVE tag to
            b258ed69a485a9cfb299d9f060eb2a46c54e5903.patch
    d4b231e1ba tiff: Security fix for CVE-2022-3970
    ad0780059a tiff: fix a number of CVEs
    535c814259 tiff: refresh with devtool
    0fc6693ab4 grub2: backport patch to fix CVE-2022-2601 CVE-2022-3775
    
    meta-virtualization as of 2022-12-08 15:33:51 -0500
    
    a0d0f4f upx: Use git fetcher instead of gitsm
    8c6de82 upx: update to latest devel release

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=72a3afd9', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=5b2ee67e3a', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=c0f3da88a9', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=50d4a8d2a9', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -70,7 +70,7 @@ webos_layers = [
 
 ('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=013b5ee', ''),
 ('meta-updater',              16, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=ffbbc2b', ''),
-('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=e68bfe7', ''),
+('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=a0d0f4f', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4.0,commit=8370a30', ''),
 
