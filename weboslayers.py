1363891f03f909bd767055102d6bafbb82329abe
Martin Jansa <martin2.jansa@lgepartner.com>
2023-01-06 19:41:50 +0000

    webos-image: Update layer pins for 2023-01-06 from kirkstone as in WRP-6164
    oe-core as of 2023-01-06 17:30:18 +0000
    
    5fa9d58f59 oeqa/concurrencytest: Add number of failures to summary
            output
    c35db8b7ac valgrind: skip the boost_thread test on arm
    ce673bc9f7 qemuboot.bbclass: make sure runqemu boots bundled initramfs
            kernel image
    22f210f9f4 devtool/upgrade: correctly handle recipes where S is a
            subdir of upstream tree
    2f2abcb6d2 kernel.bbclass: remove empty module directories to prevent
            QA issues
    55f37f90dc libxml2: fix test data checksums
    e680a7402e classes/create-spdx: Add SPDX_PRETTY option
    50c27f5f98 libepoxy: remove upstreamed patch
    803b754c64 go-crosssdk: avoid host contamination by GOCACHE
    c588b05ed4 baremetal-image: Avoid overriding qemu variables from
            IMAGE_CLASSES
    2ed579aa28 openssh: remove RRECOMMENDS to rng-tools for sshd package
    e9a05c026c gstreamer1.0: upgrade 1.20.4 -> 1.20.5
    e9f8a4ce08 tzdata: update 2022d -> 2022g
    3e43f3925b ruby: update 3.1.2 -> 3.1.3
    22d6559bc3 ruby: merge .inc into .bb
    95a1668668 libnewt: update 0.52.21 -> 0.52.23
    94c4dc8d23 webkitgtk: 2.36.7 -> 2.36.8
    63725065af libpng: upgrade 1.6.38 -> 1.6.39
    3ea61d4228 linux-yocto/5.10: update to v5.10.160
    ac6645ecd9 linux-yocto/5.10: update to v5.10.154
    c344ea005c linux-yocto/5.10: update to v5.10.152
    1f269e532a binutils : Fix CVE-2022-4285
    dc7b7a757a cairo: update patch for CVE-2019-6461 with upstream solution
    b0e0cf44fb libX11: CVE-2022-3554 & CVE-2022-3555 Fix memory leak
    a0db3ddf6f curl: Add patch to fix CVE-2022-43552
    baa18f2cf1 curl: Add patch to fix CVE-2022-43551
    95b5df5c9e curl: Correct LICENSE from MIT-open-group to curl
    18641988ca sqlite: fix CVE-2022-46908 safe mode authorizer callback
            allows disallowed UDFs.
    
    Signed-off-by: Martin Jansa <martin2.jansa@lgepartner.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=7e268c10', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=ada5e64a97', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=5fa9d58f59', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=a8055484f2', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
