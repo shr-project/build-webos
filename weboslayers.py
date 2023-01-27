1c0da36e50543e6407ea6f0388569370c8632d7c
Martin Jansa <martin2.jansa@lgepartner.com>
2023-01-27 18:04:12 +0000

    webos-image: Update layer pins for 2023-01-27 from master
    meta-oe as of 2023-01-26 13:16:34 -0800
    
    2ab113e8be mbedtls: upgrade to 2.28.2 to fix CVE-2022-46392,
            CVE-2022-46393
    91129b0c94 rocksdb: Fix build with clang compiler
    0f942d1f2f htop: fix Upstream-Status format
    8d2a06b62e redis-plus-plus: Enable SSL/TLS by default
    5da09e4b86 hiredis: Enable SSL/TLS support by default
    7e3a508325 smartmontools: fix Upstream-Status format
    3502b71da6 s-nail: fix Upstream-Status format
    438a5dd5e0 keyutils: Upgrade 1.6.1 -> 1.6.3
    cd10cbd00d msktutil: Upgrade to 1.2.1
    f7c4193e69 dbus-cxx: Upgrade to 2.3.1
    5283705655 dbus-cxx: Fix build with gcc13
    d41cfd1d9a crossguid: Fix build with gcc13
    8284a018f3 opensaf: Fix build with gcc13
    e552836284 libiodbc: Fix SRC_URI to not use gitlab archives
    df99d60370 rdfind: Fix build with gcc13
    cbd762fafc rdfind: Upgrade to 1.5.0 release
    52935c3dc0 redis-plus-plus: Fix build with gcc13
    494e8f0c2f redis-plus-plus: Upgrade to 1.3.7
    b5d0bba011 rocksdb: Upgrade to 7.9.2
    ad2a76cf36 rocksdb: Fix build with gcc13
    a493debe11 vulkan-cts,opengl-es-cts: Fix build with gcc-13
    1479989054 mariadb: Fix build with gcc-13
    d8381620cd libcamera: Fix build with gcc-13
    05ef2b6570 cups-pk-helper: add recipe
    b79af02110 python3-cachetools: Upgrade 5.2.1 -> 5.3.0
    9229fde5e2 python3-imageio: Upgrade 2.24.0 -> 2.25.0
    acf0b04700 python3-email-validator: Upgrade 1.3.0 -> 1.3.1
    12cdd958ef libgpiod: fix python bindings build
    290d4729e0 edid-decode: an utility to parse EDID information
    78b74ed90c python3-nocasedict: Upgrade 1.0.4 -> 1.1.0
    d11c3b828f python3-nocaselist: Upgrade 1.0.6 -> 1.1.0
    75a836190a Nodejs: Upgrade to 18.12.1
    c3e6365aea opensc: fix homepage URL
    eda6fd9a03 pcsc-lite: fix homepage URL
    a00a2d1040 pcsc-lite: fix native build
    d25967208b nftables: Fix missing leading whitespace with ':append'
    6a87f2ba9c Fix missing leading whitespace with ':append'
    61598be67d ntpsec: Upgrade 1.2.1 -> 1.2.2
    1e2e283635 cups-filters: build with dbus support
    5a28583594 psqlodbc: fix HOMEPAGE
    eec98799a2 uutils-coreutils: upgrade 0.0.16 -> 0.0.17
    d328049052 sdbus-c++: Disable ccache
    e95aca9bd0 uftreace: Fix a build race
    afe5ea5eaf rsyslog: update to 8.2212.0
    a96e7a1ff4 librelp: update to 1.11.0
    3998d15768 poppler: cmake: Do not use -isystem
    611f2921dc libcereal: Use -idirafter instead of -isystem
    02fa3ea1af thrift: Fix c++ and system header include order problem
    2c8470ea52 rtorrent: Add missing dependency on autoconf-archive
    554bae57a5 packagegroup-meta-oe: Remove mongodb for upsupported arches
    00a7d4b284 exiv2: Upgrade to 0.27.6
    e4a2dfb8d9 xmlsec1: Include xmlsec/parser.h for
            xmlSecParserSetDefaultOptions()
    6826c98f6d uw-imap: Fix build with clang 16
    0e12939817 espeak: Drop using 'register' keyword for storage classifier
    adb1af7105 hplip: Update to 3.22.10
    320466b540 libol: Drop recipe
    8699170d3a xmlstarlet: Fix build with clang16
    ee0de616df net-snmp: Fix build with clang16
    791d82af21 nspr: Fix build with clang16
    5fcea1efcc rtorrent: Update to latest tip of trunk
    02831aa6e3 pipewire-media-session: Use PW_ENABLE_DEPRECATED
    3e9c66580f ace: Upgrade to 6.5.19
    6e15cb7e04 libmimetic: Pin to using -std=c++14
    efe6522104 gengetopt: Use std=c++14
    1ccdb81959 log4cpp: Keep using std=c++14
    6f3ded8f27 ssiapi: Use c++14 standard
    195c2045fd audiofile: Stick to c++14 std
    b075f0e44c ippool: Use unsigned int type for 1-bit integer bitfield
    313539eeaa gnuchess: Fix build with c++17
    6d0eaf9a27 libtorrent: Update to tip of trunk
    68fe58173d freerdp: Fix incompatible function pointer type
    a92489d4b4 p8platform: Fix build with c++17 clang
    ed9fd245a9 satyr: Fix build with musl
    a56f11b144 xmlrpc-c: Upgrade to 1.54.06
    2d21f07004 libmodplug: Fix build with c++17

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=493f2d1637', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=ceceffcb1e', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=2ab113e8be', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
