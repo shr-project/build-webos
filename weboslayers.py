94aa4bd7699d5cb383ba75ec8b7dc5c41f9a0222
Martin Jansa <martin2.jansa@lgepartner.com>
2023-02-05 12:24:27 +0000

    webos-image: Update layer pins for 2023-02-05 from langdale
    oe-core as of 2023-01-31 13:15:53 -1000
    
    ab39d244cb Fix missing leading whitespace with ':append'
    2ba7a2e7f2 libusb1: Strip trailing whitespaces
    f66b0fd1ba libusb1: Link with latomic only if compiler has no atomic
            builtins
    3bf9e22c0c librsvg: Only enable the Vala bindings if GObject
            Introspection is enabled
    6e307036a1 native: Drop special variable handling
    05a062c011 quilt: use upstreamed faildiff.test fix
    f7c2e4c768 libtirpc: Check if file exists before operating on it
    da48bc443f numactl: skip test case when target platform doesn't have
            2 CPU node
    7bc6bb2d3f devtool: fix devtool finish when gitmodules file is empty
    f9167e9c31 spirv-headers/spirv-tools: set correct branch name
    0a5f5816ff createrepo-c: Include missing rpm/rpmstring.h
    199df863c1 quilt: fix intermittent failure in faildiff.test
    cdbe68d4a9 lttng-modules: Fix for 5.10.163 kernel version
    da386f40a8 lttng-modules: update 2.13.7 -> 2.13.8
    786b9bbf41 linux-yocto/5.15: update to v5.15.87
    4af92cb7c9 ppp: backport fix for CVE-2022-4603
    7007673bcd openssl: fix CVE-2022-3996 double locking leads to denial
            of service
    
    bitbake as of 2023-02-01 04:20:40 -1000
    
    a40fc6a3 fetch2/git: Clarify the meaning of namespace
    2b458392 fetch2/git: Prevent git fetcher from fetching gitlab
            repository metadata

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=28ba566c', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=a40fc6a3', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=cfd9f549a0', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=ab39d244cb', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=langdale,commit=f8cb46d803', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
