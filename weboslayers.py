310286007994f0bfbe03dbcac8caa9d878807052
Martin Jansa <martin2.jansa@lgepartner.com>
2023-02-13 08:42:54 +0000

    webos-image: Update layer pins for 2023-02-13 from dunfell
    oe-core as of 2023-02-13 07:48:21 +0000
    
    daaee6fcb0 build-appliance-image: Update to dunfell head revision
    b21e5524e3 Fix missing leading whitespace with ':append'
    a764e19736 libtirpc: Check if file exists before operating on it
    077edd3992 devtool: fix devtool finish when gitmodules file is empty
    a434079b7e devtool: Fix _copy_file() TypeError
    f4b8d8b224 linux-yocto/5.4: update to v5.4.230
    2861fb021a linux-yocto/5.4: update to v5.4.229
    520d957e14 lttng-modules: fix build with 5.4.229 kernel
    146cc298e9 lttng-modules: update 2.11.8 -> 2.11.9
    fa1056f620 lttng-modules: update 2.11.7 -> 2.11.8
    f3c18d4eda lttng-modules: update 2.11.6 -> 2.11.7
    f40ad856e8 libsdl2: Add fix for CVE-2022-4743
    3126830360 go: fix CVE-2022-1962 go/parser stack exhaustion in all
            Parse* functions
    4f069121dd python3: fix packaging of Windows distutils installer stubs
    b2e06c9cf8 selftest/virgl: use pkg-config from the host
    e872f6636f lib/oe/reproducible: Use git log without gpg signature
    2c7309eea6 toolchain-scripts: compatibility with unbound variable
            protection
    a35444fa03 systemd: Consider PACKAGECONFIG in RRECOMMENDS
    ac7c32ee2c vim: upgrade 9.0.0947 -> 9.0.1211
    5b56a80e29 linux-firmware: upgrade 20221109 -> 20221214
    dcc597d52a xserver-xorg: Fix Multiple CVEs
    a626228a4b ffmpeg: Fix CVE-2022-3109
    8b1f40639c cairo: fix CVE patches assigned wrong CVE number
    c25a5c5f6f cve-check: write the cve manifest to IMGDEPLOYDIR
    48c0e42767 cve-update-db-native: show IP on failure
    6a219c50ee cve-update-db-native: avoid incomplete updates
    60fa813543 cve-update-db-native: add more logging when fetching
    7290b3217c cve-update-db-native: Allow to overrule the URL in
            a bbappend.
    
    bitbake as of 2023-02-13 07:48:09 +0000
    
    c527976e fetch2/git: Clarify the meaning of namespace
    efb2903e fetch2/git: Prevent git fetcher from fetching gitlab
            repository metadata
    ecc1dac4 bitbake: fetch/git: use shlex.quote() to support spaces in
            SRC_URI url
    c16d364d bb/utils: include SSL certificate paths in export_proxies

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=e3db9c2e', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=c527976e', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=db81e3c7e7', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=daaee6fcb0', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=e707e9b7cf', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
