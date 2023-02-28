d82cb62cd8e32b5da80bdf982389eac99c5cd427
Martin Jansa <martin2.jansa@lgepartner.com>
2023-02-28 13:30:48 +0000

    webos-image: Update layer pins for 2023-02-28 from kirkstone
    oe-core as of 2023-02-27 23:39:05 +0000
    
    b20e2134da build-appliance-image: Update to kirkstone head revision

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=9bbdedc0', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=6b50713cd5', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=b20e2134da', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=82c75b466e', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
