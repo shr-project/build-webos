d552833a3f9eb3b86adf639297ad5ce8cbec81e2
Martin Jansa <martin2.jansa@lgepartner.com>
2023-03-07 15:48:54 +0000

    webos-image: Update layer pins for 2023-03-07 from langdale
    oe-core as of 2023-03-06 15:17:13 +0000
    
    b995ea4577 build-appliance-image: Update to langdale head revision
    9047a07bc8 runqemu: kill qemu if it hangs
    3e1a4d5729 oeqa ssh.py: fix hangs in run()
    13a567a0bf libseccomp: fix for the ptest result format
    a075bf43dd kernel-yocto: fix kernel-meta data detection
    70051189bb cml1: remove redundant addtask
    51e5e08540 lib/buildstats: handle tasks that never finished
    fdabdb6bd5 oeqa/selftest/resulttooltests: fix minor typo
    c2d2d973b3 libssh2: Clean up ptest patch/coverage
    5d36b42406 lttng-modules: fix for kernel 6.2+
    add5856e9a dhcpcd: fix dhcpcd start failure on qemuppc64
    f1f24e9dc4 sstatesig: Improve output hash calculation
    e8c4cf47ab busybox: Fix depmod patch
    0bbcc17f68 bblayers/makesetup: skip git repos that are submodules
    bd8920b3a9 bblayers/setupwriters/oe-setup-layers: create dir if
            not exists
    5f7239eebe wic: Fix usage of fstype=none in wic
    6a4f55bb30 curl: fix dependencies when building with ldap/ldaps
    d64e3cf57d classes/populate_sdk_base: Append cleandirs
    187b3a810c wireless-regdb: upgrade 2022.08.12 -> 2023.02.13
    0c69a2117c linux-firmware: upgrade 20230117 -> 20230210
    873971539f linux-firmware: add yamato fw files to qcom-adreno-a2xx
            package
    2a9659695b linux-firmware: properly set license for all Qualcomm
            firmware
    f5dce7274b openssl: Upgrade 3.0.7 -> 3.0.8
    83e9bd1507 dbus: upgrade 1.14.4 -> 1.14.6
    964ca02deb xwayland: upgrade 22.1.7 -> 22.1.8
    d57de2a716 libjpeg-turbo: upgrade 2.1.5 -> 2.1.5.1
    855f629bcd vim: update 9.0.1211 -> 9.0.1293 to resolve open CVEs
    0cb6376205 xserver-xorg: 21.1.6 -> 21.1.7
    78c44993a1 less: backport the fix for CVE-2022-46663

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=592ee222', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=ab1e3000ce', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=b995ea4577', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=langdale,commit=b5b732876d', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
