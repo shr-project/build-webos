5db1d27a635be8656f8ec35ecc93ade771ff31bd
Martin Jansa <martin2.jansa@lgepartner.com>
2023-03-17 16:03:17 +0000

    webos-image: Update layer pins for 2023-03-17 from langdale
    oe-core as of 2023-03-10 05:54:34 -1000
    
    21e8a662e6 vala: Fix install conflict when enable multilib.
    7726945826 lua: Fix install conflict when enable multilib.
    9a1ae30571 linux: inherit pkgconfig in kernel.bbclass
    25d48a686f oeqa/sdk: Improve Meson test
    1e5f847a77 meson: Fix wrapper handling of implicit setup command
    9af6c9a375 rpm: Fix hdr_hash function prototype
    faa4aa99d1 libcomps: Fix callback function prototype for PyCOMPS_hash
    11d57d8a30 mesa-demos: packageconfig weston should have a dependency
            on wayland-protocols
    a2ad52661e vim: add missing pkgconfig inherit
    46334b5266 systemd: add group sgx to udev package
    9389031c7b ffmpeg: fix build failure when vulkan is enabled
    8547304ca8 oeqa/selftest/prservice: Improve debug output for failure
    2f4ceab401 u-boot: Map arm64 into map for u-boot dts installation
    54ff75f4da binutils: Fix nativesdk ld.so search
    2ff341a7f4 libmicrohttpd: upgrade 0.9.75 -> 0.9.76
    b70351ff50 iso-codes: upgrade 4.12.0 -> 4.13.0
    4c3b1ab450 linux-yocto-rt/5.15: update to -rt59
    5f82a892ec linux-yocto/5.15: update to v5.15.96
    fbe103e571 linux-yocto/5.15: update to v5.15.94
    8dce0c01d9 shadow: ignore CVE-2016-15024
    412f9b5732 epiphany: Security fix for CVE-2023-26081
    0af05af0fc qemu: Fix CVE-2022-4144
    35ccaf2f3f libxml2: Fix CVE-2022-40303 && CVE-2022-40304
    d9ce9b3723 tiff: fix multiple CVEs
    
    bitbake as of 2023-03-15 23:15:20 +0000
    
    88a9cc03 toaster: Add refreshed oe-core and poky fixtures
    2dda597d toaster: fixtures/gen_fixtures.py: update branches
    add45691 toaster: fixtures/README: django 1.8 -> 3.2
    2b239555 cookerdata: Drop dubious exception handling code
    4b922345 cookerdata: Improve early exception handling
    8001c9b5 cookerdata: Remove incorrect SystemExit usage
    3cc9fe91 utils: Allow to_boolean to support int values
    2fe0c90d bin/utils: Ensure locale en_US.UTF-8 is available on the
            system
    
    meta-oe as of 2023-03-12 11:51:57 -0400
    
    613dc6e107 nodejs: Upgrade 16.19.0 -> 16.19.1
    e7cc9b7c95 redis: Upgrade 7.x to 7.0.9
    0acbfd267d redis: Upgrade 6.x recipe to 6.2.11
    1343c8e374 php: Upgrade to 8.1.16
    ea18a49b79 nlohmann-json: Allow empty main package for SDK

--- weboslayers.py
+++ weboslayers.py
@@ -58,11 +58,11 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=592ee222', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=88a9cc03', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=b995ea4577', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=21e8a662e6', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=langdale,commit=b5b732876d', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=langdale,commit=613dc6e107', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
