a63a89f1a3dd4e6d75b3d929ff1d8649ccecb38b
Martin Jansa <martin2.jansa@lgepartner.com>
2023-03-24 12:26:07 +0000

    webos-image: Update layer pins for 2023-03-24 from kirkstone as in WRP-11864
    oe-core as of 2023-03-17 04:52:21 -1000
    
    7df46e003e base-files: Drop localhost.localdomain from hosts file
    800ea13091 dhcpcd: Fix install conflict when enable multilib.
    d2800e00bb populate_sdk_ext: Handle spaces within user $PATH
    64dd672193 toolchain-scripts: Handle spaces within user $PATH
    c9bf05d819 buildtools-tarball: Handle spaces within user $PATH
    1819437850 cups: add/fix web interface packaging
    7b23927a72 cups: check PACKAGECONFIG for pam feature
    a42066657c cups: use BUILDROOT instead of DESTDIR
    af73a79f6d selftest/recipetool: Stop test corrupting tinfoil class
    0f317a0533 selftest/runtime_test/virgl: Disable for all Rocky Linux
    de967efc52 glibc: Add missing binutils dependency
    0aa703c06b staging/multilib: Fix manifest corruption
    29f7390add staging: Separate out different multiconfig manifests
    a5c38968e1 mdadm: Fix raid0 tests
    3c2d554f14 mdadm: fix tests/02lineargrow
    9c73484cb1 mdadm: Fix testcase 06wrmostly
    ec594fe190 vim: set modified-by to the recipe MAINTAINER
    2d9a183467 vim: upgrade to 9.0.1403
    48633d8920 python3-git: fix indent error
    37defd828c python3: fix CVE-2023-24329
    5c156d8c06 pkgconf: fix CVE-2023-24056
    
    meta-updater as of 2023-03-22 09:03:48 +0100
    
    dc93745 Merge pull request #66 from quaresmajose/ostree_rmdir_helper
    1498485 image_types_ostree: adds ostree_rmdir_helper

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=a29ffcc1', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=8b41b5d2e4', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=7df46e003e', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=9cf4ebeb3d', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -69,7 +69,7 @@ webos_layers = [
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=7e3f2a4', ''),
-('meta-updater',              16, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=ffbbc2b', ''),
+('meta-updater',              16, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=dc93745', ''),
 ('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=bfa6727', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4.1,commit=v6.4.1', ''),
