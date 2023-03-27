9cc54c7d3e6843503fd14f36ca782437c8ea198f
Martin Jansa <martin2.jansa@lgepartner.com>
2023-03-27 20:32:01 +0000

    webos-image: Update layer pins for 2023-03-27 from langdale
    oe-core as of 2023-03-21 06:55:30 -1000
    
    a0ef4386d3 systemd: fix wrong nobody-group assignment
    bad6361b2f cargo.bbclass: use offline mode for building
    726bd3b502 cracklib: update github branch to 'main'
    4bacfa12a8 mdadm: Fix raid0 tests
    fd3aaff0e5 mdadm: fix tests/02lineargrow
    04e4465fa3 mdadm: Fix testcase 06wrmostly
    abbeea6629 pybootchart: Fix extents handling to account for cpu/io/mem
            pressure changes
    ea87507b74 pybootchartui: Fix python syntax issue
    8aa68cd570 tzcode-native: fix build with gcc-13 on host
    9b6dcbf40c tzdata: use separate B instead of WORKDIR for zic output
    28b0d58d95 timezone: use 'tz' subdir instead of ${WORKDIR} directly
    dc5097e669 lib/resulttool: fix typo breaking resulttool log --ptest
    0bb93dce6a oeqa rtc.py: skip if read-only-rootfs
    417b096d5c image_types: fix multiubi var init
    612e2cff28 systemd.bbclass: Add /usr/lib/systemd to searchpaths as well
    bb350782ba devtool/upgrade: do not delete the workspace/recipes
            directory
    705b5c5161 image_types: fix vname var init in multiubi_mkfs() function
    a45bc475c7 libgit2: update license information
    3bee5e8c76 go: upgrade to 1.19.7
    7634c80081 tiff: Fix CVE-2023-0795 CVE-2023-0796 CVE-2023-0797
            CVE-2023-0798 CVE-2023-0799
    70adf86b51 git: ignore CVE-2023-22743

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.2,commit=88a9cc03', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=61f268be80', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=langdale,commit=a0ef4386d3', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=langdale,commit=3f9340a924', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
