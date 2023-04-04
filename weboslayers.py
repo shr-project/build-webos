9df0651cfb7031876f655a807c5d9f18c3bc8c30
Martin Jansa <martin2.jansa@lgepartner.com>
2023-04-05 00:47:04 +0200

    webos-image: Update layer pins for 2023-04-05 from master
    oe-core as of 2023-04-04 21:54:54 +0100
    
    aa466053bf sanity.bbclass: Update minimum gcc version to 8.0
    3344839349 cve-extra-exclusions.inc: Exclude some issues not present
            in linux-yocto
    b85b7714a4 xdg-utils: Add a patch for CVE-2020-27748

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=dafa07c0', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=686e0cb93f', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=aa466053bf', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=60afa577e5', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
