3aedd9ef92202677511faa04f79abe8a7455d527
Martin Jansa <martin2.jansa@lgepartner.com>
2023-04-04 20:10:03 +0200

    webos-image: Update to latest revision in Yocto 4.3 Nanbield
    oe-core as of 2023-04-11 11:47:58 +0100
    
    85c1713bf0 cve-exclusions: Document some further linux-yocto CVE
            statuses
    39274240b7 cve-extra-exclusion: ignore disputed CVE-2023-23005
    990d1cbb16 cve-exclusions_6.1: ignore patched CVE-2022-38457 &
            CVE-2022-40133
    2bcb015b6f build-appliance-image: Update to master head revision
    ae1e7999a0 cve-extra-exclusions: ignore inapplicable linux-yocto CVEs
    bfff138af4 cpio: Fix wrong CRC with ASCII CRC for large files
    17c995c537 oeqa ping.py: fail test if target IP address has not
            been set
    87ec75710b oeqa ping.py: avoid busylooping failing ping command
    d523079bbe devicetree.bbclass: fix typo
    7ac3bcf228 grep: upgrade 3.9 -> 3.10
    0331fac935 xz: upgrade 5.4.1 -> 5.4.2
    7c32ca2f91 selftest: efibootpartition.py: fix QEMU_USE_KVM usage
    ccef8f0b0d python3-bcrypt-crates.inc: regenerate with updated bbclass
    bf6a3cd70d python3-cryptography-crates.inc: regenerate with updated
            bbclass
    eb272afcd9 cargo-update-recipe-crates.bbclass: Do not add name=
            to crate:// URIs
    5a96847e7e build-appliance-image: Update to master head revision
    76f1645134 build-appliance-image: Update to master head revision
    3012bac35a busybox: move hwclock init earlier in startup
    67886a8473 oeqa/selftest/bblogging: uncomment python stdout checks
    9e41fac1c2 qemu: rename deprecated --enable-trace-backend configure
            option
    c31396a30d qemu: retain default trace backend if 'ust' is not enabled
    85e30c507b qemu: make tracetool-generated output reproducible
    5feb065f1b cve-extra-exclusions: ignore inapplicable linux-yocto CVEs
    b3e2729f68 xdg-utils: Fix CVE number
    aa466053bf sanity.bbclass: Update minimum gcc version to 8.0
    3344839349 cve-extra-exclusions.inc: Exclude some issues not present
            in linux-yocto
    b85b7714a4 xdg-utils: Add a patch for CVE-2020-27748
    686e0cb93f vte: depend on glib-2.0-native
    
    bitbake as of 2023-04-06 14:28:54 +0100
    
    ae2efb05 fetch2/crate: create versioned 'name' entries
    46e1ea2e bitbake: Bump to version 2.4.0
    
    meta-clang as of 2023-04-07 08:39:27 -0700
    
    db1f7df bindgen-cli: regenerate with updated bbclass
    e836c2e Clang: Update to llvmorg-16.0.1 tag
    63aac1d clang: Move clang-cl from clang-tools -> clang package
    16da933 castxml: Fix nativesdk build
    ea526f0 castxml: Upgrade to 0.6.1
    0c09a7e recipes: Update patch status
    4219cb9 clang: Update patch status
    c82a3f7 mesa: Make a step closer to yocto compatible
    6bb8b82 packagegroup-core-standalone-sdk-target: Use toolchain-clang
            override
    c28334a clang: Update to 16.0.1 release
    8bf663a compiler-rt: fix installed-vs-shipped qa issue
    7882d71 qtpdf: Inherit clang-native
    52c2740 layer.conf: Add qt5 layer to dynamic layer list
    2462338 Revert "webkitgtk: Use gcc for now"
    eac942c bpftrace: Fix build with clang16
    a3f629b frr: Use gcc when compiling for rv32/rv64
    074385b spirv-llvm-translator: Turn shared libs off
    2d9f57f spirv-llvm-translator: Update to latest 16.0.0 branch
    8daf40d nodejs: Disable enum-constexpr-conversion warning as error
    335f2e7 opengl-es-cts,vulkan-cts,pidgin-sipe: Disable new warnings
            as errors
    8abfcaa glib-2.0: Disable -Wincompatible-function-pointer-type with
            clang-16
    75feaf4 compiler-rt-sanitizers: Disable building tests
    6ce232d compiler-rt: Do not include tests in build
    de56f14 clang: Update to 16.0.0

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.4,commit=d97d62e2', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=ae2efb05', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=mickledore,commit=c57d1a561d', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=85c1713bf0', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=mickledore,commit=2d89a469e5', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -68,13 +68,13 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=mickledore,commit=1e0af2f', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=db1f7df', ''),
 ('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=master,commit=63fee1a', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=50ceb5a', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4,commit=24f23ee', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=mickledore-2023-04-26', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2023-04-26', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=bf948e0', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
@@ -82,5 +82,5 @@ webos_layers = [
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
 ('meta-webos-smack',          75, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
-('meta-security',             77, 'https://git.yoctoproject.org/git/meta-security',         'branch=mickledore,commit=53c5cc7', ''),
+('meta-security',             77, 'https://git.yoctoproject.org/git/meta-security',         'branch=master,commit=53c5cc7', ''),
 ]
