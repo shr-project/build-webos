9a2480755ebe3092bdd06ff383fb0bf37ad05b6e
Martin Jansa <martin2.jansa@lgepartner.com>
2023-05-04 12:44:22 +0200

    webos-image: Update layer pins for 2023-05-04 from master
    oe-core as of 2023-05-03 14:37:09 +0100
    
    2a86ca0289 maintainers.inc: Fix email address typo
    0defbb5925 mesa: 23.0.2 -> 23.0.3
    ba1d66131a piglit: upgrade to latest revision
    94cf6ef11b waffle: upgrade 1.7.0 -> 1.7.2
    8ac81784ce gtk+3: upgrade 3.24.36 -> 3.24.37
    e652d9bee3 libinput: upgrade to 1.23.0
    5691f554b2 update-alternatives.bbclass: fix old override syntax
    07ed616f77 musl: Update to latest master
    9db1a06969 patchelf: Upgrade 0.17.2 -> 0.18.0
    dd5f69ab17 maintainers.inc: Move apt/dpkg to unassigned
    e1c9cbe8b1 python3-psutil: Drop nativesdk class extension due to
            breakage
    7db8c52dd3 maintainers.inc: remove myself from assignment
    7870364e48 glide: remove as 'go mod' has become standard
    2bc8c71350 python3-pytest: add missing tomllib RDEPENDS
    5ad0cf57b4 oeqa/runtime/ptest: Make returning no test results a failure
    
    meta-oe as of 2023-05-03 08:45:24 -0700
    
    f3cdc9d7ee pointercal: Add native & nativestdk package support
    7f4e9933e8 tslib: Add native & nativestdk package support
    4cd2006c6b etcd: remove gobin requirement for build
    078e867c97 etcd: use v2.1.2 xhash to fix build issue
    05c9ba951e etcd: add recipe
    77f031776e freediameter: fix typo and old overide syntax
    88b295625d nfacct: Update SRC_URI to point to valid URL
    3c34a8f8eb ttf-gentium: Switch to debian archive mirror for SRC_URI
    34796ff654 debootstrap: Use DEBIAN_MIRROR for SRC_URI
    2db438f241 debootstrap: Update SRC_URI to point to valid URL
    d0e061bc72 faenza-icon-theme: Switch to a valid download  location
            for SRC_URI
    fc811eb7e5 xdotool: Upgrade to 3.20211022.1 release
    d4be801b86 mosh: Point SRC_URI to https://mosh.org/
    d6e59649ff p910nd: Switch to using github for SRC_URI
    edf94a6e8d httpfs2: Do not use S during compile/install tasks
    fe62e64c97 radiusclient-ng: Point SRC_URI to archive.ubuntu.com
    f37833fc63 ttf-lklug: Point SRC_URI to a working location
    
    meta-virtualization as of 2023-04-21 11:34:42 -0400
    
    5649493 ovs: update to 3.1.1
    
    meta-clang as of 2023-05-03 16:37:47 -0700
    
    e7b3acb Drop crosssdk suffix from virtual binutils provides to
            match oe-core

--- weboslayers.py
+++ weboslayers.py
@@ -60,20 +60,20 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=6e99d89f', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=2ee144a0bf', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=2a86ca0289', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=f030642efa', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=f3cdc9d7ee', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=e174cc8', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=50ceb5a', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=e7b3acb', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=5649493', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4,commit=24f23ee', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2023-04-27', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2023-05-04', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=bf948e0', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
