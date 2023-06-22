89baa45533aeb41da850daefdc83fa526bbdb749
Martin Jansa <martin2.jansa@lgepartner.com>
2023-06-22 06:36:49 +0200

    webos-image: Update layer pins for 2023-06-22 from master
    oe-core as of 2023-06-21 18:38:21 +0100
    
    a81dc665e8 sysfsutils: don't install to base_libdir
    bb07338031 sysfsutils: update 2.1.0 -> 2.1.1
    504b2f590c sysfsutils: fetch a supported fork from github
    943d438e2e python3-dbusmock: only recommend python3-pygobject
    b913f6c1f9 u-boot-tools: Use PATH_MAX for path length
    3aba44a75d rpm2cpio.sh: update to the last 4.x version
    dca7d1ce69 unzip: remove hardcoded LARGE_FILE_SUPPORT
    b9aca339b5 unzip: fix configure check for cross compilation
    601f45bc4e zip: remove unnecessary LARGE_FILE_SUPPORT CLFAGS
    dda778d855 zip: fix configure check by using _Static_assert
    b4392ebb51 python3-bcrypt: Use BFD linker when building tests
    
    meta-oe as of 2023-06-21 09:15:20 -0700
    
    be8c765c7c *.patch: add Upstream-Status to all patches
    2393cd93ae postfix: fix malformed Upstream-Status
    dd2a100449 libgpiod: fix malformed Upstream-Status
    a01c94b71b asio: fix malformed Upstream-Status
    1aaf1023e0 ostree: Upgrade 2023.3 -> 2023.4
    9cb5f5c6e9 mbedtls: add support for v3.x
    d552fa0478 mdns: remove unneeded headers
    63b1dbd093 syzkaller: Upgrade to latest tip of trunk
    
    meta-raspberrypi as of 2023-06-22 00:04:40 +0100
    
    195c7d5 *.patch: add Upstream-Status to all patches
    
    meta-clang as of 2023-06-21 19:31:03 -0700
    
    91776cb clang: Add missing SOB

--- weboslayers.py
+++ weboslayers.py
@@ -60,22 +60,22 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=0556c7bd', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=b9ef82727e', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=a81dc665e8', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=39dfcaceba', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=be8c765c7c', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=cae721a', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=91776cb', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=49fdceb', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.5.1,commit=v6.5.1', ''),
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2023-06-21', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=8e07f0d', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=195c7d5', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
