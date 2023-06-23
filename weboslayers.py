462e54dfaf2c53e7a7027db494c4d763bc46b18b
Martin Jansa <martin2.jansa@lgepartner.com>
2023-06-23 17:22:10 +0200

    webos-image: Update layer pins for 2023-06-23 from kirkstone
    oe-core as of 2023-06-23 04:17:20 -1000
    
    7949e786cf build-appliance-image: Update to kirkstone head revision
    59f1a1b232 lib/terminal.py: Add urxvt terminal
    dad053c4b9 staging.bbclass: do not add extend_recipe_sysroot to
            prefuncs of prepare_recipe_sysroot
    9ce30faa48 oeqa/selftest/cases/devtool.py: skip all tests require
            folder a git repo
    5dec509c33 kmscube: Correct DEPENDS to avoid overwrite
    44b75c7442 kernel: don't force PAHOLE=false
    c89090e148 conf: add nice level to the hash config ignred variables
    f9b1a56c50 go.bbclass: don't use test to check output from ls
    86e8e31a41 iso-codes: upgrade 4.13.0 -> 4.15.0
    6b82d99818 nasm: fix CVE-2022-46457

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=0c6f86b6', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=0e17a5a4f0', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=7949e786cf', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=a82d92c8a6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
