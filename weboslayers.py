22ed2ae39dd6496aacf1b3a2211e9eeca90034a4
Martin Jansa <martin2.jansa@lgepartner.com>
2023-07-01 14:08:45 +0200

    webos-image: Update layer pins for 2023-07-01 from master
    oe-core as of 2023-06-30 10:02:17 +0100
    
    bffa6d381d ptest-runner: Pull in "runner: Remove threads and mutexes"
            fix
    
    bitbake as of 2023-06-30 18:05:25 +0100
    
    a5c145f4 cooker: Log when parsing starts in server log
    ac3cd866 server/process: Show command in timeout message
    9941b480 fetch2/npmsw: Support old and new shrinkwrap formats

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=b2e1be67', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=a5c145f4', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=26d97acc71', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=bffa6d381d', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=bf314d2c57', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
