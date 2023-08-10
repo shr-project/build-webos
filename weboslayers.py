ab3ed58b85c9bc561c042991ed19d73d8d80a2a2
Martin Jansa <martin2.jansa@lgepartner.com>
2023-08-10 20:01:55 +0200

    webos-image: Update layer pins for 2023-08-10 from mickledore
    meta-oe as of 2023-08-03 16:46:11 -0400
    
    75cf318cef lmsensors: do not pull in unneeded perl modules for
            run-time dependencies

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=mickledore,commit=dc40993071', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=mickledore,commit=03fd1d368a', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=mickledore,commit=75cf318cef', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
