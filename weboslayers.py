696a730c41d1a1ccaffcba720bf3a936ec94cc22
Martin Jansa <martin2.jansa@lgepartner.com>
2023-09-02 09:42:01 +0200

    webos-image: Update layer pins for 2023-09-02 from master
    oe-core as of 2023-09-02 07:47:41 +0100
    
    79d453eb0c weston: fix comment
    319249ea63 site: remove at-spi2-core values
    39f987fcb2 yocto-uninative: Update to 4.3
    43a3ab6b41 README: Update to point to new contributor guide
    f65f0811e3 glibc: Add glibc 2.38 stable updates
    
    bitbake as of 2023-09-02 07:46:32 +0100
    
    0fc821a22 command: Avoid time intensive distractions for ping
    fb19d6476 README: Update to point to new contributor guide
    
    meta-oe as of 2023-08-31 07:43:23 -0700
    
    05e32934d0 openh264: make neon optional and disable if not supported
    57b37873c5 sysbench: avoid -L/usr/lib32 and configure-unsafe QA issue
    d88ac90b62 feat: bump sdbus-c++ up to v1.3.0
    c38239b3d9 python3-piccata: piccata - a simple CoAP toolkit added
    f4a4dbfb6c python3-gspread: interface for google spreadsheet
    aecd7f7f4a phodav: make sure systemd files are packaged correctly
    9b37c1cc88 json-schema-validator: restore
            0004-cmake-Use-GNUInstallDirs.patch
    b0c46ce73a libosinfo: build vapi only if gobject-introspection
            is enabled
    5c5994a3d5 libfaketime: simplify packaging
    00f8179720 spice-guest-vdagent: add missing dependencies
    1bc98e3643 spice: add missing dependency on orc
    1cfd2bcc84 emacs: update to 29.1
    f9afd63370 pipewire: upgrade 0.3.78 -> 0.3.79
    24b89b9ccc thrift: Remove buildpaths
    9156898cc5 ne10: set incompatible for armv7 without neon
    
    meta-clang as of 2023-08-31 07:40:31 -0700
    
    7a9fe6f clang.bbclass: Simplify check when to enable lld

--- weboslayers.py
+++ weboslayers.py
@@ -58,17 +58,17 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=703792c48', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=0fc821a22', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=d0bd1c823c', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=79d453eb0c', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=ee1026ab77', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=05e32934d0', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=1addd3d', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=7a9fe6f', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=683bfbfc', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.5.1,commit=v6.5.1', ''),
