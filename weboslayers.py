8179827140b95c11a8bf6b3cbefa80138d7e8b70
Martin Jansa <martin2.jansa@lgepartner.com>
2023-10-06 23:08:49 +0200

    webos-image: Update layer pins for 2023-10-06 from nanbield
    oe-core as of 2023-10-06 11:50:54 +0100
    
    fa53f898ea busybox: Set PATH in syslog initscript
    7c42b976d7 qemu: fix CVE-2023-42467
    53d9d8789e linux-firmware: create separate package for cirrus and
            cnm firmwares
    ff2fc789e0 gstreamer: upgrade 1.22.5 -> 1.22.6
    ae9c64cd22 libtirpc: conditionally enable gssapi
    90edba29f9 meson: upgrade 1.2.1 -> 1.2.2
    457f0dad87 recipetool/create_buildsys_python: use importlib instead
            of imp
    de62d538db packages.bbclass: Correct the check for conflicts with
            renamed packages
    0830f53900 scripts/oe-setup-layers: Update how to determine if
            directory is git repo
    4925591919 autotools.bbclass: Add *.m4 to CONFIGURE_FILES
    09873b3fb2 externalsrc.bbclass: Support specifying patterns in
            CONFIGURE_FILES
    46dd8ce417 libxpm: upgrade to 3.5.17
    a1534bb34b libx11: upgrade to 1.8.7
    b4dd9d8735 uboot-extlinux-config.bbclass: fix missed override syntax
            migration
    6b872ee729 oeqa/concurrencytest: Remove invalid buffering option
    dd8f47a9c4 linux-yocto/6.5: integrate fixes for sanity issues
    
    meta-clang as of 2023-10-05 17:47:39 -0700
    
    897ebdf kernel-selftest: Fix build with lld and clang on x86

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.6,commit=f3ba9c372', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=nanbield,commit=6fce7f99ff', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=nanbield,commit=fa53f898ea', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=nanbield,commit=3dd5d208be', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -68,7 +68,7 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=c0f69b2', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=897ebdf', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=827092c2', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.5.2,commit=v6.5.2', ''),
