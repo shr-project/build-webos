9741ea1b970e02b6cc24ed99288fcab15e9cb689
Martin Jansa <martin2.jansa@lgepartner.com>
2023-10-24 17:47:57 +0200

    webos-image: Update layer pins for 2023-10-24 from nanbield
    oe-core as of 2023-10-19 23:56:32 +0100
    
    4c261f8cbd build-appliance-image: Update to nanbield head revision
    6ecb3dac0b build-appliance-image: Update to nanbield head revision
    12fa669ea2 build-appliance-image: Update to nanbield head revision
    4c9a85ed1d linux-yocto/6.5: serial: core: integrate upstream fixes
    40f2edd66a linux-yocto/6.5: config: remove VIDEO_STK1160_COMMON
    b2bbd5b407 patchtest: sort when reading patches from a directory
    2d24ff9568 patchtest: check for untracked changes
    2a9ae3ca37 python3-urllib3: Update to 2.0.6
    9292eb70a2 vim: Upgrade 9.0.2009 -> 9.0.2048
    4895e1892a SECURITY.md: add file
    4f84537670 build-appliance-image: Update to master head revision
    6cd547b248 patchtest/selftest: fix command arguments
    9bea6b3907 patchtest: update SPDX identifiers
    fc424293b2 strace: further clean up of ptest folders
    f5dee6290c qemuarmv5: Drop QB_DTB conditional for older kernels
    bae7421ece patchtest: set default repo and testdir targets
    cf318c3c05 patchtest: add scripts to oe-core
    499cdad7a1 patchtest: add supporting modules
    f88e295cb5 patchtest/requirements.txt: update
    85ddbb67f0 insane: unimplemented-ptest: ignore source file errors
    9a2d2f7c2b insane: skip unimplemented-ptest on S=WORKDIR recipes
    43b6d04097 qemu: ignore RHEL specific CVE-2023-2680
    8715d72caa linux-yocto/6.5: avoid serial port suspend issues
    97f0a73c0a recipes-rt: update README to match newer override syntax
    6f123655c7 gcompat: Upgrade to 1.1.0 release
    e6efe08c5a gcompat: Add fcntl64 wrapper
    
    bitbake as of 2023-10-24 12:50:27 +0100
    
    936fcec41 SECURITY.md: add file
    5419a8473 codeparser: replace deprecated ast.Str and 's'
    87f5987e2 Toaster: Fixed javascript issue on
            tests.browser.test_js_unit_tests
    8d671e500 Toaster: Bug-fix failure on
            tests.browser.test_layerdetails_page
    d34d1f58b toaster: bug-fix on
            tests.browser.test_most_recent_builds_states
    fa30af75b toaster: fixed functional test
    8490844a0 toaster: update jquery version 2.0.3 -> 3.7.1
    046bbe22f toaster: Update bootstrap version to 3.4.1
    5254e8268 toaster: updated bootstrap version 3.3.6 -> 3.3.7
    
    meta-raspberrypi as of 2023-10-24 12:41:50 +0100
    
    3a0f667 linux-raspberrypi/linux-raspberrypi-v7: drop 5.10 version
    9a296d7 docs: Fix ReadTheDocs builds.os requirement
    
    meta-clang as of 2023-10-19 14:29:45 -0700
    
    4c65122 clang: Update to 17.0.3 release

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.6,commit=05051152c', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.6,commit=936fcec41', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=nanbield,commit=6cd57b2d14', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=nanbield,commit=4c261f8cbd', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=nanbield,commit=278c3f75e3', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -68,7 +68,7 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=897ebdf', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=4c65122', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=master,commit=827092c2', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.5.2,commit=v6.5.2', ''),
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=nanbield-2023-10-15', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=482d864', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=3a0f667', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
