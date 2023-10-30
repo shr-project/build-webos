c61c3110b3693b5d189298706848663c675e9bc4
Martin Jansa <martin2.jansa@lgepartner.com>
2023-10-30 08:51:11 +0100

    webos-image: Update layer pins for 2023-10-30 (WRP-29200)
    oe-core as of 2023-10-20 06:38:00 -1000
    
    7681436190 vim: Upgrade 9.0.2009 -> 9.0.2048
    8175d023c2 libx11: Security Fix for CVE-2023-43785, CVE-2023-43786
            and CVE-2023-43787
    d3f1448246 linux-firmware: upgrade 20230625 -> 20230804
    6e265e44fe zlib: patch CVE-2023-45853
    0547b60d34 gawk: backport Debian patch to fix CVE-2023-4156
    a1256b8fa4 qemu: ignore RHEL specific CVE-2023-2680

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=6c1ffa909', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=5570e49791', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=7681436190', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=79a6f60dab', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
