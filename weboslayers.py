625334d5632deddbee6868d5532032d4476ba7b5
Martin Jansa <martin2.jansa@lgepartner.com>
2023-10-31 08:11:00 +0100

    webos-image: Update layer pins for 2023-10-31 from master
    oe-core as of 2023-10-30 17:12:16 +0000
    
    7480ff9064 qemu: Upgrade 8.1.0 -> 8.1.2
    d53c1aca79 libcroco: drop recipe
    266cd948d4 kernel.bbclass: add preceding space in appendVar setting
    befec4d359 python3-urllib3: Upgrade to 2.0.7
    64ed88e32c patchtest-send-results: send results to submitter
    3c04747b68 scripts/contrib/patchreview: fix commit identification
    0cc2c0a9ec bitbake.conf: remove ${CCACHE} from FORTRAN compiler
    dd52102a6c kernel: Commit without running hooks
    66f0c2a167 volatile-binds: Calculate the name of the /var/lib service
    1ca031b775 volatile-binds: Allow creation of subdirectories
    ababf6ceeb Revert "bin_package.bbclass: Inhibit the default
            dependencies"
    d99b4883b4 recipetool: add python_hatchling support
    
    bitbake as of 2023-10-30 22:24:29 +0000
    
    11dfd7c05 toaster: add tox.ini file to execute test suite
    
    meta-oe as of 2023-10-29 21:49:04 -0700
    
    8ce73fefb0 openbox: Drop deprecated libcroco dependency
    4eaf1b4018 gnome-shell: Remove deprecated libcroco dependency

--- weboslayers.py
+++ weboslayers.py
@@ -58,11 +58,11 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=efbd9d54f', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=11dfd7c05', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=c10d0bb542', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=7480ff9064', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=22889b13f3', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=8ce73fefb0', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
