b69be30c4058c99c04f8bf54cc7260043038a42d
Martin Jansa <martin2.jansa@lgepartner.com>
2023-10-31 07:27:25 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2023-10-30 (build 6xx part 1/3) (WRQ-1043)
    meta-clang as of 2023-10-30 22:50:31 -0700
    
    eb8b3a5 clang.bbclass: fix a typo
    2187629 clang.bbclass: Simplify check when to enable lld
    
    :Issues Addressed:
    [WRQ-1043] CCC: Various build fixes
    
    Change-Id: I1b4ece0b9421318d44155011bf1c96476f177827

--- weboslayers.py
+++ weboslayers.py
@@ -68,7 +68,7 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=2d08d6b', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=eb8b3a5', ''),
 ('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=2d8b3cba', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.5.2,commit=v6.5.2', ''),
