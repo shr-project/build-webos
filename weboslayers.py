3af548f3f296dc9b38755afa512a933df641895e
Martin Jansa <martin2.jansa@lgepartner.com>
2023-11-11 08:59:28 +0100

    webos-image: Update layer pins for 2023-11-11 from nanbield
    meta-clang as of 2023-11-10 10:12:35 -0800
    
    5170ec9 nonclangable: Make tegra specific packages only for tegra
    
    Change-Id: I5c58f818956e079e6ee83efc30bc5dbbee023b8a

--- weboslayers.py
+++ weboslayers.py
@@ -68,7 +68,7 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=master,commit=a096a3c', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=nanbield,commit=5170ec9', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=nanbield,commit=9fb41d83', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.6.0,commit=6c474cf', ''),
