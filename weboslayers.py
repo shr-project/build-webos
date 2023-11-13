fcfed72467ef4dadf783eb14b2777d4773809a0d
Martin Jansa <martin2.jansa@lgepartner.com>
2023-11-13 09:33:44 +0100

    webos-image: Update layer pins for 2023-11-13 from mickledore
    oe-core as of 2023-11-11 08:23:53 -1000
    
    ae69823d94 build-appliance-image: Update to mickledore head revision
    1681813ef1 sudo: upgrade 1.9.13p3 -> 1.9.15p2
    74da05b636 python3-urllib3: update to v1.26.18
    d7ecf2b605 xserver-xorg: Fix for CVE-2023-5367 and CVE-2023-5380
    11370f237d qemu: fix "Bad FPU state detected" fault on qemu-system-i386
    07c31cd619 systemd: backport patch to fix warning in
            systemd-vconsole-setup
    b535bf84a0 linux-yocto: make sure the pahole-native available before
            do_kernel_configme
    914f5ed4cf openssl: Upgrade 3.1.3 -> 3.1.4
    4f21354c72 cve-check: don't warn if a patch is remote
    7d7d1de621 cve-check: slightly more verbose warning when adding the
            same package twice
    e32b4655e7 cve-check: sort the package list in the JSON report
    e2bd9494b5 libwebp: Fix CVE-2023-4863
    3625bed6d7 pixman: ignore CVE-2023-37769
    3623f48ff7 shadow: Fix CVE-2023-4641
    77a7921660 curl: Fix CVE-2023-38039

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.4,commit=c7e094ec3', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=mickledore,commit=e9ca1405b7', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=mickledore,commit=ae69823d94', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=mickledore,commit=3996883719', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
