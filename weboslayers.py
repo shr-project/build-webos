3fe58c080f6832e2befd97772eaf48735710b4bc
Martin Jansa <martin2.jansa@lgepartner.com>
2023-12-13 10:05:17 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2023-12-07 (build 6xx part 1/2) (WRQ-5184)
    oe-core as of 2023-12-07 08:09:37 -1000
    
    09ecafaf0e native: Clear TUNE_FEATURES/ABIEXTENSION
    9fb5f81f58 rust-cross: Simplfy the rust_gen_target calls
    0adada8111 rust-cross/rust-common: Merge arm target handling code to
            fix cross-canadian
    d8c030ef90 rust-cross-canadian: Fix ordering of target json config
            generation
    cccbb8358b rust-common: Set llvm-target correctly for cross SDK targets
    93458c3029 rust-llvm: Allow overriding LLVM target archs
    a351d62ca7 bash: changes to SIGINT handler while waiting for a child
    d910f67c61 cve-exclusion_5.10.inc: update for 5.10.202
    8f2c31ddd3 linux-yocto/5.10: update to v5.10.202
    630963a139 linux-yocto/5.10: update to v5.10.200
    2c379dc348 linux-yocto/5.10: update to v5.10.198
    7a6aab8b28 vim: upgrade 9.0.2068 -> 9.0.2130
    2d104f78cd python3-cryptography: fix CVE-2023-49083
    9c21b08c18 xwayland: fix CVE-2023-5367
    0dc086b37b libsndfile: fix CVE-2022-33065 Signed integer overflow
            in src/mat4.c
    
    meta-oe as of 2023-12-13 13:35:51 -0500
    
    402affcc07 squid: fix CVE-2023-46847 Denial of Service in HTTP Digest
            Authentication
    1117be8983 postgresql: fix CVE-2023-5868 CVE-2023-5869 CVE-2023-5870
    3fdd260209 nginx: Mitigate HTTP/2 Stream Resets Flood impact
    a9f070da58 python3-gcovr: switch to main branch
    ad3dc46c87 samba: fix CVE-2023-4091
    01c0aaaf62 sdbus-c++: Update ptest path
    cdab5037c9 frr: Fix for multiple CVE's
    
    :Issues Addressed:
    [WRQ-5184] CCC: Various build fixes
    
    Change-Id: I482675b5ed64a1a743f882c200e6ac83ccb9fd57

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=42a1c9fe6', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=11da43b58e', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=09ecafaf0e', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=8a75c61cce', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=402affcc07', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
