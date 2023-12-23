a1b4ca92517d7cff7890f7b3b775f0154dea34b4
Martin Jansa <martin2.jansa@lgepartner.com>
2023-12-23 11:35:01 +0100

    webos-image: Update layer pins for 2023-12-23 from master
    oe-core as of 2023-12-23 08:45:55 +0000
    
    6548354f04 archiver.bbclass: Drop tarfile module to improve performance
    cac54e6a6c python3-maturin: Add missing space appending to CFLAGS
    45bfbe5578 libdrm: Upgrade to 2.4.119
    c4ffd18d3e linux/cve-exclusion6.1: Update to latest kernel point
            release
    98dd722534 linux-yocto/6.1: update to v6.1.68
    1c8d29ab6b linux-yocto/6.5: fix AB-INT: QEMU kernel panic: No irq
            handler for vector
    a80b32127d linux-yocto/6.1: update to v6.1.67
    83c1336891 linux-yocto/6.1: update to v6.1.66
    53e612c42c linux-yocto-rt/6.1: update to -rt18
    d575b2822c linux-yocto/6.5: drop removed IMA option
    db11dfcd83 linux-yocto/6.1: drop removed IMA option
    67c507e3d4 image-live.bbclass: LIVE_ROOTFS_TYPE support compression
    12f92772c8 conf/machine-sdk: declare qemu-usermode SDK_MACHINE_FEATURE
    14571764b7 nativesdk: don't unset MACHINE_FEATURES, let machine-sdk/
            set it
    f560ac0a5c nativesdk: ensure features don't get backfilled
    605b4a91dc man-pages: remove conflict pages
    104b38e204 appstream: Upgrade 0.16.3 -> 1.0.0
    7c02a11691 libadwaita: update 1.4.0 -> 1.4.2
    8b1482a4e2 lib/packagedata.py: Fix broken symlinks for providers with a
            '/'
    3eeec7f341 lib/oe/path.py: Add relsymlink()
    2419afd802 ncurses: Fix - tty is hung after reset
    f0056dca0a devtool: deploy: provide max_process to strip_execs
    06a2f55337 debianutils: upgrade 5.14 -> 5.15
    6452368935 diffoscope: upgrade 252 -> 253
    1bc3e9bbaa ipk: Switch to using zstd compression
    ca7e78c8be oeqa: add runtime 'maturin develop' test case
    bf0bb7b94e xwayland: upgrade 23.2.2 -> 23.2.3
    e503e15a6c ttyrun: upgrade 2.29.0 -> 2.30.0
    1a3307753a python3-wcwidth: upgrade 0.2.11 -> 0.2.12
    feb3a23a68 python3-typing-extensions: upgrade 4.8.0 -> 4.9.0
    5b79e2f00f python3-trove-classifiers: upgrade 2023.11.22 -> 2023.11.29
    8ab849b0eb python3-pytest-runner: upgrade 6.0.0 -> 6.0.1
    69bc749e38 python3-psutil: upgrade 5.9.6 -> 5.9.7
    51bd305c02 python3-pip: upgrade 23.3.1 -> 23.3.2
    b26d7f4e98 python3-pathspec: upgrade 0.11.2 -> 0.12.1
    2fd616e68d python3-license-expression: upgrade 30.1.1 -> 30.2.0
    c19ed018ee python3-importlib-metadata: upgrade 6.8.0 -> 7.0.0
    e6c6637780 python3-hypothesis: upgrade 6.90.0 -> 6.92.1
    5ef9935ea0 python3-hatchling: upgrade 1.18.0 -> 1.20.0
    52d4b5476b python3-dbusmock: upgrade 0.29.1 -> 0.30.0
    db1967e48e python3-babel: upgrade 2.13.1 -> 2.14.0
    b02e282d4c p11-kit: upgrade 0.25.2 -> 0.25.3
    cebecc455d sysstat: upgrade 12.7.4 -> 12.7.5
    d345b1fb98 repo: upgrade 2.39 -> 2.40
    f6bee62670 opkg-utils: upgrade 0.6.2 -> 0.6.3
    7afab39fd1 kea: upgrade 2.4.0 -> 2.4.1
    178dab3a47 gpgme: upgrade 1.23.1 -> 1.23.2
    dc1c094e54 dnf: upgrade 4.18.1 -> 4.18.2
    2bd1443d04 libva-utils: upgrade 2.20.0 -> 2.20.1
    35c92894ec libseccomp: upgrade 2.5.4 -> 2.5.5
    9837ab2d59 libnl: upgrade 3.8.0 -> 3.9.0
    1dea52da8c libatomic-ops: upgrade 7.8.0 -> 7.8.2
    c153aca2f4 kbd: upgrade 2.6.3 -> 2.6.4

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=fe0881615', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=1bfa564f1a', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=6548354f04', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=10f1890af0', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
