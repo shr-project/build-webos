9fcd81831bea37226b8b369d6e8c29721b0ec51e
Martin Jansa <martin2.jansa@lgepartner.com>
2024-01-10 18:11:14 +0100

    webos-image: Update layer pins for 2024-01-10 from mickledore
    meta-oe as of 2024-01-07 13:15:11 -0500
    
    8e1f0fa6bf nginx: fix CVE-2023-44487
    b0d67900ae poco: fix branch

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=mickledore,commit=23b5141400', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=mickledore,commit=f29290563c', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=mickledore,commit=8e1f0fa6bf', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
