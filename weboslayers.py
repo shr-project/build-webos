6b7d9d2cf66a100dc1ea5867539ba199a9c5e4f3
Martin Jansa <martin2.jansa@lgepartner.com>
2024-03-01 11:07:56 +0100

    webos-image: Update layer pins for 2024-03-01 from scarthgap
    oe-core as of 2024-03-01 09:32:11 +0000
    
    f6064e573b python3-attrs: disable Hypothesis deadline
    a80ce6e3c0 rust: reproducibility issue fix with v1.75
    83bd5e12e0 rust: Revert PGO to it's default
    9aec2c6c77 rust: Upgrade 1.74.1 -> 1.75.0
    fce06a1632 linux-firmware: split out more firmware pieces
    db3bf550a3 llvm: Update to 18.1.0 RC4
    643b799a0c waf: Improve version parsing to avoid failing on warnings
    a741be42bd xprop: upgrade 1.2.6 -> 1.2.7
    3ae80d1870 xkeyboard-config: upgrade 2.40 -> 2.41
    c0aad6acc4 xkbcomp: upgrade 1.4.6 -> 1.4.7
    2f4fb72a33 swig: upgrade 4.2.0 -> 4.2.1
    dc3f5c4e04 python3-yamllint: upgrade 1.33.0 -> 1.35.1
    c1968ceedd python3-urllib3: upgrade 2.1.0 -> 2.2.1
    288bdc3420 python3: upgrade 3.12.1 -> 3.12.2
    91dd6f2878 python3-typing-extensions: upgrade 4.9.0 -> 4.10.0
    0174e7c4d1 python3-trove-classifiers: upgrade 2024.1.8 -> 2024.2.23
    2ba36c4ebf python3-setuptools-rust: upgrade 1.8.1 -> 1.9.0
    17b6104583 python3-pytz: upgrade 2023.4 -> 2024.1
    0a6824bc92 python3-pytest: upgrade 8.0.0 -> 8.0.2
    1b8054fb17 python3-pycairo: upgrade 1.25.1 -> 1.26.0
    73040d2ed2 python3-pip: upgrade 23.3.2 -> 24.0
    755e27ebef python3-hypothesis: upgrade 6.97.3 -> 6.98.12
    7967c57f30 python3-dbusmock: upgrade 0.30.2 -> 0.31.1
    d606655c7a python3-certifi: upgrade 2023.11.17 -> 2024.2.2
    cc8cacb07d python3-beartype: upgrade 0.17.0 -> 0.17.2
    5a6d00328c pkgconf: upgrade 2.1.0 -> 2.1.1
    bbda388857 pciutils: upgrade 3.10.0 -> 3.11.1
    3322ac8781 pango: upgrade 1.51.0 -> 1.52.0
    c24adeab82 ofono: upgrade 2.3 -> 2.4
    038313876c mpg123: upgrade 1.32.4 -> 1.32.5
    e9507afad6 makedepend: upgrade 1.0.8 -> 1.0.9
    23e2aa465d lighttpd: upgrade 1.4.73 -> 1.4.74
    872c3cb7ea libxvmc: upgrade 1.0.13 -> 1.0.14
    4129c497c2 libxkbfile: upgrade 1.1.2 -> 1.1.3
    dc4bddeebb libxext: upgrade 1.3.5 -> 1.3.6
    351d9c9b06 liburi-perl: upgrade 5.25 -> 5.27
    f9be1e083d libunistring: upgrade 1.1 -> 1.2
    b0494d7fa7 libsecret: upgrade 0.21.2 -> 0.21.4
    b92fb50237 libpng: upgrade 1.6.41 -> 1.6.42
    e546932671 libmicrohttpd: upgrade 0.9.77 -> 1.0.1
    f476d845fe libksba: upgrade 1.6.5 -> 1.6.6
    3256d357dc libhandy: upgrade 1.8.2 -> 1.8.3
    e484b0dda4 libgpg-error: upgrade 1.47 -> 1.48
    0ea9b851f3 libffi: upgrade 3.4.5 -> 3.4.6
    238ba27471 git: upgrade 2.43.0 -> 2.44.0
    5084a7f8a0 gcr: upgrade 4.1.0 -> 4.2.0
    d8fdd64d13 efivar: upgrade 38 -> 39
    49b6535779 ed: upgrade 1.20 -> 1.20.1
    81316b98d2 createrepo-c: upgrade 1.0.3 -> 1.0.4
    df17a0d566 ccache: upgrade 4.9 -> 4.9.1
    64c0587b69 bash-completion: upgrade 2.11 -> 2.12.0
    6cf98a4954 mirrors: Switch llvm to use shallow cloning
    c2c9cbc107 gstreamer1.0: skip a test that is known to be flaky
    da7cc5def2 oeqa/lib/utils/postactions: fix host disk usage stats
            retrieval
    83dad4a93f python3-cryptography{-vectors}: upgrade to 42.0.5
    1048992c0d libc-locale: fix ASCII compatible warning cause build
            failure.
    c9cb2eb2e6 rpm: Fix the following error when run nativesdk-rpm in
            nativesdk environment.
    e92d7dd55d python3: dont disable readline module for editline
    728784e4df python3-maturin: Recognise riscv32 architecture
    33bd36308b python3-bcrypt: Fix build break on arches without 64
            bit atomics
    b4dc9793f3 python3-git: upgrade 3.1.41 -> 3.1.42
    57b6a329df wpa-supplicant: Fix CVE-2023-52160
    3f8da47b54 libexif: remove unused version_underscore

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=1f4d517b7', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=efa1420085', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=f6064e573b', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=9f0e513211', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
