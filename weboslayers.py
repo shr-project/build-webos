96fbda65a6929759c0500b57a7ddb5fa920c6755
Martin Jansa <martin2.jansa@lgepartner.com>
2024-03-01 16:47:10 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-02-28 (build 7xx part 1/2) (WRQ-12963)
    oe-core as of 2024-03-03 11:56:20 -1000
    
    d63af11e92 kernel.bbclass: Set pkg-config variables for building
            modules
    5f9463d8c9 tzdata : Upgrade to 2024a
    122be5a683 linux-firmware: upgrade 20231030 -> 20231211
    f7ab2e9990 bind: Upgrade 9.18.19 -> 9.18.24
    1745208bc0 cve_check: cleanup logging
    6488fc1a8f dbus: Add missing CVE_PRODUCT
    e9af3d328d qemu: Fix CVE-2023-42467
    e993720fc3 qemu: Backport fix CVE-2023-6693
    71600de72c qemu: Fix for CVE-2024-24474
    c8b30db7fc glibc: ignore CVE-2023-0687
    8d91ab2093 less: Fix for CVE-2022-48624
    cca0971a7d kernel: make LOCALVERSION consistent between recipes
    74897e505d kernel: fix localversion in v6.3+
    29a0d2a49d kernel.bbclass: introduce KERNEL_LOCALVERSION
    63998f13d5 scripts: python 3.12 regex
    d4e11eebdf meta/recipes: python 3.12 regex
    939046c5e9 meta/lib/oeqa: python 3.12 regex
    2331982cf4 feature-microblaze-versions.inc: python 3.12 regex
    095598eb7c oeqa/selftest/oelib/buildhistory: git default branch
    979b216330 oeqa/selftest/recipetool: expect meson.bb
    24bf7bbd1f oeqa/selftest/recipetool: downgrade meson version to not
            use pyproject.toml
    9c1a1110a3 oeqa/selftest/recipetool: fix for python 3.12
    a20325690a oeqa: replace deprecated assertEquals
    31de620e9d runqemu: add qmp socket support
    50815c328e populate_sdk_ext: use ConfigParser instead of
            SafeConfigParser
    2f8982ef4c scripts/runqemu: fix regex escape sequences
    b9dcdf2346 recipetool/create_buildsys_python: use importlib instead
            of imp
    
    bitbake as of 2024-02-27 04:20:42 -1000
    
    40fd5f4ee fetch2: Ensure that git LFS objects are available
    2a6fd7744 tests/fetch: Add real git lfs tests and decorator
    1d5d4a46c tests/fetch: git-lfs restore _find_git_lfs
    7efed7bc0 bitbake/lib/bs4/tests/test_tree.py: python 3.12 regex
    6c19b6cf1 bitbake/codeparser.py: address ast module deprecations in
            py 3.12
    459101144 codeparser: replace deprecated ast.Str and 's'
    
    :Issues Addressed:
    [WRQ-12963] CCC: Various build fixes
    
    Change-Id: I3245d63642856546037f5012b297415e18c69078

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=a4c516ef5', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=40fd5f4ee', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=5103ce6774', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=d63af11e92', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=fda737ec0c', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
