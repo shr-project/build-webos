ef074c78f7c5445457c042f9d876462fd5b83b6e
Martin Jansa <martin2.jansa@lgepartner.com>
2024-03-25 16:20:38 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-03-19 (build 7xx part 1/2) (WRQ-15156)
    oe-core as of 2024-03-19 03:33:32 -1000
    
    1b5405955c glibc: Fix subscript typos for get_nscd_addresses
    b2de7d7569 stress-ng: avoid calling sync during do_compile
    d8e3d1eb48 yocto-uninative: Update to 4.4 for glibc 2.39
    e5956d94ba linux-firmware: upgrade 20231211 -> 20240220
    11c9c6eec5 wireless-regdb: Upgrade 2023.09.01 -> 2024.01.23
    3af65ed130 wireless-regdb: upgrade 2023.05.03 -> 2023.09.01
    717f0df5f3 cve-update-nvd2-native: Remove rejected CVE from database
    38402b5e89 cve-update-nvd2-native: Fix CVE configuration update
    a98387021d cve-update-nvd2-native: nvd_request_next: Improve comment
    b6285f0549 cve-update-nvd2-native: Remove duplicated CVE_CHECK_DB_FILE
            definition
    5259971a47 cve-update-nvd2-native: Add an age threshold for incremental
            update
    6f49c54a0e cve-update-nvd2-native: Fix typo in comment
    7864c4605c python3-cryptography: Backport fix for CVE-2024-26130
    aa20dd9eb6 expat: fix CVE-2023-52426
    c02175e973 expat: patch CVE-2024-28757
    
    :Issues Addressed:
    [WRQ-15156] CCC: Various build fixes
    
    Change-Id: I9da66067ee8396292781d1330f5fc9baa936a2b1

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=40fd5f4ee', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2501534c95', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=1b5405955c', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=fda737ec0c', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
