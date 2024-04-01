286fed3281509501c0498f1c44217628bff0bce3
Martin Jansa <martin2.jansa@lgepartner.com>
2024-04-01 12:30:26 +0200

    webos-image: Update layer pins for 2024-04-01 from scarthgap
    oe-core as of 2024-03-31 06:56:57 +0100
    
    8cfc94a440 oeqa/sstatetests: Fix race issue
    fac4d33041 oe/package: enhance objdump command call with llvm
    daecdd5772 utils: enhance readelf command call with llvm
    22174e5b64 mesa: Drop LLVM-17 patch
    8509edeafe systemd: disable mdns feature in resolved for zeroconf
    c74048f5ff readline: Apply patches from readline-8.2-patches
    97afe73e6f curl: fix quoting when disabling flaky tests
    6a4c83919f populate_sdk_ext.bbclass: only overwirte lsb string if
            uninative is used
    b104470763 cml1: prompt location of updated .config after
            do_menuconfig()
    035fe46fbf cml1: remove needless check for write_taint attribute
    d86c0e3468 dnf: fix Exception handling for class ProcessLock
    a04a084b6e bblayers/makesetup.py: Move git utility functions to
            oe.buildcfg module
    04a8f73762 tcl: Forward port skip logic for musl ptests
    151dfe7a7b valgrind: Backport fixes from 3.22 branch
    438a390e8e openssl: fix crash on aarch64 if BTI is enabled but no
            Crypto instructions
    cad5d53e13 gstreamer: upgrade 1.22.10 -> 1.22.11
    96d37df074 run-postinsts: Add workaround for locking deadlock issue
    d2b784110e util-linux: Add fcntl-lock
    24078c0542 util-linux: Add missing MIT license
    b0d64611c8 linux-yocto/6.6: update CVE exclusions (6.6.23)
    1b6c377536 linux-yocto/6.6: update to v6.6.23
    ee8e8b75fd linux-yocto/6.6: nftables: ptest and cleanup tweaks
    1334de8faf linux-yocto/6.6: cfg: genericarm64 configuration updates
    0db3466303 selftest/sstatetests: run CDN check twice, ignoring errors
            the first time
    684ac8005a glib-2.0: skip a timing sensitive ptest
    7750469e0a liberror-perl: move to meta-perl
    16c8c8de33 coreutils: drop obsolete liberror-perl RDEPENDS
    
    meta-oe as of 2024-03-31 13:42:55 -0700
    
    7f301195d8 liberror-perl: move recipe from oe-core
    e11b291d50 meta-python-image-ptest: Use 2G RAM for some demanding tests
    41d5dfbf91 python3-ecdsa: remove python3-pbr
    d471828ddc gnome-user-share: remove hardcoded paths
    dbb85013ae python3-pydbus: Drop ${PYTHON_PN}
    e6673cfd15 python3-validators: upgrade 0.22.0 > 0.24.0 and enable ptest
    469a39e8c0 gdm: update 45.0.1 -> 46.0
    27ce5289b1 gtkmm4: add x11 to REQUIRED_DISTRO_FEATURES
    7b4e9e6405 gnome-control-center: update 46.0 -> 46.0.1
    4ed68ecd75 gnome-user-share: add recipe
    44c5937a9c apache2: preset mpm=prefork by default
    ea071cfc13 microsoft-gsl: Disable disabled-macro-expansion warning
            as error on clang/musl

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=6366ea8d9', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=47ea9418df', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=8cfc94a440', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=b29e413d92', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=7f301195d8', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
