fd5d8e545eac0c9754d79e9f4c20ba967a850882
Martin Jansa <martin2.jansa@lgepartner.com>
2024-04-21 20:52:45 +0200

    webos-image: Update layer pins for 2024-04-21 from master
    bitbake as of 2024-04-20 07:27:38 +0100
    
    97ffe1431 fetch2/gcp: Add missing runfetchcmd import
    
    meta-oe as of 2024-04-20 11:05:12 -0700
    
    87cdaf04b1 ydotool: Do not package systemd unit files on non-systemd
            distros
    eb30a6b92b sdbus-c++: Fix build and upgrade to latest git
    d607d161bf aravis: new recipe
    b5ebed99e2 wireplumber: update 0.5.0 -> 0.5.1
    534032bbe2 pipewire: update 1.0.4 -> 1.0.5
    a85d23b314 python3-traitlets: Upgrade to 5.14.3
    0ef3e9918d freediameter: fix dependency from libidn to libidn2
    c341cdb58c apache2: Upgrade v2.4.58 -> v2.4.59
    5f2f51409e yajl: set correct homepage
    37f98cb038 xmlsec1: upgrade 1.3.3 -> 1.3.4
    a0ac2f5290 webkitgtk3: upgrade 2.44.0 -> 2.44.1
    af3d9fe291 toybox: upgrade 0.8.10 -> 0.8.11
    25346e6908 tcsh: upgrade 6.24.11 -> 6.24.12
    a898418538 st: upgrade 0.9.1 -> 0.9.2
    8c1aa2b8ae squid: upgrade 6.8 -> 6.9
    b58ee068c1 sngrep: upgrade 1.8.0 -> 1.8.1
    2824a7ef39 rdma-core: upgrade 50.0 -> 51.0
    ca39c1572c python3-zopeinterface: upgrade 6.2 -> 6.3
    24c2f5febe python3-web3: upgrade 6.16.0 -> 6.17.0
    c1fb68c20f python3-validators: upgrade 0.24.0 -> 0.28.0
    370ac7784b python3-sentry-sdk: upgrade 1.44.0 -> 1.45.0
    33cd26f803 python3-send2trash: upgrade 1.8.2 -> 1.8.3
    5f8cac6466 python3-rarfile: upgrade 4.1 -> 4.2
    cb74f0eab2 python3-pymodbus: upgrade 3.6.6 -> 3.6.7
    e8869a25d0 python3-pydantic: upgrade 2.6.4 -> 2.7.0
    c5df6e5e40 python3-pulsectl: upgrade 23.5.2 -> 24.4.0
    8848b7b8d6 python3-pdm: upgrade 2.13.2 -> 2.14.0
    de06099420 python3-path: upgrade 16.10.0 -> 16.14.0
    8607271e63 python3-parso: upgrade 0.8.3 -> 0.8.4
    0dc34e2d41 python3-joblib: upgrade 1.3.2 -> 1.4.0
    2cc5fb4bb6 python3-javaobj-py3: upgrade 0.4.3 -> 0.4.4
    1c716a99c2 python3-ipython: upgrade 8.22.2 -> 8.23.0
    f6db1f738f python3-google-api-python-client: upgrade 2.124.0 -> 2.125.0
    9c18bb1ada python3-filelock: upgrade 3.13.3 -> 3.13.4
    1c755d1a2d python3-execnet: upgrade 2.0.2 -> 2.1.1
    39a4c6ed8d python3-eth-typing: upgrade 4.0.0 -> 4.1.0
    e4b687c2c4 python3-eth-account: upgrade 0.11.0 -> 0.12.1
    0069363f26 python3-eth-abi: upgrade 5.0.1 -> 5.1.0
    00e390e611 python3-django: upgrade 5.0.3 -> 5.0.4
    08b6f12632 python3-cbor2: upgrade 5.6.2 -> 5.6.3
    d107f358bc python3-aiohttp: upgrade 3.9.3 -> 3.9.4
    8563c33054 python3-aiodns: upgrade 3.1.1 -> 3.2.0
    89af1300cf opensc: upgrade 0.25.0 -> 0.25.1
    d4ce52c1e7 makedumpfile: upgrade 1.7.4 -> 1.7.5
    beb832955e libopus: upgrade 1.5.1 -> 1.5.2
    20ea3999df libcrypt-openssl-random-perl: upgrade 0.15 -> 0.16
    10c4507b53 libbpf: upgrade 1.3.0 -> 1.4.0
    0c3d4c5a30 hwdata: upgrade 0.380 -> 0.381
    03f95c59ae gtkwave: upgrade 3.3.117 -> 3.3.119
    2969bba6db gnome-text-editor: upgrade 46.0 -> 46.1
    20d214010a gnome-online-accounts: upgrade 3.50.0 -> 3.50.1
    f6b1a0f731 file-roller: upgrade 44.0 -> 44.1
    c7a2dde455 cryptsetup: upgrade 2.7.1 -> 2.7.2
    2f4f2cfef1 bindfs: upgrade 1.17.6 -> 1.17.7
    baab654008 adw-gtk3: upgrade 5.2 -> 5.3
    d510636b95 abseil-cpp: upgrade 20240116.1 -> 20240116.2
    392b844722 libcbor: use shared libraries
    61bcec1215 libtorrent-rasterbar: fix CVE mapping
    ffb9e53b12 libtorrent: remove incorrect CVE mapping
    8d6078b19b nng: upgrade 1.5.2 -> 1.7.3
    4d41a11052 Revert "nng: upgrade 1.5.2 -> 12"
    c90c045775 python3-scrypt: Move from PTESTS_PROBLEMS_META_PYTHON
            to PTESTS_SLOW_META_PYTHON

--- weboslayers.py
+++ weboslayers.py
@@ -58,11 +58,11 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=67a1aa8db', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=master,commit=97ffe1431', ''),
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=master,commit=e956f1116d', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=24044fd3a0', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=master,commit=87cdaf04b1', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -73,7 +73,7 @@ webos_layers = [
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.6.0,commit=6c474cf', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=scarthgap-2024-04-21-b', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=master,commit=master-2024-04-21-b', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=master,commit=1879cb8', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
