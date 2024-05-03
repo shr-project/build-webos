efd43fdb13e0f5245656e466c4c90914ce243ba1
Martin Jansa <martin2.jansa@lgepartner.com>
2024-05-03 07:55:46 +0200

    webos-image: Update layer pins for 2024-05-03 from scarthgap
    bitbake as of 2024-05-02 16:07:18 +0100
    
    8f90d10f9 cooker: Use hash client to ping upstream server
    
    meta-oe as of 2024-05-01 11:00:51 -0700
    
    a72010b414 Revert "nautilus: update 45.1 -> 46.1"
    a67c2d04a0 renderdoc: remove vim-native DEPENDS
    e642f62eee nautilus: update 45.1 -> 46.1
    63db9c0d52 networkmanager: fix gir build
    d8ba3e4400 networkd-dispatcher: Add dependency on python3-json
    41e8a4b855 open-vm-tools: Update status for CVE-2014-4199 and
            CVE-2014-4200
    3be6d57c67 sthttpd: Update status for CVE-2017-10671
    566703d589 mpd: Update status for CVE-2020-7465 and CVE-2020-7466
    cf8c850690 procmail: Update status for CVE-1999-0475
    e7fdb6c5b2 st: Update status for CVE-2017-16224
    a64e300c72 libgweather: fix build with gobject-introspection 1.80.0
    9fed5812ab msgraph: Add opengl to REQUIRED_DISTRO_FEATURES
    2e25f21f52 reproducibility: move repro excludes from AB config.json
            to meta-oe
    fe0ba70f91 python3-protobuf: drop python3-six from RDEPENDS
    9b3b5a900d python3-pyhamcrest: cleanup RDEPENDS and correct build
            backend
    5e3b83ba99 fwupd: Upgrade to 1.9.18 release
    c9c56287b7 nodejs: upgrade 20.11.1 -> 20.12.2
    7017017b48 chore(sdbus-c++): upgrade to 2.0.0 release
    98d499b238 rygel: update 0.42.4 -> 0.42.5
    2392a26b4c gssdp: fix a reproducibility issue
    a29bbcb236 gupnp: fix reproducibility issue
    542b544afe gnome-control-center: update 46.0.1 -> 46.1
    162013701b python3-gcovr: add dep on python3-colorlog
    8340dc7dd5 python3-colorlog: BBCLASEXTEND native nativesdk
    63d546fb21 soci: update build options
    d6de210817 Add class for appending dm-verity hash data to block
            device images
    e186d26bbb python3-txdbus: cleanup RDEPENDS
    7167eed066 python3-incremental: cleanup RDEPENDS and use
            python_setuptools_build_meta
    a57eea75cd python3-twisted: upgrade 22.10.0 -> 24.3.0
    42773342f5 gnome-remote-desktop: update 46.0 -> 46.1
    0b8e416edc spice-gtk: add PACKAGECONFIG for webdav
    1ebbb331ea spice-gtk: use hwdata instead of usbids
    d76329247a sanlock: upgrade 3.9.1 -> 3.9.2
    11998cf015 python3-yamlloader: upgrade 1.3.2 -> 1.4.1
    90c5d29327 python3-regex: upgrade 2023.12.25 -> 2024.4.16
    cb3bd0f52f python3-pywbemtools: upgrade 1.2.1 -> 1.3.0
    b4736767d4 python3-pywbem: upgrade 1.6.3 -> 1.7.2
    70fda4950f python3-pymisp: upgrade 2.4.188 -> 2.4.190
    4fd132d894 python3-pycups: upgrade 2.0.1 -> 2.0.4
    6131308eee python3-grpcio: upgrade 1.62.1 -> 1.62.2
    4916f56a28 python3-grpcio-tools: upgrade 1.62.1 -> 1.62.2
    e7473f7ab8 python3-croniter: upgrade 2.0.3 -> 2.0.5
    e6909d8b68 python3-argcomplete: upgrade 3.2.3 -> 3.3.0
    ff6b6f57bc openfortivpn: upgrade 1.21.0 -> 1.22.0
    3864450575 mpich: upgrade 4.2.0 -> 4.2.1
    ac4e8ed3d4 gensio: upgrade 2.8.3 -> 2.8.4
    f9a54da4aa asio: upgrade 1.28.0 -> 1.30.2
    c9c6d6bda5 aravis: fix LICENSE and enable viewer PACKAGECONFIG only
            with GTK3DISTROFEATURES
    876a9665f9 gst-instruments: enable ui PACKAGECONFIG only with
            GTK3DISTROFEATURES
    4f5eda4fc3 vlc: do not depend on mpeg2dec
    e8df6ea909 gnome-shell-extensions: update 46.0 -> 46.1
    513ee1643e gnome-calendar: update 46.0 -> 46.1
    4232211fc0 xdg-desktop-portal-gnome: update 46.0 -> 46.1
    33cdf41004 mutter: update 46.0 -> 46.1
    c02847df91 gnome-shell: update 46.0 -> 46.1
    
    meta-virtualization as of 2024-05-02 18:16:26 +0000
    
    1a547c0a kubernetes: Pass -trimpath to build
    089cbaba containerd-opencontainers: fix wrong version display
    829dc1f9 cni: pass BuildVersion info to avoid runtime warning
    0ad7db7d kubernetes: skip textrel QA warning
    56168b10 podman: Fix missing brace in FILES
    6e7db363 ceph: Add the missing Upstream-Status Field
    b339d02b diod: Build in 32bit systems with 64bit time_t

--- weboslayers.py
+++ weboslayers.py
@@ -58,18 +58,18 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=c86466d51', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=8f90d10f9', ''),
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=b65b4e5a8e', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=6819f825ca', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=a72010b414', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=66414c1', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=3ff80e14', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=1a547c0a', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.6.0,commit=6c474cf', ''),
 
