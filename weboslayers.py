53f00225a8d4e6ff7ab160d5a4192b7e5d7bce1f
Martin Jansa <martin2.jansa@lgepartner.com>
2024-05-29 12:19:10 +0200

    webos-image: Update layer pins for 2024-05-29 from scarthgap
    oe-core as of 2024-05-19 13:50:01 -0700
    
    0795169be2 oeqa/selftest/debuginfod: use localpkgfeed to speed
            server startup
    00e4eb179b selftest/classes: add localpkgfeed class
    20a6f55328 lib/oe/package-manager: allow including self in
            create_packages_dir
    b198617a98 recipetool: Handle several go-import tags in go resolver
    d61934122c recipetool: Handle unclean response in go resolver
    6a81ccc68f bash: Fix file-substitution error-handling bug
    b5406d3691 patchtest: test_metadata: fix invalid escape sequences
    c0708adce6 libcgroup_3.1.0: fix build on non-systemd systems
    39fc503036 classes: image_types: apply EXTRA_IMAGECMD:squashfs*
            in oe_mksquashfs()
    137bb70ddf Revert "goarch: disable dynamic linking globally"
    941c8535ea go: Drop the linkmode completely
    0862abfede uboot-sign: fix loop in do_uboot_assemble_fitimage
    f891b66b98 webkitgtk: 2.44.0 -> 2.44.1
    c2666c09e9 llvm: upgrade 18.1.2 -> 18.1.3
    939140fa72 glibc: correct license
    a8b1034d97 glibc: Update to latest on stable 2.39 branch
    7f3e6019a9 glibc: Update to latest on stable 2.39 branch
    51da20dc57 glib-2.0: Upgrade 2.78.5 -> 2.78.6
    14de0c10f6 glib-2.0: Upgrade 2.78.4 -> 2.78.5
    bd6a6733c9 openssl: patch CVE-2024-4603
    55cca07079 ncurses: Fix CVE-2023-50495
    294a7dbe44 build-appliance-image: Update to scarthgap head revision
    e3b4a05f19 python3: skip test_concurrent_futures/test_shutdown
    704d369509 build-appliance-image: Update to scarthgap head revision
    6954cd4bb6 systemd: sed ROOT_HOME only if sysusers PACKAGECONFIG is set
    4bf3f11e39 scripts/oe-setup-build: write a build environment
            initialization one-liner into the build directory
    4138f0a048 sstate.bbclass: Add _SSTATE_EXCLUDEDEPS_SYSROOT to
            vardepsexclude
    
    bitbake as of 2024-05-28 15:27:08 +0100
    
    b5159c037 fetch2/gcp: Add missing runfetchcmd import
    2aa519fec parse: Improve/fix cache invalidation via mtime
    93190565f cooker: Handle ImportError for websockets
    
    meta-oe as of 2024-05-22 12:44:19 -0700
    
    6de0ab7443 nodejs-oe-cache: fix offline install of dependencies
    
    meta-raspberrypi as of 2024-05-28 23:14:17 +0100
    
    0f46a1d rpi-cmdline: Fix being renamed of network interfaces
    fb03b44 raspberrypi5.conf: Fix KERNEL_IMAGETYPE_UBOOT
    6d59364 u-boot: re-enable rapsberrypi5
    16656a6 layer.conf: rpi5 recommends lts-u-boot-mixin
    6160d0d linux: drop unused 5.15 version
    e7476ed linux: drop unused rpi4-64-kernel-misc.cfg
    
    meta-clang as of 2024-05-07 17:45:27 -0700
    
    e7dceb1 clang: use release tarball instead of git
    ad26539 frame: Use --undefined-version with lld
    4d4987d clang: Upgrade to 18.1.5

--- weboslayers.py
+++ weboslayers.py
@@ -58,17 +58,17 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=8f90d10f9', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=b5159c037', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=e38a0f1a5e', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=0795169be2', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=a72010b414', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=6de0ab7443', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=66414c1', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=e7dceb1', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=1a547c0a', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.6.0,commit=6c474cf', ''),
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=scarthgap,commit=scarthgap-2024-05-29-b', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=scarthgap,commit=1879cb8', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=scarthgap,commit=0f46a1d', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
