701d68d15e09114abcbdc135cb2dd6eb98fd4ea2
Martin Jansa <martin2.jansa@lgepartner.com>
2024-06-02 13:01:47 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-06-01 (build 25xx part 1/2) (WRQ-23181)
    oe-core as of 2024-06-01 19:12:27 -0700
    
    ab2649ef6c build-appliance-image: Update to kirkstone head revision
    209c41377a git: Fix multiple CVEs
    28d9f94853 util-linux: Fix CVE-2024-28085
    66228a9e81 ghostscript: fix CVE-2023-52722
    18e03cadca ghostscript: fix CVE-2024-29510
    edcaa55aa5 ghostscript: fix CVE-2024-33871
    fb0271a2d4 ghostscript: fix CVE-2024-33869
    9f0c63b568 ghostscript: fix CVE-2024-33870
    
    :Issues Addressed:
    [WRQ-23181] CCC: Various build fixes
    
    Change-Id: Ia14de77e61008f748163761b7c0093bd1e9cedf4

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=5a90927f3', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=e0a1ed7aa1', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=ab2649ef6c', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=0560b84899', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
