62a2f16aabf9f8c815366f06da1202e3b88f2663
Martin Jansa <martin2.jansa@lgepartner.com>
2024-07-15 21:15:34 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-07-15 (build 7xx part 1/2) (WRQ-28951)
    oe-core as of 2024-07-15 10:31:11 -0700
    
    5d97b0576e build-appliance-image: Update to kirkstone head revision
    
    :Issues Addressed:
    [WRQ-28951] CCC: Various build fixes
    
    Change-Id: Iedcf3e062fedbd96f7c7eb052204903f9ffbcb82

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=734b0ea3d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=8e65050688', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=5d97b0576e', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=6ff0748a47', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
