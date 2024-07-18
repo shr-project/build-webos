ce048e8d12cc8e630a2b35ce7b5ce30ec58352eb
Martin Jansa <martin2.jansa@lgepartner.com>
2024-07-18 13:54:44 +0200

    webos-image: Update layer pins for 2024-07-18 from scarthgap
    oe-core as of 2024-07-13 07:07:10 -0700
    
    bf3fe8c01c webkitgtk: fix do_compile errors on beaglebone-yocto
    feeadc65bd webkitgtk: fix do_configure error on beaglebone-yocto
    7eef8839d1 cve-exclusion: Drop the version comparision/warning
    20276890b2 iptables: submit
            0001-configure-Add-option-to-enable-disable-libnfnetlink.patch
            upstream
    35675cd810 perl: submit the rest of determinism.patch upstream
    8c03f8c491 grub: mark
            grub-module-explicitly-keeps-symbole-.module_license.patch as
            a workaround
    1b3da78af1 vorbis: mark patch as Inactive-Upstream
    2a83956abf kexec-tools: submit
            0003-kexec-ARM-Fix-add_buffer_phys_virt-align-issue.patch upstream
    eb06788f3a dnf/mesa: Fix missing leading whitespace with ':append'
    5dabb17313 mesa: remove obsolete
            0001-meson.build-check-for-all-linux-host_os-combinations.patch
    49ae51c05e curl: skip FTP tests in run-ptest
    14ee7a2310 meson: don't use deprecated pkgconfig variable
    506673555f ncurses: switch to new mirror
    37ddcbd51e selftest/cases/runtime_test: Exclude centos-9 from virgl
            tests
    98119e3f45 sdk: Fix path length limit to match reserved size
    5726348e04 populate_sdk_ext.bbclass: Fix undefined variable error
    35be70c8c7 linux-yocto: Enable team net driver
    d7019c20a6 libportal: fix rare build race
    5b00a8efdf gstreamer1.0: skip another known flaky test
    ab3ed4270a wic: engine.py: use raw string for escape sequence
    941474ed77 llvm: upgrade 18.1.5 -> 18.1.6
    2c473a2cad ed: upgrade 1.20.1 -> 1.20.2
    03828e381e fribidi: upgrade 1.0.13 -> 1.0.14
    32e8402f9f libcap-ng-python: upgrade 0.8.4 -> 0.8.5
    5eff01bbc5 libcap-ng: upgrade 0.8.4 -> 0.8.5
    16685f3b2d ruby: fix CVE-2024-27281
    0f2cd2bbad cpio: mark CVE-2023-7216 as disputed
    
    meta-oe as of 2024-07-09 08:18:46 -0400
    
    18f939a5fb libdbd-mysql-perl: avoid invoking assert_lib at do_configure
            stage
    4f9606591a flatbuffers: adapt for cross-compilation environments
    85e08ec256 python3-pyyaml-include: support native and nativesdk build
    41fedbb8a9 krb5: upgrade 1.21.2 -> 1.21.3
    d570d397c0 thin-provisioning-tools: install missed thin_shrink and
            era_repair
    0cbf21fd51 apache2: Upgrade 2.4.59 -> 2.4.60
    e9318e6519 meta-oe/conf/layer.conf: remove libbpf from
            NON_MULTILIB_RECIPES for x86 and x86-64

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=8714a02e1', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=d511c41dac', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=bf3fe8c01c', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=78a14731cf', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=18f939a5fb', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
