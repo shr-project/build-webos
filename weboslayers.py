166e8740e3a4f6d6b53b1fdabbef72cbbbbcafe0
Martin Jansa <martin2.jansa@lgepartner.com>
2024-08-28 18:57:24 +0200

    webos-image: Update layer pins for 2024-08-28 from scarthgap
    oe-core as of 2024-08-20 05:03:49 -0700
    
    8b5c66c91d oeqa/utils/postactions: transfer whole archive over ssh
            instead of doing individual copies
    9f0eba6aba oeqa/selftest/reproducibile: Explicitly list virtual targets
    52a67132d4 oeqa/runtime/ssh: check for all errors at the end
    1bd6b0e296 oeqa/runtime/ssh: In case of failure, show exit code and
            handle -15 (SIGTERM)
    f6eacc39dc oeqa/runtime/ssh: add retry logic and sleeps to allow for
            slower systems
    fd21b5fa15 qemuboot: Trigger write_qemuboot_conf task on changes of
            kernel image realpath
    bdf07c1eb2 populate_sdk_ext.bclass: make sure OECORE_NATIVE_SYSROOT
            is exported.
    310e9387a7 librsvg: don't try to run target code at build time
    90a510acd1 gstreamer1.0: disable flaky baseparser tests
    58a5ddeff4 glibc: fix fortran header file conflict for arm
    63ecb048f8 libgfortran.inc: fix nativesdk-libgfortran dependencies
    d53b627a68 os-release: Fix VERSION_CODENAME in case it is empty
    29b33a9329 initramfs-framework: fix typos
    97c2cf74e5 maintainers.inc: add self for unassigned python recipes
    bd451b9cd9 llvm: Enable libllvm for native build
    bd62a437dd go: upgrade 1.22.5 -> 1.22.6
    7fd3c7e974 Tiff: Security fix for CVE-2024-7006
    1103182ac9 ruby: Backport fix for CVE-2024-27282
    
    meta-oe as of 2024-08-25 15:15:10 -0400
    
    1235dd4ed4 python3-twisted: Fix CVE-2024-41671
    399b7b9051 gtk+: Fix CVE-2024-6655
    208caeb50a krb5: fix CVE-2024-26458 and CVE-2024-26461
    c432a61a29 poppler: CVE-2024-6239
    b7148ebb47 nginx: Backport fix for CVE-2024-7347
    28f14d5d19 netplan: add missing runtime dependencies
    17aacc80f7 networkmanager: remove modemmanager rdepends
    be2127dce5 squid: patch CVE-2024-37894
    8887a98d27 libndp: Patch CVE-2024-5564
    8e6bee4c20 cjson: upgrade 1.7.17 -> 1.7.18
    
    meta-clang as of 2024-08-21 13:26:58 -0700
    
    2b74336 Revert "libcxx: Do not induce -mbranch-protection externally
            on arm64"
    bff7fe1 nonclangable: remove '-mbranch-protection=standard' globally
            on aarch64
    722c8ad Update libcxx_git.bb

--- weboslayers.py
+++ weboslayers.py
@@ -60,15 +60,15 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=d251668d9', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=bbb8db8fec', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=8b5c66c91d', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=64c481d017', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=1235dd4ed4', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=7d9bf6d', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=2b74336', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=54b806b1', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.6.0,commit=6c474cf', ''),
