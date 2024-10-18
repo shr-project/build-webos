f71e677d6dbd04bcd1504c06b94406144a148605
Martin Jansa <martin2.jansa@lgepartner.com>
2024-10-18 19:27:21 +0200

    webos-image: Update layer pins for 2024-10-18 from scarthgap
    oe-core as of 2024-10-16 06:21:24 -0700
    
    a1b28a88bc image.bbclass: Drop support for ImageQAFailed exceptions
            in image_qa
    bec6b535c9 meta-ide-support: Mark recipe as MACHINE-specific
    8fe7aef17e image_qa: fix error handling
    7ee654579c libsdl2: Fix non-deterministic configure option for
            libsamplerate
    bb7dad0106 recipes-bsp: usbutils: Fix usb-devices command using busybox
    7f81c38e91 uboot-sign: fix counters in do_uboot_assemble_fitimage
    44c1f003d6 runqemu: Fix detection of -serial parameter
    865b7149da makedevs: Fix matching uid/gid
    f944a1be48 lib/oe/package-manager: skip processing installed-pkgs
            with empty globs
    17049482f0 virglrenderer: Add patch to fix -int-conversion build issue
    a31b46627a license: Fix directory layout issues
    31861ad29c libpcre2: Update base uri PhilipHazel -> PCRE2Project
    e39cad8d84 sysvinit: take release tarballs from github
    edb7968fe2 ptest-runner: Update 2.4.4 -> 2.4.5
    69ffe5bc09 ruby: upgrade 3.2.2 -> 3.3.5
    14b1560b2f linux-firmware: upgrade 20240312 -> 20240909
    8b520c3cea libarchive: fix CVE-2024-48957 & CVE-2024-48958
    f00847d91e cups: Backport fix for CVE-2024-47175
    6ed9968bf3 rust: ignore CVE-2024-43402

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=d251668d9', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=5ea3ba0053', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=a1b28a88bc', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=72018ca1b1', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
