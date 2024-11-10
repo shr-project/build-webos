7ae5e9e6e314f9ae9b5baa8318add9f4b3b96419
Martin Jansa <martin2.jansa@lgepartner.com>
2024-11-10 09:56:42 +0100

    webos-image: Update layer pins for 2024-11-10 from scarthgap
    oe-core as of 2024-11-09 05:55:33 -0800
    
    a051a066da build-appliance-image: Update to scarthgap head revision
    4aa19f4444 weston: backport patch to allow neatvnc < v0.9.0
    5b3b290baa e2fsprogs: removed 'sed -u' option
    79fed0fa3e xserver-xorg: upgrade 21.1.13 -> 21.1.14
    baa0ba7084 python3-lxml=v5.0.2
    f3bc048351 go: upgrade 1.22.7 -> 1.22.8
    4a0ccebab0 go: upgrade 1.22.6 -> 1.22.7
    a11152a65f orc: upgrade 0.4.39 -> 0.4.40
    175e22f2df rust-llvm: Fix CVE-2024-0151
    57de654569 cve_check: Use a local copy of the database during builds
    290407b378 cve-check: add support for cvss v4.0

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=377eba236', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=bcd4e6d77d', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=a051a066da', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=2e3126c9c1', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
