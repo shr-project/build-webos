325059e8fa3cdd5b5397600c54e138a646802560
Martin Jansa <martin2.jansa@lgepartner.com>
2024-11-11 11:21:44 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-11-xx (build 8xx part 1/2) (WRR-9978)
    oe-core as of 2024-11-05 14:15:16 -0800
    
    2e8819c0b9 xmlto: backport a patch to fix build with gcc-14 on host
    80289f49d0 package: Switch debug source handling to use prefix map
    4c75edda8e gcc: restore a patch for Neoverse N2 core
    9b6363994e cve_check: Use a local copy of the database during builds
    eb9c981808 zstd: patch CVE-2022-4899
    
    meta-virtualization as of 2024-11-12 23:19:19 +0000
    
    c996df33 go-cli: use main branch
    
    :Issues Addressed:
    [WRR-9978] CCC: Various build fixes
    
    Change-Id: I4b6a2797b20258220708fc97c6bad5bda5526b2f

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=0c30e9aad', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2c913a7b66', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2e8819c0b9', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=4ad41baed6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -69,7 +69,7 @@ webos_layers = [
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=8bc893d', ''),
-('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=02a6c00d', ''),
+('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=c996df33', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.8.0,commit=d6e576a', ''),
 
