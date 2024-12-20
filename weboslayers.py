4b4d40f8c83fb4e81841b5c3a9b5733446f42932
Martin Jansa <martin2.jansa@lgepartner.com>
2024-12-20 17:30:26 +0100

    webos-image: Update layer pins for 2024-12-20 from scarthgap
    oe-core as of 2024-12-18 06:41:14 -0800
    
    b19b1e905d cve-update-nvd2-native: Tweak to work better with NFS DL_DIR

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=f40a3a477', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=336eec6808', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=b19b1e905d', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=b8d1a14f7f', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
