e996e92f12213388f361c24f302cbea7d603cbe1
Martin Jansa <martin2.jansa@lgepartner.com>
2024-12-20 17:31:01 +0100

    webos-image: Update layer pins for 2024-12-20 from styhead
    oe-core as of 2024-12-18 06:35:02 -0800
    
    6e517366cd cve-update-nvd2-native: Tweak to work better with NFS DL_DIR
    a12be80914 shadow: use update-alternatives to handle su.1 and nologin.8
    82b1c8fb49 dnf: drop python3-iniparse from DEPENDS and RDEPENDS
    01a938f176 python3-poetry-core: drop python3-six from RDEPENDS
    0df327ea64 systemd-boot: drop intltool-native from DEPENDS
    5c6254279c systemd: drop intltool-native from DEPENDS
    067e769ada ghostscript: upgrade 10.03.1 -> 10.04.0
    c8282f5f79 curl: patch CVE-2024-9681
    10abc451f3 rust: ignore CVE-2024-43402
    4d76d2f2af libsndfile1: backport the fix for CVE-2024-50612
    4f6a2eea14 libarchive: fix CVE-2024-48957 & CVE-2024-48958
    ded62bdb5c qemu: patch CVE-2024-6505
    1877317049 builder: set CVE_PRODUCT

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.10,commit=1452e74cc', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=styhead,commit=8717a2e216', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=styhead,commit=6e517366cd', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=styhead,commit=5d54a52fbe', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
