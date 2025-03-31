fa8caec13cdae3d22cc0fb0f6fe0f40f5f7e8533
Martin Jansa <martin2.jansa@lgepartner.com>
2025-03-31 12:09:25 +0200

    webos-image: Update layer pins for 2025-03-31 from scarthgap
    meta-oe as of 2025-03-27 14:04:21 -0400
    
    67ad83dd7c2 krb5: fix CVE-2025-24528
    f23c8d7362a netplan: Fix CVE-2022-4968
    cd1aa14313b wireshark: upgrade 4.2.7 -> 4.2.9
    6f488ab7157 libgpiod: fix gpiod-cxx-test failed test case
    3e3de7632e5 wolfssl: Upgrade 5.7.0 -> 5.7.2
    7bc1db1659b Wolfssl: add ptest
    32ddea6c361 postgresql: upgrade 16.5 -> 16.8
    5412412b97a lcov: Add missing RDEPENDS
    c06d8dc3e58 lcov: sort RDEPENDS alphabetical
    990f47a6144 lcov: include UPSTREAM_CHECK_* to fix UNKNOWN_BROKEN status

--- weboslayers.py
+++ weboslayers.py
@@ -62,7 +62,7 @@ webos_layers = [
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=a720df7ad77', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=edd1a1e284f', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=67ad83dd7c2', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
