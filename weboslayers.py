89b258e0b662dbb9752e9bd58d88973721718929
Martin Jansa <martin2.jansa@lgepartner.com>
2025-04-11 08:50:15 +0200

    webos-image: Update layer pins for 2025-04-11 from scarthgap
    oe-core as of 2025-04-01 08:10:07 -0700
    
    4003b5faa1e mc: set ac_cv_path_ZIP to avoid buildpaths QA issues
    76260674327 yocto-uninative: Update to 4.7 for glibc 2.41
    ef2ba1f04f6 rust-cross-canadian: Set CVE_STATUS ignore for
                CVE-2024-43402
    41f855ea5a2 freetype: follow-up patch for CVE-2025-27363
    3ece58813fa expat: patch CVE-2024-8176
    
    meta-raspberrypi as of 2025-04-10 17:16:41 -0700
    
    3b27c95c163 linux-raspberrypi: Drop deprecated configs from
                android-driver.cfg
    
    meta-virtualization as of 2025-04-09 04:04:42 +0000
    
    9287a355b33 docker: fix Upstream-Status format
    
    meta-clang as of 2025-04-10 17:54:52 -0700
    
    eaa08939eae bcc: fix CVE-2024-2314

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=696c2c1ef09', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=2f242f2a269', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=4003b5faa1e', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=67ad83dd7c2', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -68,17 +68,17 @@ webos_layers = [
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=b9ef0228219', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=52cf1d59c5a', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=eaa08939eae', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=9287a355b33', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.8.1,commit=f4ef1331f0c', ''),
 
 ('meta-webos-backports-5.1',  34, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-5.2',  35, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=scarthgap,commit=scarthgap-2025-04-02', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=scarthgap,commit=scarthgap-2025-04-11', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=scarthgap,commit=c153c694bd7', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=scarthgap,commit=3b27c95c163', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
