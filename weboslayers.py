99f2b37ba5a2990275761006b14a890552a41973
Martin Jansa <martin2.jansa@lgepartner.com>
2025-07-05 14:32:22 +0200

    webos-image: Update layer pins for 2025-07-05 from scarthgap
    oe-core as of 2025-06-20 12:41:51 -0700
    
    cfa97a50e06 cmake: Correctly handle cost data of tests with arbitrary
                chars in name
    c94b6686a1e glibc: stable 2.39 branch updates
    7af711c0a31 coreutils: fix CVE-2025-5278
    b8841097eaf go: set status of CVE-2024-3566
    db02a4cc542 systemd: Rename systemd_v255.21 to systemd_255.21
    1b52b7ebe5f libpng: Add ptest
    8ee21ec40e9 tune-cortexr52: Remove aarch64 for ARM Cortex-R52
    7af83314fea gcc: Upgrade to GCC 13.4
    6c3344ba51a scripts/install-buildtools: Update to 5.0.10
    0f0a06ccef4 python3-requests: upgrade 2.32.3 -> 2.32.4
    0ffe159d9a4 ffmpeg: fix CVE-2025-1373
    352525443b1 net-tools: patch CVE-2025-46836
    f7ee6db8ca5 testimage: get real os-release file
    46c7f1a8d6b gtk+: add missing libdrm dependency
    6cca08b2857 python3: upgrade 3.12.9 -> 3.12.11
    2f3d2a28484 kea: upgrade 2.4.1 -> 2.4.2
    504d92b01ac libsoup: fix CVE-2025-2784
    9c014c1b96f libsoup-2.4: fix CVE-2025-2784
    ebb87904c97 libsoup-2.4: fix CVE-2025-4476
    737d50288a3 libsoup: fix CVE-2025-4948
    b4fb5cd0d33 libsoup-2.4: fix CVE-2025-4948
    f1450eea342 libsoup: fix CVE-2025-46421
    9e32f4fd761 libsoup-2.4: fix CVE-2025-46421
    563a34faae3 libsoup: fix CVE-2025-32050
    d1662790112 libsoup-2.4: fix CVE-2025-32050
    dd92cad3975 libsoup: fix CVE-2025-32051
    9a8a5072969 libsoup: fix CVE-2025-32052
    f3890f25cc0 libsoup-2.4: fix CVE-2025-32052
    
    meta-oe as of 2025-06-23 17:03:24 -0400
    
    727811eaf25 imagemagick: guard sed operations in do_install for
                optional files
    5dfaf0e236f postgresql: upgrade 16.8 -> 16.9
    f79cb5d4455 canutils: use https instead of git protocol
    4f79df74593 libsocketcan: use https instead of git protocol
    9a0a923b647 redis: upgrade 7.2.7 -> 7.2.8
    40c9f33ad29 chrony: use inherit_defer for conditional inherit of
                useradd
    154b5010be6 lmsensors: Fix build without sensord
    
    meta-clang as of 2025-06-20 13:00:12 -0700
    
    eea5ec23155 bpftrace: fix buildpaths QA issues
    525625cb266 bpftrace: move from dynamic-layers/openembedded-layer
                to dynamic-layers/meta-python
    
    meta-raspberrypi as of 2025-07-03 23:08:04 -0700
    
    48452445d77 raspberrypi5.conf: Add CM5 dtb's
    9e89558cb8f rpi-bootfiles: update to latest release
    8c916b683de rpi-default-versions: Switch default kernel to 6.12
    4597c80b122 linux-raspberrypi: update 6.12.2 -> 6.12.25
    bae1bac2e7f linux-raspberrypi: add recipe for 6.12
    1091bde25e9 rpi-bootfiles: Update to latest release
    acc1e1e9f20 rpi-eeprom: Update to latest release
    
    Signed-off-by: Martin Jansa <martin2.jansa@lgepartner.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,15 +60,15 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=139f61fe9ee', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=29e623b2ad0', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=cfa97a50e06', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=491671faee1', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=727811eaf25', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=eaa08939eae', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=scarthgap,commit=eea5ec23155', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=scarthgap,commit=9e040ee8dd6', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.8.1,commit=f4ef1331f0c', ''),
@@ -76,9 +76,9 @@ webos_layers = [
 ('meta-webos-backports-5.1',  34, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-backports-5.2',  35, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
-('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=scarthgap,commit=scarthgap-2025-04-11', ''),
+('meta-webos',                40, 'https://github.com/shr-project/meta-webosose.git',       'branch=scarthgap,commit=scarthgap-2025-07-05', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=scarthgap,commit=bce7b3acd2e', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=scarthgap,commit=48452445d77', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/shr-project/meta-webosose.git',       '', ''),
 
