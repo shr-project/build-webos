webOS-ports/webos-ports-setup

scripts: my rebase scripts
* it's ugly don't look and don't use

* it's just for myself and even this commit message is meant only for
  my future-self :P

* I had parts of this in my ~/bin for 10+ years, moving here just
  so that I can more easily to replicate my environment whenever I
  need to temporarily use different builders.

* the helper scripts also need to be moved outside this repo before
  use, because otherwise it will fail after checking out branch
  from upstream which doesn't have them (this wasn't an issue when
  they were in ~/bin), e.g.:

  Your branch is up to date with 'origin/master'.
  Current branch ose-master is up to date.
  /OE/lge/layers/build-webos-ose/scripts/rebase-multiple.sh: line 20: /OE/lge/layers/build-webos-ose/scripts/rebase-with-log.sh: No such file or directory
  /OE/lge/layers/build-webos-ose/scripts/rebase-multiple.sh: line 20: /OE/lge/layers/build-webos-ose/scripts/rebase-with-log.sh: No such file or directory
  /OE/lge/layers/build-webos-ose/scripts/rebase-multiple.sh: line 20: /OE/lge/layers/build-webos-ose/scripts/rebase-with-log.sh: No such file or directory
  /OE/lge/layers/build-webos-ose/scripts/rebase-multiple.sh: line 20: /OE/lge/layers/build-webos-ose/scripts/rebase-with-log.sh: No such file or directory
  scripts/update.sh: line 23: /OE/lge/layers/build-webos-ose/scripts/rebase-multiple.sh: No such file or directory
  scripts/update.sh: line 32: /OE/lge/layers/build-webos-ose/scripts/rebase-multiple.sh: No such file or directory

* it's just something I use to efficiently manage MANY build
  directories for different DISTROs and OE releases

* All OE layers and LGE build-* directories are primarily maintained in
  /OE/layers/* with similar update.sh script which just rebases
  branches on top of other whenever there is a new change from upstream
  and only the interesting branches and pushed to gerrit/github

* Then the actuall /OE/build/<name>/<release> directories are created:
  git clone --shared [--single-br

scripts/rebase.sh
@@ -1,0 +1,22 @@
+#!/bin/bash
+
+SCRIPTDIR=/OE/lge/layers/scripts
+
+RELEASE="kirkstone"
+BUILD="webos"
+
+BUILD_BRANCH="jansa/${RELEASE}"
+BRANCH="jansa/${RELEASE}"
+BASE_LAYERS=" \
+  meta-webosose \
+  meta-qt6 \
+  meta-oe \
+  meta-raspberrypi \
+  meta-virtualization \
+  meta-security \
+  meta-clang \
+  oe-core \
+  bitbake \
+"
+
+${SCRIPTDIR}/update-layers.sh ${BRANCH} ${BUILD_BRANCH} "${BASE_LAYERS}" build-${BUILD}-${RELEASE}

shr-project/build-webos

scripts/rebase.sh
@@ -1,0 +1,22 @@
+#!/bin/bash
+
+SCRIPTDIR=/OE/lge/layers/scripts
+
+RELEASE="scarthgap"
+BUILD="webos"
+
+BUILD_BRANCH="jansa/${RELEASE}"
+BRANCH="jansa/${RELEASE}"
+BASE_LAYERS=" \
+  meta-webosose \
+  meta-qt6 \
+  meta-oe \
+  meta-raspberrypi \
+  meta-virtualization \
+  meta-security \
+  meta-clang \
+  oe-core \
+  bitbake \
+"
+
+${SCRIPTDIR}/update-layers.sh ${BRANCH} ${BUILD_BRANCH} "${BASE_LAYERS}" build-${BUILD}-${RELEASE}